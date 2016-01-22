23909ef057e8b95dfeaa81ae75dd13681b455b96
mlehtiva <maria.lehtivaara@csc.fi>
2016-01-22 14:06:32 +0200

    Add option to choose png or pdf to correlogram and illumina qc

--- modules/microarray/R/plot-corrgram.R
+++ modules/microarray/R/plot-corrgram.R
@@ -1,13 +1,16 @@
 # TOOL plot-corrgram.R: Correlogram (Plots the correlations between samples in a graph.)
 # INPUT normalized.tsv: normalized.tsv TYPE GENE_EXPRS 
 # INPUT META phenodata.tsv: phenodata.tsv TYPE GENERIC 
-# OUTPUT corrgram.pdf: corrgram.pdf 
+# OUTPUT OPTIONAL corrgram.png: corrgram.png 
+# OUTPUT OPTIONAL corrgram.pdf: corrgram.pdf 
 # PARAMETER OPTIONAL image.width: "Image width" TYPE INTEGER FROM 200 TO 3200 DEFAULT 600 (Width of the plotted network image)
 # PARAMETER OPTIONAL image.height: "Image height" TYPE INTEGER FROM 200 TO 3200 DEFAULT 600 (Height of the plotted network image)
+# PARAMETER OPTIONAL image.type: "Image type" TYPE [PNG: PNG, PDF: PDF] DEFAULT PNG (Format of the output image. Please note that if you choose the PDF format, it will take a lot of time to draw it, and thus it is advisable to export the PDF for closer examination.)
 
 
 # Correlogram
 # JTT 18.10.2007
+# ML 22.1.2016, added option to print image either as png or pdf
 
 # Parameter settings (default) for testing purposes
 #image.width<-c(600)
@@ -35,8 +38,16 @@ phenodata<-read.table("phenodata.tsv", header=T, sep="\t")
 colnames(dat2)<-gsub(" ", "", phenodata$description)
 
 # Plotting
-pdf(file="corrgram.pdf", width=w/72, height=h/72)
-corrgram(x=dat2, lower.panel=panel.shade, upper.panel=panel.pts, pch=".")
-dev.off()
+if (image.type == "PNG") {
+	bitmap(file="corrgram.png", width=w/72, height=h/72)
+	corrgram(x=dat2, lower.panel=panel.shade, upper.panel=panel.pts, pch=".")
+	dev.off()
+}
+if (image.type == "PDF") {
+	pdf(file="corrgram.pdf", width=w/72, height=h/72)
+	corrgram(x=dat2, lower.panel=panel.shade, upper.panel=panel.pts, pch=".")
+	dev.off()
+}
+
 
 
--- modules/microarray/R/qc-illumina.R
+++ modules/microarray/R/qc-illumina.R
@@ -1,14 +1,19 @@
 # TOOL qc-illumina.R: Illumina (Illumina quality control using boxplots and density plots. This tool should be run on normalized data.)
 # INPUT normalized.tsv: normalized.tsv TYPE GENE_EXPRS 
-# OUTPUT boxplot.png: boxplot.png 
+# INPUT META phenodata.tsv: phenodata.tsv TYPE GENERIC 
+# OUTPUT OPTIONAL boxplot.pdf: boxplot.pdf 
+# OUTPUT OPTIONAL boxplot.png: boxplot.png 
 # OUTPUT densityplot.pdf: densityplot.pdf 
 # PARAMETER image.width: "Image width" TYPE INTEGER FROM 200 TO 3200 DEFAULT 600 (Width of the plotted network image)
 # PARAMETER image.height: "Image height" TYPE INTEGER FROM 200 TO 3200 DEFAULT 600 (Height of the plotted network image)
+# PARAMETER OPTIONAL image.type: "Boxplot image type" TYPE [PNG: PNG, PDF: PDF] DEFAULT PNG (Format of the boxplot output image. Please note that if you choose the PDF format, it will take a lot of time to draw it, and thus it is advisable to export the PDF for closer examination.)
+
 
 
 # Quality control of Illumina chips
 # 30.7.2007 JTT
 # 19.4.2011 MG, added legend to histogram
+# 12.1.2016 ML, changed the naming in plots, added option to print boxplot image as PDF
 
 # Loads the libraries
 library(limma)
@@ -21,6 +26,10 @@ h<-image.height
 file<-c("normalized.tsv")
 dat<-read.table(file, header=T, sep="\t", row.names=1)
 
+# Loads the phenodata to get the description column
+phenodata<-read.table("phenodata.tsv", header=T, sep="\t")
+# groups<-phenodata[,pmatch(column,colnames(phenodata))]
+
 # Separates expression values and flags
 calls<-dat[,grep("flag", names(dat))]
 dat2<-dat[,grep("chip", names(dat))]
@@ -28,9 +37,17 @@ dat2<-dat[,grep("chip", names(dat))]
 # Producing some basic plots of the data
 
 # Boxplot
-bitmap(file="boxplot.png", width=w/72, height=h/72)
-boxplot(as.data.frame(dat2), las=2, cex.axis=0.5)
-dev.off()
+
+if (image.type == "PNG") {
+	bitmap(file="boxplot.png", width=w/72, height=h/72)
+	boxplot(as.data.frame(dat2), las=2, cex.axis=0.5, names=phenodata$description)
+	dev.off()
+}
+if (image.type == "PDF") {
+	pdf(file="boxplot.pdf", width=w/72, height=h/72)
+	boxplot(as.data.frame(dat2), las=2, cex.axis=0.5, names=phenodata$description)
+	dev.off()
+}
 
 # Density plot
 pdf(file="densityplot.pdf", width=w/72, height=h/72)
@@ -44,5 +61,5 @@ plot(x[,1], y[,1], col=1, ylim=c(0, max(y)), main="Intensities", type="l", lwd=2
 for(i in 2:ncol(dat2)) {
    lines(x[,i], y[,i], col=i, lwd=2, lty=1)
 }
-legend(legend=colnames(dat2), x="topright", lty=1, cex=0.75, col=c(1:ncol(dat2)))
+legend(legend=phenodata$description, x="topright", lty=1, cex=0.75, col=c(1:ncol(dat2)))
 dev.off()
