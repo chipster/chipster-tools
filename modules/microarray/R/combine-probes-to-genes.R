3cb70587bff957f8603ffd1c96ee0b374ac8f66e
mlehtiva <maria.lehtivaara@csc.fi>
2016-02-26 10:25:17 +0200

    Fix combine-probes-to-genes.R

--- modules/microarray/R/combine-probes-to-genes.R
+++ modules/microarray/R/combine-probes-to-genes.R
@@ -51,42 +51,42 @@ for(i in 1:ncol(dat2)) {
 	dat3[,i]<-m$x
 }
 
-# Second round of combination
-if(produce.identifiers=="yes") {
-	
-	symbol<-m$Group.1
-	
-	# Generating rownames
-	genes<-rep(NA, length(symbol)) 
-	for(i in 1:length(symbol)) {
-		genes[i]<-rownames(dat)[grep(symbol[i], dat$symbol)][1]
-	}
-	
-	# Second round of combination, now according to rownames
-	test2<-aggregate(dat3[,1], list(genes), mean)
-	dat6<-matrix(ncol=ncol(dat2), nrow=nrow(test2), data=NA)
-	
-	for(i in 1:ncol(dat2)) {
-		m<-aggregate(dat3[,i], list(genes), mean)
-		dat6[,i]<-m$x
-	}
-	
-	rownames(dat6)<-genes[!duplicated(genes)]
-	colnames(dat6)<-colnames(dat2)
-	
-	# Fetch back the symbol and description column, if present
-	# in the original data
-	if (length(symbols)>0) {
-		symbols2 <- as.character(dat$symbol[match (rownames(dat6), rownames(dat))])
-		descriptions2 <- as.character(dat$description[match (rownames(dat6), rownames(dat))])
-		annotations <- cbind(symbols2, descriptions2)
-		dat6 <- data.frame(annotations, dat6)
-		names(dat6) [1:2] <- c("symbol", "description")
-	}
-	
-	write.table(data.frame(dat6), file="combined.tsv", col.names=T, quote=F, sep="\t", row.names=T)
-	
-} else {	
+## Second round of combination
+#if(produce.identifiers=="yes") {
+#	
+#	symbol<-m$Group.1
+#	
+#	# Generating rownames
+#	genes<-rep(NA, length(symbol)) 
+#	for(i in 1:length(symbol)) {
+#		genes[i]<-rownames(dat)[grep(symbol[i], dat$symbol)][1]
+#	}
+#	
+#	# Second round of combination, now according to rownames
+#	test2<-aggregate(dat3[,1], list(genes), mean)
+#	dat6<-matrix(ncol=ncol(dat2), nrow=nrow(test2), data=NA)
+#	
+#	for(i in 1:ncol(dat2)) {
+#		m<-aggregate(dat3[,i], list(genes), mean)
+#		dat6[,i]<-m$x
+#	}
+#	
+#	rownames(dat6)<-genes[!duplicated(genes)]
+#	colnames(dat6)<-colnames(dat2)
+#	
+#	# Fetch back the symbol and description column, if present
+#	# in the original data
+#	if (length(symbols)>0) {
+#		symbols2 <- as.character(dat$symbol[match (rownames(dat6), rownames(dat))])
+#		descriptions2 <- as.character(dat$description[match (rownames(dat6), rownames(dat))])
+#		annotations <- cbind(symbols2, descriptions2)
+#		dat6 <- data.frame(annotations, dat6)
+#		names(dat6) [1:2] <- c("symbol", "description")
+#	}
+#	
+#	write.table(data.frame(dat6), file="combined.tsv", col.names=T, quote=F, sep="\t", row.names=T)
+#	
+#} else {	
 	# Putting a data file together
 	colnames(dat3)<-colnames(dat2)
 	# colnames(dat4)<-paste("sd.", colnames(dat2), sep="")
@@ -105,4 +105,4 @@ if(produce.identifiers=="yes") {
 	
 	# Writing data to disk
 	write.table(data.frame(dat5), file="combined.tsv", col.names=T, quote=F, sep="\t", row.names=T)
-}
+#}
