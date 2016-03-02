72b9c856a9ae9fec064c296a9818a842605053e8
klemela <klemela>
2016-03-02 17:04:23 +0200

    Test tool for Python3

---
+++ modules/ngs/python3/test-data-in-out3.py
@@ -0,0 +1,8 @@
+# TOOL test-data-in-out3.py: "Test data input and output in Python3" (Data input output test.) 
+# INPUT input TYPE GENERIC
+# OUTPUT output
+
+import shutil
+
+shutil.copyfile('input', 'output')
+
