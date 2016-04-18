7b739a08a2c16ccbc985e747569567e0411c48e6
kkmattil <kimmo.mattila@csc.fi>
2016-04-18 10:38:31 +0300

    concatenate updated.

--- modules/misc/R/concatenate.R
+++ modules/misc/R/concatenate.R
@@ -1,21 +1,21 @@
 # TOOL concatenate.R: "Concatenate files" (Writes the content of seletced files into one output file)
 # INPUT input{...}.data: "Input files" TYPE GENERIC
-# OUTPUT OPTIONAL result.txt
-# OUTPUT OPTIONAL result.html
-# OUTPUT OPTIONAL result.tsv
-# OUTPUT OPTIONAL result.csv
-# OUTPUT OPTIONAL result.fasta
-# OUTPUT OPTIONAL result.fastq
-# OUTPUT OPTIONAL result.xml
-# OUTPUT OPTIONAL result.bed
-# OUTPUT OPTIONAL result.gff3
-# OUTPUT OPTIONAL result.text
+# OUTPUT OPTIONAL concatenated.txt
+# OUTPUT OPTIONAL concatenated.html
+# OUTPUT OPTIONAL concatenated.tsv
+# OUTPUT OPTIONAL concatenated.csv
+# OUTPUT OPTIONAL concatenated.fasta
+# OUTPUT OPTIONAL concatenated.fastq
+# OUTPUT OPTIONAL concatenated.xml
+# OUTPUT OPTIONAL concatenated.bed
+# OUTPUT OPTIONAL concatenated.gff3
+# OUTPUT OPTIONAL concatenated.text
 # PARAMETER outformat: "Output name extension" TYPE [ auto: "Automatic", txt: ".txt, General text formatted data", tsv: ".tsv, Tab delimited table", html: ".html, HTML formatted data", fasta: ".fasta, FASTA formatted sequence data",  fastq: ".fastq, Sequence data in FASTQ format " ] DEFAULT auto (Output format type)
 
+#K.M. 18.4. 2016
 source(file.path(chipster.common.path, "zip-utils.R"))
-#unzipIfGZipFile("input1")
-#unzipIfGZipFile("input2")
-# Change bam names in VCF to original names
+
+#check if files are zipped
 input.names <- read.table("chipster-inputs.tsv", header=F, sep="\t")
 for (i in 1:nrow(input.names)) {
 	unzipIfGZipFile(input.names[i,1])
@@ -34,7 +34,7 @@ if (outformat == "auto"){
 	 
 	 if ( outformat != outformat2){
 		 fcheck <- 1
-	 }		      #stop("CHIPSTER-NOTE:",input.names[1,2], outformat, input.names[2,2])
+	 }		      
    }
  if ( fcheck != 0) {
 	 stop("CHIPSTER-NOTE:Input files contain different file name extensions! If you are sure you want to do the concatenation, please select explicit output file name extension.")
@@ -48,7 +48,7 @@ if (outformat == "txt" || outformat == "html" || outformat == "tsv" || outformat
  } else { outformat <- "txt" }
 
 
-outfile <- paste("result.",outformat , sep="" )
+outfile <- paste("concatenated.",outformat , sep="" )
 command.full <- paste("cat *.data > ", outfile)
 
 system(command.full)
