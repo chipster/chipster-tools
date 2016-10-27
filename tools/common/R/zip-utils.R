ef2715afc245049e483da8099427a904c5576d32
Taavi Hupponen <taavi.hupponen@csc.fi>
2016-10-27 16:01:32 +0300

    Improve string comparison in isGzipFile

--- tools/common/R/zip-utils.R
+++ tools/common/R/zip-utils.R
@@ -16,9 +16,5 @@ isGZipFile <- function(file.name) {
 	# get file type with the unix file command
 	file.type = system(paste("file -Lb --mime", file.name), intern=TRUE)
 	
-	if (!is.na(pmatch("application/x-gzip", file.type)) || !is.na(pmatch("application/gzip", file.type))) {
-		return(TRUE);
-	} else { 
-		return(FALSE);
-	}
+	return (charmatch(file.type,c("application/x-gzip","application/gzip"), nomatch=0) > 0)
 }
