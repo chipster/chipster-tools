14bbf933aff954ca3bb645bcc385cd92e626ed36
amsaren <ari-matti.saren@csc.fi>
2016-12-15 16:24:15 +0200

    Additional info lines in output, fixed single-end bug.

--- tools/ngs/R/rseqc_infer_rnaseq_experiment.R
+++ tools/ngs/R/rseqc_infer_rnaseq_experiment.R
@@ -33,7 +33,6 @@ if (pe){
 	bowtie.command <- paste(bowtie.command, "-U subset.reads1.fq")
 }
 bowtie.command <- paste(bowtie.command, "-S alignment.sam 2>> bowtie2.log'")
-#stop("CHIPSTER-NOTE:", bowtie.command)
 system(bowtie.command)
 
 internal.bed <- file.path(chipster.tools.path, "genomes", "bed", paste(organism, ".bed" ,sep="" ,collapse=""))
@@ -41,40 +40,57 @@ internal.bed <- file.path(chipster.tools.path, "genomes", "bed", paste(organism,
 # Infer experiment
 # tools-bin RSeQC
 ie.binary <- c(file.path(chipster.tools.path, "rseqc", "infer_experiment.py"))
-# Old RSeQC
-#ie.binary <- c(file.path(chipster.tools.path, "RSeQC", "scripts", "infer_experiment.py"))
 
 ie.command <- paste(ie.binary, "-i alignment.sam -r", internal.bed, "> experiment_data.txt")
 system(ie.command)
 
 # Add some helpful explanation to the output
 # Get the values from the output file
-system(paste(" grep 1++ experiment_data.txt | awk -F \": \" '{print $2}' > values.txt"))
-system(paste(" grep 2++ experiment_data.txt | awk -F \": \" '{print $2}' >> values.txt"))
+system(paste(" grep \":\" experiment_data.txt | awk -F \": \" '{print $2}' > values.txt"))
 # read them into R
 values <- scan("values.txt", what=double(), sep="\n")
 # Make sure we dont get a division by zero error
-values[1] <- values[1] + 0.0001
 values[2] <- values[2] + 0.0001
+values[3] <- values[3] + 0.0001
 # Check the case
-ratio <- values[1] / values[2]
+ratio <- values[2] / values[3]
 
+
+# Add additional help lines to output
 if (ratio > 10){
 	message <- paste("\nIt seems the data is stranded. Read 1 is always on the same strand as the gene.")
+	message <- paste(message, "\n\nCorresponding parameters are:")
+	message <- paste(message, "\nTopHat, Cufflinks and Cuffdiff: library-type fr-secondstrand")
+	message <- paste(message, "\nHTSeq: stranded -- yes")
 }else if (ratio < 0.1){
 	message <- paste("\nIt seems the data is stranded. Read 2 is always on the same strand as the gene.")
+	message <- paste(message, "\n\nCorresponding parameters are:")
+	message <- paste(message, "\nTopHat, Cufflinks and Cuffdiff: library-type fr-firststrand")
+	message <- paste(message, "\nHTSeq: stranded -- reverse")
 }else{
-	message <- paste("\nIt seems the data is unstranded.")	
+	message <- paste("\nIt seems the data is unstranded.")
+	message <- paste(message, "\n\nCorresponding parameters are:")
+	message <- paste(message, "\nTopHat, Cufflinks and Cuffdiff: library-type fr-unstranded")
+	message <- paste(message, "\nHTSeq: stranded -- no")
 }
+
+# Add input names to output
+source(file.path(chipster.common.path, "tool-utils.R"))
+inputnames <- read_input_definitions()
+
+message <- paste(message, "\n\nInput files were assigned as follows:")
+message <- paste(message, "\nRead 1 file:", inputnames$reads1.fq)
+if(pe){
+	message <- paste(message, "\nRead 2 file:", inputnames$reads2.fq)
+}
+
 write(message, file = "experiment_data.txt", append = TRUE)
 
 # Inner distance. Only for paired end reads
 if (pe){
     # tools-bin RSeQC
     ie.binary <- c(file.path(chipster.tools.path, "rseqc", "inner_distance.py"))
-    # Old RSeQC
-    #ie.binary <- c(file.path(chipster.tools.path, "RSeQC", "scripts", "inner_distance.py"))
-
+   
     id.command <- paste(ie.binary, "-i alignment.sam -r", internal.bed, " -o id")
     system(id.command)
     try(source("id.inner_distance_plot.r"), silent=TRUE)
