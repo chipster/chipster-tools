7c06249df5843aab5b817edf163a1ee343091e33
mlehtiva <maria.lehtivaara@csc.fi>
2016-12-19 15:24:55 +0200

    Add gzip option

--- tools/common/R/srafetch.R
+++ tools/common/R/srafetch.R
@@ -5,7 +5,8 @@
 # PARAMETER entry_id: "Name or SRR ID of the SRA entry" TYPE STRING DEFAULT "entry" (Give the SRR id of the SRA entry to be retrieved. For example: SRR000021) 
 # PARAMETER dump: "Sequences to dump" TYPE [all: "All", aligned: "Only aligned sequences", unaligned: "Only unaligned sequences"  ] DEFAULT all (Define the reads to be retrieved from the SRA entry)
 
-# KM 8.11. 2014
+# KM 08.11.2014
+# ML 19.12.2016
 
 dump.par<-(" ")
 
@@ -19,7 +20,7 @@ if ( dump == "unaligned"){
 
 sra.path <- file.path(chipster.tools.path, "sratoolkit", "bin")
 sra.binary <- file.path(sra.path, "fastq-dump")
-command.full <- paste(sra.binary, dump.par, '--split-files', entry_id, ' 1> srafetch.tmp 2> srafetch2.tmp' )
+command.full <- paste(sra.binary, dump.par, '--split-files --gzip', entry_id, ' 1> srafetch.tmp 2> srafetch2.tmp' )
 echo.command <- paste('echo "',command.full, ' "> srafetch.log' )
 system(echo.command)
 system(command.full)
