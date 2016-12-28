ae51917644bab50443c64190ea9896ad983499c9
mlehtiva <maria.lehtivaara@csc.fi>
2016-12-28 10:39:44 +0200

    Mothur alignseqs new silva version

--- tools/ngs/R/mothur-alignseqs.R
+++ tools/ngs/R/mothur-alignseqs.R
@@ -5,6 +5,7 @@
 # OUTPUT aligned-summary.tsv
 
 # EK 05.06.2013
+# ML 21.12.2016 update (new Silva version)
 
 # check out if the file is compressed and if so unzip it
 source(file.path(chipster.common.path, "zip-utils.R"))
@@ -12,8 +13,8 @@ unzipIfGZipFile("reads.fasta")
 
 # binary
 binary <- c(file.path(chipster.tools.path, "mothur", "mothur"))
-data.path <- c(file.path(chipster.tools.path, "mothur-data"))
-template.path <- c(file.path(data.path, "silva.bacteria.fasta"))
+data.path <- c(file.path(chipster.tools.path, "mothur-silva-reference"))
+template.path <- c(file.path(data.path, "silva.bacteria/silva.bacteria.fasta"))
 
 # batch file
 if (file.exists("reference.fasta")){
