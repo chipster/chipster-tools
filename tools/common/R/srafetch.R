bd1f6ccdd0162d029866707a0eda26b4e63a790a
Taavi Hupponen <taavi.hupponen@csc.fi>
2017-01-30 16:57:09 +0200

    Fix srafetch.R

--- tools/common/R/srafetch.R
+++ tools/common/R/srafetch.R
@@ -1,36 +1,42 @@
 # TOOL srafetch.R: "Retrieve FASTQ files from SRA database" (Retrieve reads in FASTQ format from the SRA database based on the entry ID or name.)
 # OUTPUT OPTIONAL srafetch.log
-# OUTPUT OPTIONAL sra_reads_1.fastq
-# OUTPUT OPTIONAL sra_reads_2.fastq
+# OUTPUT sra_reads_{...}.fastqc.gz: "FASTQ files"
 # PARAMETER entry_id: "Name or SRR ID of the SRA entry" TYPE STRING DEFAULT "entry" (Give the SRR id of the SRA entry to be retrieved. For example: SRR000021) 
 # PARAMETER dump: "Sequences to dump" TYPE [all: "All", aligned: "Only aligned sequences", unaligned: "Only unaligned sequences"  ] DEFAULT all (Define the reads to be retrieved from the SRA entry)
 
 # KM 08.11.2014
 # ML 19.12.2016
+# TH 20.1.2017
 
-dump.par<-(" ")
-
-if ( dump == "aligned"){
-	dump.par <- ("--aligned")
+dump.param <- ""
+if (dump == "aligned") {
+	dump.param <- "--aligned"
+} else if (dump == "unaligned") {
+	dump.param <- "--unaligned"
 }
-if ( dump == "unaligned"){
-	dump.par <- ("--unaligned")
-}
-
 
 sra.path <- file.path(chipster.tools.path, "sratoolkit", "bin")
 sra.binary <- file.path(sra.path, "fastq-dump")
-command.full <- paste(sra.binary, dump.par, '--split-files --gzip', entry_id, ' 1> srafetch.tmp 2> srafetch2.tmp' )
-echo.command <- paste('echo "',command.full, ' "> srafetch.log' )
-system(echo.command)
+command.full <- paste(sra.binary, dump.param, '--split-files --gzip', entry_id,  '1>>srafetch.log 2>>srafetch.log')
+cat(command.full, "\n", file="srafetch.log", append=TRUE)
 system(command.full)
-system('cat srafetch.tmp >> srafetch.log')
-system('cat srafetch2.tmp >> srafetch.log')
-#  system('ls -l >> dbfetch.log')
-system("mv *RR*_1.fastq sra_reads_1.fastq")
-system("mv *RR*_2.fastq sra_reads_2.fastq")  
-#  system('ls -l >> dbfetch.log')
 
+# check that we got something
+fastq.files <- Sys.glob("*.fastq.gz")
+if (length(fastq.files) == 0) {
+	system('echo srafetch.log:')
+	system('cat srafetch.log')
+	stop("Fetching fastq files failed, see the end of the details for more information")
+}
 
-
+# rename outputs and create output names file
+source(file.path(chipster.common.path, "tool-utils.R"))
+outputnames <- matrix(NA, nrow=length(fastq.files), ncol=2)
+for (i in 1:length(fastq.files)) {
+	original.name <- fastq.files[i]
+	new.name <- paste("sra_reads_", i, ".fastqc.gz", sep ="")
+	outputnames[i,] <- c(new.name, original.name)
+	system(paste("mv", original.name, new.name))
+}
+write_output_definitions(outputnames)
 
