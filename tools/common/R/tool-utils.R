60fa5ca14efbfe2a312d355244e9c4558e5781a8
amsaren <ari-matti.saren@csc.fi>
2017-03-20 10:40:39 +0200

    New tool-utils

--- tools/common/R/tool-utils.R
+++ tools/common/R/tool-utils.R
@@ -97,3 +97,127 @@ make_input_list <-function(listfile){
 	}
 	return(input.list)
 }
+
+# fileOk and fileNotOk ar functions to check that file exists and optionally that it is larger than the provided threshold.
+# fileOk("myfile.txt") : returns TRUE if file myfile.txt exists
+# fileOk("myfile.txt", 100) : returns TRUE if file myfile.txt exists and is min. 100 bytes
+# fileOk("myfile.txt",,2) : returnd TRUE if file myfile.txt exists and is min. 2 lines
+# The two functions work the same exept fileOk returns TRUE if the file meets the criteria, and fileNotOk returns TRUE
+# if it does not. The idea is to make if statements more readable.
+
+fileOk <- function(filename, minsize, minlines){
+	# Check that file exists
+	if (!(file.exists(filename))){
+		return(FALSE)
+	}
+	# Check minimum size if provided
+	if (!(missing(minsize))){
+		if (file.info(filename)$size < minsize) {
+			return(FALSE)	
+		}
+	}
+	# Check line number if provided
+	if (!(missing(minlines))){
+		linenumber <- as.integer(system(paste("wc -l <", filename), intern=TRUE))
+		if (linenumber < minlines) {
+			return(FALSE)	
+		}
+	}
+	# If all checks pass, return TRUE
+	return(TRUE)
+}
+
+fileNotOk <- function(filename, minsize, minlines){
+	# Check if file exists
+	if (!(file.exists(filename))){
+		return(TRUE)
+	}
+	# Check minimum size if provided
+	if (!(missing(minsize))){
+		if (file.info(filename)$size < minsize) {
+			return(TRUE)	
+		}
+	}
+	# Checkline number if provided
+	if (!(missing(minlines))){
+		linenumber <- as.integer(system(paste("wc -l <", filename), intern=TRUE))
+		if (linenumber < minlines) {
+			return(TRUE)	
+		}
+	}
+	# If all checks pass, return FALSE
+	return(FALSE)
+}
+
+fileCheck <- function(filename, minsize, minlines){
+	# Check if file exists
+	if (!(file.exists(filename))){
+		stop(paste('CHIPSTER-NOTE: ', "Required file", filename, "does not exist."))
+	}
+	# Check minimum size if provided
+	if (!(missing(minsize))){
+		filesize <- file.info(filename)$size
+		if (filesize < minsize) {
+			stop(paste('CHIPSTER-NOTE: ', "Required file", filename, "exists, but is smaller (", filesize, ") than required size (", minsize, ")."))	
+		}
+	}
+	# Checkline number if provided
+	if (!(missing(minlines))){
+		linenumber <- as.integer(system(paste("wc -l <", filename), intern=TRUE))
+		if (linenumber < minlines) {
+			stop(paste('CHIPSTER-NOTE: ', "Required file", filename, "exists, but has fewer lines (", linenumber, ") than required (", minlines, ")."))	
+		}
+	}
+}
+
+# Wrapper for system2() command. 
+# Optionally captures stderr.
+# Checks for exit status and gives an error message if status != 0
+# Allows setting environment variables give as character vector in style "VARIABLE=value"
+#
+runExternal <- function(command, env = NULL, capture = TRUE, checkexit = TRUE){
+	
+	# Split command to words
+	wcom <- strsplit(command, " ")[[1]]
+	
+	# if environment varaiables are set, the command has to be started with "bash -c" and encased in single quotes.
+	if (!is.null(env)){
+		wcom <- c("bash", "-c", "\'", wcom, "\'")		
+	}
+	
+	# Add timestamp and command line to stderr.log
+	system("echo \"# Chipster log\" >> stderr.log")
+	system("echo \"# \"\`date\` >> stderr.log")
+	write(paste(wcom, collapse = " "), file = "stderr.log", append = TRUE)
+	
+	# Capture of stderr is optional
+	if (capture){
+		# Run command, capture stderr
+		exitcode <- system2(wcom[1], wcom[2:length(wcom)], stderr="stderr.tmp", env=env )
+		
+		# Append error messages to the log file. stderr.tmp is overwritten each time runExternal is called.
+		system("echo >> stderr.log")
+		system("cat stderr.tmp >> stderr.log")
+		system("echo  >> stderr.log")
+	} else {
+		# Run command without capturing stderr
+		exitcode <- system2(wcom[1], wcom[2:length(wcom)], env=env )		
+	}
+	# Show error message if command fails
+	if (checkexit){
+		if (exitcode != 0){
+			
+			error <- scan("stderr.tmp", what=" ", sep="\n")
+			
+			msg <- paste("External program did not complete succesfully.\n")
+			msg <- paste(msg,"Please check your parameters, input files and input file assignment.\n\n")
+			msg <- paste(msg,"Failed command:\n")
+			msg <- paste(msg, command, "\n\n")
+			msg <- paste(msg, "Error:\n")
+			msg <- paste(msg, paste(error, collapse = "\n"))
+			
+			stop(paste('CHIPSTER-NOTE: ', msg))
+		}
+	}
+	
+}
