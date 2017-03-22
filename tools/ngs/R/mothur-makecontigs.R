f14fbfdf9a713421848146c62949d6788b1b3729
amsaren <ari-matti.saren@csc.fi>
2017-03-22 17:02:38 +0200

    Improved error messages.

--- tools/ngs/R/mothur-makecontigs.R
+++ tools/ngs/R/mothur-makecontigs.R
@@ -22,7 +22,11 @@
 # AMS 16.03.2017: Changed to use single tar file as input
 # OUTPUT OPTIONAL contigs.summary.tsv
 
-#source(file.path(chipster.common.path, "tool-utils.R"))
+source(file.path(chipster.common.path, "tool-utils.R"))
+source(file.path(chipster.common.path, "zip-utils.R"))
+
+# check out if the file is compressed and if so unzip it
+unzipIfGZipFile("reads.tar")
 
 # binary
 binary <- c(file.path(chipster.tools.path, "mothur", "mothur"))
@@ -32,24 +36,35 @@ template.path <- c(file.path(data.path, "silva.bacteria.fasta"))
 # Read the contents of the tar file into a list
 system("tar tf reads.tar > tar.contents")
 file.list <- scan("tar.contents", what="", sep="\n")
+# Check that the input is a valid tar file
+if (length(file.list) == 0){
+	stop(paste('CHIPSTER-NOTE: ', "It seems your input file was not a valid Tar package. Please check your input file."))
+}
+# Check if tar packa contains folders
+if (grepl("/", file.list[1])){
+	stop(paste('CHIPSTER-NOTE: ', "It seems your Tar package contains folders. The FASTQ files need to be in the root of the package, not in subfolders."))
+}
 
-# Open tar file
+# Open tar package
 system("tar xf reads.tar")
 
 # Go through the file list and gzip. Files that are already gzipped will be skipped
 for (i in 1:length(file.list)) {
-	system(paste("gzip", file.list[i]))
+		system(paste("gzip", file.list[i]))
 }
 
 # Use Mothur make.file tool to generate the input list
 write("make.file(inputdir=., type=gz, prefix=fastq)", "makefile.mth", append=F)
 command <- paste(binary, "makefile.mth")
 system(command)
+system("cat *.logfile > log.tmp")
+
 
 # Run Mothur make.contigs
 write("make.contigs(file=fastq.files)", "makecontigs.mth", append=F)
 command <- paste(binary, "makecontigs.mth", "> log2.txt")
 system(command)
+system("cat *.logfile >> log.tmp")
 
 # rename the result files
 system("mv fastq.trim.contigs.fasta contigs.fasta")
@@ -62,14 +77,19 @@ system("sed -n  '/Group count: / ,/Output File/p' log2.txt > log.txt")
 write("summary.seqs(fasta=contigs.fasta)", "summary.mth", append=F)
 command <- paste(binary, "summary.mth", "> log_raw.txt")
 system(command)
+system("cat *.logfile >> log.tmp")
 
 # Post process output
 system("grep -A 10 Start log_raw.txt > fastq-summary2.tsv")
 # Remove one tab to get the column naming look nice:
-system("sed 's/^		/	/' fastq-summary2.tsv > contigs.summary.tsv")
+system("sed 's/^		/	/' fastq-summary2.tsv > contigs.summary.tsv.tmp")
+
+# If contigs.summary.tsv is empty, return stderr.log
+if (fileOk("contigs.summary.tsv.tmp", minlines = 1)){
+	system("mv contigs.summary.tsv.tmp contigs.summary.tsv")
+}else{
+	system("mv log.tmp log.txt")
+}
 
 # Gzip output fasta
 system("gzip contigs.fasta")
-
-#system("cat *.logfile >> log.txt")
-#system("ls -l >>log.txt")
