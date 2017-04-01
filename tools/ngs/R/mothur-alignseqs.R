27b3d375413a360c39fe041424c9cf8806ddc2d3
ekorpela <ekorpela@EKORPELA-820G2.windows.csc.fi>
2017-04-01 07:38:48 +0300

    Clarified manuals.

--- tools/ngs/R/mothur-alignseqs.R
+++ tools/ngs/R/mothur-alignseqs.R
@@ -1,4 +1,4 @@
-# TOOL mothur-alignseqs.R: "Align sequences to reference" (Given a fasta file of 16S rRNA sequences, this tool aligns them to the Silva reference alignment available on the server. Please indicate the region of the reference alignment that your amplified region corresponds to. If you opt to use your own reference alignment fasta, please make sure the files are correctly assigned in the parameters section! The speed of this tool depends on several things, please see the manual. This tool is based on the Mothur tool align.seqs.)
+# TOOL mothur-alignseqs.R: "Align sequences to reference" (Given a fasta file of 16S rRNA sequences, this tool aligns them to the Silva reference alignment available on the server. Please indicate the region of the reference alignment that your amplified region corresponds to. If you opt to use your own reference alignment, please make sure the input fasta files are correctly assigned in the parameters section! The speed of this tool depends on several things, please see the manual. This tool is based on the Mothur tool align.seqs.)
 # INPUT reads.fasta: "FASTA file" TYPE FASTA
 # INPUT OPTIONAL reference.fasta: "Custom reference FASTA file" TYPE FASTA
 # INPUT OPTIONAL a.count_table: "Count table" TYPE MOTHUR_COUNT
@@ -8,7 +8,6 @@
 # PARAMETER OPTIONAL reference: "Reference" TYPE [bacterial, full, own] DEFAULT bacterial (Reference sequences to use.)
 # PARAMETER OPTIONAL start: "Start" TYPE INTEGER (Start point of your region of interest)
 # PARAMETER OPTIONAL end: "End" TYPE INTEGER (End point of your region of interest)
-# PARAMETER OPTIONAL keepdots: "Remove leading and trailing dots" TYPE [yes, no] DEFAULT yes (Remove leading and trailing dots.)
 
 
 # EK 05.06.2013
@@ -16,6 +15,8 @@
 # ML 4.1.2016 new, whole Silva reference
 # ML 14.3.2017 reference option (bacterial vs whole)
 # ML 15.3.2017 add pcr.seqs options
+# PARAMETER OPTIONAL keepdots: "Remove leading and trailing dots" TYPE [yes, no] DEFAULT yes (Remove leading and trailing dots.)
+
 
 # OUTPUT log.txt
 
@@ -49,14 +50,14 @@ if (!is.na(start) | !is.na(end)){
 	pcrseqs.options <- ""
 	if (reference=="own"){
 		if (file.exists("reference.fasta")) {
-		pcrseqs.options <- paste(pcrseqs.options, "pcr.seqs(fasta=reference.fasta", sep="")
+		pcrseqs.options <- paste(pcrseqs.options, "pcr.seqs(fasta=reference.fasta, keepdots=F", sep="")
 		} else{
 		stop('CHIPSTER-NOTE: If you choose to use your own reference, you need to give the fasta file for that as input!')
 		}
 	} else {
 		# if using full or bacterial silva reference:
 		#pcrseqs.options <- paste(pcrseqs.options, "pcr.seqs(fasta=", template.path, sep="")	
-		pcrseqs.options <- paste(pcrseqs.options, "pcr.seqs(fasta=template.fasta", sep="")	
+		pcrseqs.options <- paste(pcrseqs.options, "pcr.seqs(fasta=template.fasta, keepdots=F", sep="")	
 	}
 
 	if (!is.na(start)){
@@ -65,12 +66,12 @@ if (!is.na(start) | !is.na(end)){
 	if (!is.na(end)){
 		pcrseqs.options <- paste(pcrseqs.options, ", end=", end, sep="")
 	}
-	if (keepdots=="yes"){
-		pcrseqs.options <- paste(pcrseqs.options, ", keepdots=F", sep="")
-	}
-	if (keepdots=="no"){
-		pcrseqs.options <- paste(pcrseqs.options, ", keepdots=T", sep="")
-	}
+#	if (keepdots=="yes"){
+#		pcrseqs.options <- paste(pcrseqs.options, ", keepdots=F", sep="")
+#	}
+#	if (keepdots=="no"){
+#		pcrseqs.options <- paste(pcrseqs.options, ", keepdots=T", sep="")
+#	}
 	pcrseqs.options <- paste(pcrseqs.options, ")", sep="")
 
 	# Write batch file
