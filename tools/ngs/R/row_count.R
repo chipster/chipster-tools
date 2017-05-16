9f9c2384af64ba754426895fcefd77918b85697f
aoikari <aoikari@qubit.x.csc.fi>
2017-05-16 12:25:35 +0300

    Add tool Row count

--- tools/ngs/R/row_count.R
+++ tools/ngs/R/row_count.R
@@ -1,8 +1,8 @@
-# TOOL axel_row_count_practice.R: "Row count" (Counts how many lines there is)
+# TOOL row_count.R: "Row count" (Counts how many lines there are)
 # INPUT input.txt: "Input file" TYPE GENERIC 
 # OUTPUT output.txt
 
 # AO 15.5.2017
 
-line<-system("wc -l input.txt")
+line<-system("wc -l input.txt", intern = TRUE)
 write(line, file = "output.txt")
