b7b50c7311bc2c63767b03e24953f241039107eb
aoikari <axel.oikari@csc.fi>
2017-05-30 08:12:57 +0300

    Change parameter name from header to rownames.

--- tools/common/R/transpose-matrix.R
+++ tools/common/R/transpose-matrix.R
@@ -1,9 +1,7 @@
 # TOOL transpose-matrix.R: "Transpose matrix" (Given a matrix X returns the transpose of X. Returns same file type)
 # INPUT input: input TYPE GENERIC
 # OUTPUT output: output
-# PARAMETER  header_text: "Header" TYPE [yes: Yes, no: No] DEFAULT yes (Is there a header in the input file)
-
-# PARAMETER OPTIONAL file_format: "File format" TYPE [tsv: tsv, csv: csv] DEFAULT tsv (In which format is the input file, currently only tsv is supported)
+# PARAMETER  header_text: "Does the first column have rownames" TYPE [yes: Yes, no: No] DEFAULT yes (Does the first column have rownames in the input file)
 
 #AO 22.5.2017
 
@@ -30,13 +28,7 @@ if( header_text == "yes") {
 }
 
 ## Load, transpose, and write back
-	
-#if(file_format == "tsv") {
-	matrix <- read.table(file = "input", sep = '\t', header = header.bool)
-	matrix.transposed <- t(matrix)
-	write.table(matrix.transposed, file = "output", sep = '\t', col.names = header.bool, row.names = header.bool)	
-#} else if(file_format == "csv") {
-	#matrix <- read.csv(input, HEADER = header.bool)
-	#matrix.transposed <- t(matrix)
-	#write.csv(matrix.transposed, file = "output", row.names=header.bool, col.names=header.bool)
-#}
+
+matrix <- read.table(file = "input", sep = '\t', header = header.bool)
+matrix.transposed <- t(matrix)
+write.table(matrix.transposed, file = "output", sep = '\t', col.names = header.bool, row.names = header.bool)	
