f9f0ff2ccdc7af5418c03d3b0cfc163b249f3dd8
aoikari <axel.oikari@csc.fi>
2017-06-01 09:21:50 +0300

    Add parameters

--- tools/ngs/R/hisat2.R
+++ tools/ngs/R/hisat2.R
@@ -2,7 +2,11 @@
 # INPUT read.fq: "Reads" TYPE GENERIC
 # OUTPUT OPTIONAL hisat.bam
 # OUTPUT OPTIONAL hisat.log
+# PARAMETER organism: "Genome" TYPE ["FILES genomes/indexes/hisat2 .fa"] DEFAULT "SYMLINK_TARGET genomes/indexes/hisat2/default .fa" (Genome or transcriptome that you would like to align your reads against.)
 # PARAMETER OPTIONAL quality.format: "Base quality encoding used" TYPE [sanger: "Sanger - Phred+33", phred64: "Phred+64"] DEFAULT sanger (Quality encoding used in the fastq file.)
+# PARAMETER OPTIONAL min.intron.length: "Minimum intron length" TYPE INTEGER FROM 1 TO 1000 DEFAULT 20 (Sets minimum intron length. Default: 20)
+# PARAMETER OPTIONAL max.intron.length: "Maximum intron length" TYPE INTEGER FROM 1 TO 1000000 DEFAULT 500000 (Sets maximum intron length. Default: 500000)
+# PARAMETER OPTIONAL library.type: "Library type" TYPE [fr-unstranded: fr-unstranded, fr-firststrand: fr-firststrand, fr-secondstrand: fr-secondstrand] DEFAULT fr-unstranded (Which library type to use. For directional\/strand specific library prepartion methods, choose fr-firststrand or fr-secondstrand depending on the preparation method: if the first read \(read1\) maps to the opposite, non-coding strand, choose fr-firststrand. If the first read maps to the coding strand, choose fr-secondstrand. For example for Illumina TruSeq Stranded sample prep, choose fr-firstsrand.)
 
 # AO 30.5.2017 First version
 
@@ -13,8 +17,6 @@
 # OUTPUT OPTIONAL junctions.bed
 # OUTPUT OPTIONAL hisat-summary.txt
 # PARAMETER OPTIONAL no.discordant: "Report only concordant alignments" TYPE [yes, no] DEFAULT yes (Report only concordant mappings.) 
-# PARAMETER organism: "Genome" TYPE ["FILES genomes/indexes/hisat2 .fa"] DEFAULT "SYMLINK_TARGET genomes/indexes/hisat2/default .fa" (Genome or transcriptome that you would like to align your reads against.)
-# PARAMETER OPTIONALlibrary.type: "Library type" TYPE [fr-unstranded: fr-unstranded, fr-firststrand: fr-firststrand, fr-secondstrand: fr-secondstrand] DEFAULT fr-unstranded (Which library type to use. For directional\/strand specific library prepartion methods, choose fr-firststrand or fr-secondstrand depending on the preparation method: if the first read \(read1\) maps to the opposite, non-coding strand, choose fr-firststrand. If the first read maps to the coding strand, choose fr-secondstrand. For example for Illumina TruSeq Stranded sample prep, choose fr-firstsrand.)
 # TODO: comment library.type away, unstranded is the default on hisat2
 # PARAMETER OPTIONAL mate.inner.distance: "Expected inner distance between mate pairs" TYPE INTEGER DEFAULT 200 (Expected mean inner distance between mate pairs. For example, if your fragment size is 300 bp and read length is 50 bp, the inner distance is 200.)
 # TODO: commonet inner.distance away? maxins is by default 500
@@ -25,8 +27,6 @@
 # PARAMETER OPTIONAL mismatches: "Number of mismatches allowed in final alignment" TYPE INTEGER FROM 0 TO 5 DEFAULT 2 (Final read alignments having more than this many mismatches are discarded.)
 # PARAMETER OPTIONAL min.anchor.length: "Minimum anchor length" TYPE INTEGER FROM 3 TO 1000 DEFAULT 8 (TopHat2 will report junctions spanned by reads with at least this many bases on each side of the junction. Note that individual spliced alignments may span a junction with fewer than this many bases on one side. However, every junction involved in spliced alignments is supported by at least one read with this many bases on each side.)
 # PARAMETER OPTIONAL splice.mismatches: "Maximum number of mismatches allowed in the anchor" TYPE INTEGER FROM 0 TO 2 DEFAULT 0 (The maximum number of mismatches that may appear in the anchor region of a spliced alignment.)
-# PARAMETER OPTIONAL min.intron.length: "Minimum intron length" TYPE INTEGER FROM 4 TO 1000 DEFAULT 70 (TopHat2 will ignore donor-acceptor pairs closer than this many bases apart.)
-# PARAMETER OPTIONAL max.intron.length: "Maximum intron length" TYPE INTEGER FROM 1 TO 1000000 DEFAULT 500000 (TopHat2 will ignore donor-acceptor pairs farther than this many bases apart, except when such a pair is supported by a split segment alignment of a long read.)
 # PARAMETER OPTIONAL no.mixed: "Report only paired alignments" TYPE [yes, no] DEFAULT yes (Only report read alignments if both reads in a pair can be mapped.)
 
 ## Source required functions
@@ -56,15 +56,52 @@ debug <- TRUE
 debugPrint("")
 debugPrint("DEBUG MODE IS ON")
 
-
-##Parse parameters and store them into hisat.parameters
+## Parse parameters and store them into hisat.parameters
+hisat.parameters <- ""
+# Reads to align
+hisat.parameters <-paste(hisat.parameters, "-U", "read.fq" )
+# Quality score
 if (quality.format=="phred64") {
-	quality <- "--phred64"
+	hisat.parameters <- paste(hisat.parameters, "--phred64")
 } else {
-	quality <- "--phred33"
+	hisat.parameters <- paste(hisat.parameters, "--phred33") 
 }
-hisat.parameters <- list(quality)
+# Intron length, defaluts are 20 and 500 000
+hisat.parameters <- paste(hisat.parameters, "--min-intronlen", min.intron.length)
+hisat.parameters <- paste(hisat.parameters, "--max-intronlen", max.intron.length)
+# Specify strand-specific information: the default is unstranded
+if (library.type == "fr-firststrand") {
+	hisat.parameters <- paste(hisat.parameters, "--rna-strandness F")
+} else if (library.type == "fr-secondstrand") {
+	hisat.parameters <- paste(hisat.parameters, "--rna-strandness R")	
+}
+# Organism
+# -x declares the basename of the index for reference genome
+hisat.parameters <- paste(hisat.parameters, "-x", organism)
+# Set environment variable that specifies what index to use, HISAT2 requires this
+Sys.setenv(HISAT2_INDEXES = "/opt/chipster/tools/genomes/indexes/hisat2")
 
+## Set hard coded parameters
+# Threads that hisat uses
+hisat.parameters <- paste(hisat.parameters, "-p" , chipster.threads.max)
+# Name of the output file
+hisat.parameters <- paste(hisat.parameters,  "-S", "hisat.sam")
+# Forward errors to hisat.log
+hisat.parameters <- paste(hisat.parameters,  "2>> hisat.log")
+
+#Print the HISAT2_INDEXES into debug
+debugPrint("")
+debugPrint("HISAT2_INDEXES:")
+debugPrint("$HISAT2_INDEXES")
+
+
+
+
+# Threads
+
+
+
+# Print parameters into log
 debugPrint(toString(hisat.parameters))
 
 # Get input name
@@ -77,15 +114,7 @@ unzipInputs(input.names)
 hisat.binary <- file.path(chipster.tools.path, "hisat2", "hisat2")
 samtools.binary <- file.path(chipster.tools.path, "samtools", "samtools")
 
-# TODO: this should be determined by paramter, there is a fancy way to determine the different possibilites for GUI
-organism <- "grch37"
-# Set environment variable HISAT2_INDEXES, HISAT2 requires this
-Sys.setenv(HISAT2_INDEXES = "/opt/chipster/tools/genomes/indexes/hisat2/grch37")
 
-#Print the HISAT2_INDEXES into debug
-debugPrint("")
-debugPrint("HISAT2_INDEXES:")
-debugPrint("$HISAT2_INDEXES")
 
 ## Run HISAT
 #Parameters:-p Launch NTHREADS parallel search threads (default: 1). Threads will run on separate processors/cores and synchronize when parsing reads 
@@ -104,22 +133,16 @@ debugPrint("$HISAT2_INDEXES")
 #				If - is specified, hisat2 gets the reads from the "standard in" or "stdin" filehandle.
 #			2>> Redirects error messages from stderr to spesified location.
 # Note a single ' at the beginning, it allows us to use special characters like >
-command <- paste("bash -c '", hisat.binary, "-p" , chipster.threads.max,"-x", organism, "-U", "read.fq" , "-S", "hisat.sam",  "2>> hisat.log")
-system(paste("echo '",command ,"' >> hisat.log"))
+command <- paste("bash -c '", hisat.binary)
 
+# Add the parameters
+command <- paste(command, hisat.parameters)
 
-#TODO: add a ' before and after the command
-# Add parameters to the command
-for(i in 1:length(hisat.parameters)) {
-	debugPrint("Parameter:")
-	debugPrint(toString(hisat.parameters[[i]]))
-	command <- paste(command, toString(hisat.parameters[[i]]))
-}
 # Close the command with a ', because there is a opening ' also
 command <- paste(command,"'") 
-
-# Print the command into hisat.log
+# Print the command to the hisat.log file
 system(paste("echo ",command ," >> hisat.log"))
+
 # Run command
 system(command)
 
@@ -140,3 +163,4 @@ Sys.unsetenv("HISAT2_INDEX")
 system("cat debug.log >> hisat.log")
 
 #EOF
+
