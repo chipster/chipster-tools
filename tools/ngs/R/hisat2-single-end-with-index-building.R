c33e5c7ca342b61e5382eeee53ceac9871b098d4
aoikari <axel.oikari@csc.fi>
2017-06-15 12:23:07 +0300

    Fix SADL syntax

--- tools/ngs/R/hisat2-single-end-with-index-building.R
+++ tools/ngs/R/hisat2-single-end-with-index-building.R
@@ -11,6 +11,7 @@
 # PARAMETER OPTIONAL library.type: "Library type" TYPE [fr-unstranded: fr-unstranded, fr-firststrand: fr-firststrand, fr-secondstrand: fr-secondstrand] DEFAULT fr-unstranded (Which library type to use. For directional\/strand specific library prepartion methods, choose fr-firststrand or fr-secondstrand depending on the preparation method: if the first read \(read1\) maps to the opposite, non-coding strand, choose fr-firststrand. If the first read maps to the coding strand, choose fr-secondstrand. For example for Illumina TruSeq Stranded sample prep, choose fr-firstsrand.)
 # PARAMETER OPTIONAL max.multihits: "How many hits is a read allowed to have" TYPE INTEGER FROM 1 TO 1000000 DEFAULT 5 (Instructs HISAT2 to allow up to this many alignments to the reference for a given read.)
 # PARAMETER OPTIONAL no.softclip: "Disallow soft-clipping" TYPE [nosoft: "No soft-clipping", yessoft: "Use soft-clipping"] DEFAULT yessoft (Is soft-cliping used. By default HISAT2 may soft-clip reads near their 5' and 3' ends.)
+
 # AO 30.5.2017 First version
 
 # INPUT OPTIONAL reads1.txt: "List of read 1 files" TYPE GENERIC
