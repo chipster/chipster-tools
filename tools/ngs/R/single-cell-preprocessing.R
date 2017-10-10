4a2279728364963df278dd0c4f72118d753fe679
mlehtiva <maria.lehtivaara@csc.fi>
2017-10-10 08:34:26 +0300

    Improve parameter and input naming in dropseq preprocessing

--- tools/ngs/R/single-cell-preprocessing.R
+++ tools/ngs/R/single-cell-preprocessing.R
@@ -1,19 +1,19 @@
 # TOOL single-cell-preprocessing.R: "Preprocessing DropSeq FASTQ files" (Transforms FASTQ to BAM, then tags the reads based on the cell barcodes using XC tag and molecular barcodes using XM tag. The rest of the read is discarded. XQ tag is added to each read to represent the number of bases that have quality scores below the base quality threshold. Filtering can be performed. Finally the BAM is transformed back to FASTQ format.)
-# INPUT input.fastq.gz: "FASTQ file, read1" TYPE GENERIC
-# INPUT OPTIONAL input2.fastq.gz: "FASTQ file, read2 " TYPE GENERIC
+# INPUT input.fastq.gz: "FASTQ file, read1, barcode read" TYPE GENERIC
+# INPUT OPTIONAL input2.fastq.gz: "FASTQ file, read2, sequence read " TYPE GENERIC
 # OUTPUT OPTIONAL unaligned_tagged.bam
 # OUTPUT OPTIONAL tagging_and_trimming_histograms.pdf 
 # OUTPUT OPTIONAL preprocessed.fq.gz
 # OUTPUT OPTIONAL tagging_and_trimming_summary.txt
 # PARAMETER base_range_cell: "Base range for cell barcode" TYPE STRING DEFAULT 1-12 (Which bases correspond to the cell barcode)
 # PARAMETER base_range_mol: "Base range for molecule barcode" TYPE STRING DEFAULT 13-20 (Which bases correspond to the molecule barcode)
-# PARAMETER OPTIONAL base_quality: "Base quality" TYPE INTEGER DEFAULT 10 (Mark in the XQ tag how many bases fall below this threshold.)
-# PARAMETER OPTIONAL sequence: "Adapter sequence" TYPE STRING DEFAULT AAGCAGTGGTATCAACGCAGAGTGAATGGG (Adapter sequence to trim off. As a default, SMART adapter sequence.)
-# PARAMETER OPTIONAL mismatches: "Mismatches in adapter" TYPE INTEGER DEFAULT 0 (How many mismatches allowed in the adapter sequence)
-# PARAMETER OPTIONAL num_bases: "Number of bases to check in adapter" TYPE INTEGER DEFAULT 5 (How many bases to check of the adapter sequence)
-# PARAMETER OPTIONAL mismatches_polyA: "Mismatches in polyA" TYPE INTEGER DEFAULT 0 (How many mismatches allowed in the polyA sequence)
-# PARAMETER OPTIONAL num_bases_polyA: "Number of bases to check in polyA" TYPE INTEGER DEFAULT 6 (How many bases to at least have to be A in the polyA tail)
-# PARAMETER OPTIONAL minlen: "Minimum length of reads to keep" TYPE INTEGER DEFAULT 50 (Drop the read if it is below a specified length. Trimmomatic tool is used for this step.)
+# PARAMETER OPTIONAL base_quality: "Barcode quality filtering\: Minimum base quality required from the adapter bases" TYPE INTEGER DEFAULT 10 (The tool marks in the XQ tag how many bases in the barcode fall below this threshold. Barcodes and reads with bad quality bases are filtered out.)
+# PARAMETER OPTIONAL sequence: "Adapter trimming\: sequence" TYPE STRING DEFAULT AAGCAGTGGTATCAACGCAGAGTGAATGGG (Adapter sequence to trim off. As a default, SMART adapter sequence.)
+# PARAMETER OPTIONAL mismatches: "Adapter trimming\: Mismatches allowed in the adapter sequence" TYPE INTEGER DEFAULT 0 (How many mismatches allowed in the adapter sequence)
+# PARAMETER OPTIONAL num_bases: "Adapter trimming\: Number of bases to check in adapter" TYPE INTEGER DEFAULT 5 (How many bases to check of the adapter sequence)
+# PARAMETER OPTIONAL mismatches_polyA: "PolyA trimming\: Mismatches allowed in the polyA sequence" TYPE INTEGER DEFAULT 0 (How many mismatches allowed in the polyA sequence)
+# PARAMETER OPTIONAL num_bases_polyA: "PolyA trimming\: Number of bases to check in polyA" TYPE INTEGER DEFAULT 6 (How many bases at least have to be A in the polyA tail)
+# PARAMETER OPTIONAL minlen: "Minimum length filtering\: Minimum length of sequence reads to keep" TYPE INTEGER DEFAULT 50 (Drop the sequence read if it is below a specified length -the barcodes reads are not considered here. Trimmomatic tool is used for this step.)
 
 # PARAMETER OPTIONAL discard_read: "Discard read" TYPE [True, False] DEFAULT False (Discard the read)
 # PARAMETER OPTIONAL tag_name_cell: "Tag name for cell" TYPE [XC:XC, XM:XM] DEFAULT XC (Which BAM tag to use for the barcodes)
