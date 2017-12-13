e5d42af3e94869bb40225c808121b40ed8f94a29
klemela <klemela>
2017-12-13 09:43:27 +0200

    Test that missing optional outputs aren't created

--- tools/misc/python/test-data-in-out.py
+++ tools/misc/python/test-data-in-out.py
@@ -1,6 +1,7 @@
 # TOOL test-data-in-out.py: "Test data input and output in Python" (Data input output test.) 
 # INPUT input TYPE GENERIC
 # OUTPUT output
+# OUTPUT OPTIONAL missing_output.txt
 
 import shutil
 
