e127fba560f802db23cdc71fdd01180124ae6041
kkmattil <kkmattil@punatulkku.x.csc.fi>
2018-05-02 13:14:37 +0300

    Minima2 manual and tool updated.

--- tools/ngs/R/minimap2.R
+++ tools/ngs/R/minimap2.R
@@ -1,15 +1,16 @@
-# TOOL minimap2.R: "Minimap2 for mapping single reads and own genome" (Aligns reads to genomes using the minimap2 algorithm. If just one read file is given, then a single end analysis is run. The input files can be in FASTQ or FASTA format. Results are sorted and indexed BAM files, which are ready for viewing in the Chipster genome browser. 
-# Note that this minimap2 tool requires that you have imported the reference genome to Chipster in fasta format.)
+# TOOL minimap2.R: "Minimap2 for mapping reads to genomes" (This tool aligns reads to genomes using the Minimap2 algorithm. Minimap2 is deverloped especially for long reads. If just one input file is given, then the tool assumes that it contains the reads to be aligned and that the refreces genome is selected with the Genome parameter. If two input files are given the other one will be used as the file contang query sequences and the other as containg the reference sequeces.  The input reads file can be in FASTQ or FASTA format. Reference genome should be in fasta format.. Results are sorted and indexed BAM files. .)
 # INPUT reads.fq : "Reads" TYPE GENERIC
-# INPUT genome.fasta: "Reference genome" TYPE GENERIC
+# INPUT OPTIONAL genome.fasta: "Reference genome" TYPE GENERIC
 # OUTPUT OPTIONAL minimap2.bam 
 # OUTPUT OPTIONAL minimap2.bam.bai 
-# OUTPUT minimap2.log 
-# PARAMETER OPTIONAL task: "Task type" TYPE [ map-pb: "Map PacBio subreads to a genome", map-ont: "Map Oxford nanopore reads to a genome", splice_2: "Map PacBio Iso-seq or traditional cDNA to reference", splice: "Map Nanopore 2D cDNA-seq data to refrence",  splice_3: "Map Nanopore Direct RNA-seq to refrence",  splice_4: "Mapping against SIRV control reference", asm5: "Aligning assebly to reference genome" ] DEFAULT none (Mapping or aligment task to be performed)
+# OUTPUT OPTIONAL minimap2.log 
+# PARAMETER OPTIONAL chipster_genome: "Genome" TYPE ["FILES genomes/fasta .fa"] DEFAULT "SYMLINK_TARGET genomes/indexes/bowtie2/default .fa" (Genome that you would like to align your reads against. This parameter is ignored if you provide yout own refrence genome as the second input file.)
+# PARAMETER OPTIONAL task: "Task type" TYPE [ map-pb: "Map PacBio subreads to a genome", map-ont: "Map Oxford nanopore reads to a genome", splice_2: "Map PacBio Iso-seq or traditional cDNA to reference", splice: "Map Nanopore 2D cDNA-seq data to reference",  splice_3: "Map Nanopore Direct RNA-seq to reference",  splice_4: "Mapping against SIRV control reference", asm5: "Aligning assembly to reference genome" ] DEFAULT none (Mapping or aligment task to be performed)
 # PARAMETER OPTIONAL rgid: "Read group identifier" TYPE STRING (Read group identifier. If you want to add the read group line in the BAM file, you have to give this information.)
 # PARAMETER OPTIONAL rgsm: "Sample name for read group" TYPE STRING (The name of the sample sequenced in this read group. Note that you have to fill in also the read group identifier parameter for the read group information to appear in the BAM file.)
 # PARAMETER OPTIONAL rgpl: "Platform for read group" TYPE [ none: "Not defined", ILLUMINA, SOLID, LS454, HELICOS, PACBIO] DEFAULT none (Platform\/technology used to produce the read. Note that you have to fill in also the read group identifier parameter for the read group information to appear in the BAM file.)
 # PARAMETER OPTIONAL rglb: "Library identifier for read group" TYPE STRING (DNA preparation library identifier. The Mark Duplicates tool uses this field to determine which read groups might contain molecular duplicates, in case the same DNA library was sequenced on multiple lanes. Note that you have to fill in also the read group identifier parameter for the read group information to appear in the BAM file.)
+# PARAMETER OPTIONAL save_log: "Collect a log file about the Mimimap run" TYPE [yes: Yes, no: No] DEFAULT yes (Collect a log file about the Mimimap2 run.)
 
 # KM 6.3.2018
 
@@ -24,6 +25,7 @@ source(file.path(chipster.common.path, "bam-utils.R"))
 # check out if the file is compressed and if so unzip it
 # above step is not needed as minimap 2 does this automatically 
 
+
 # minimap2
 #minimap2.binary <- file.path(chipster.tools.path, "minimap2", "minimap2")
 minimap2.binary <- file.path("/opt/chipster/tools_local/minimap2-2.9_x64-linux/minimap2")
@@ -37,6 +39,16 @@ if ( task.test == "XX" ){
 }
 
 
+#copy genome file if needed
+if(!file.exists("genome.fasta")){
+	fasta.genome.path <- c(file.path(chipster.tools.path, "genomes", "fasta"))
+	genomecopy.command <- paste("cp ",fasta.genome.path, "/",chipster_genome ,".fa  genome.fasta", sep="" )
+	echo.command <- paste("echo Using genome ",fasta.genome.path, "/",chipster_genome ,".fa >> minimap2.log ", sep=""  )
+	system(echo.command)
+	system(genomecopy.command)
+	
+}
+
 command.start <-(minimap2.binary)
 
 if ( task == "splice_2"){
@@ -48,7 +60,7 @@ if ( task == "splice_3"){
 }
 
 if ( task == "splice_4"){
-	task <- ("splice -uf -k14")
+	task <- ("splice --splice-flank=no")
 }
 
 minimap2.parameters <- paste("-ax", task )
@@ -83,15 +95,16 @@ if ( nchar(rgid) < 1 ){
 	}
 }
 
-
+genome.test <- paste("X", task, "X", sep="")
+if ( task.test == "XX" ){
+	stop("CHIPSTER-NOTE: Please define the aligment task type.")
+}
 
 command.end <- paste("genome.fasta reads.fq 1> alignment.sam 2>> minimap2.log")
-
-
 # run minimap2 alignment
 minimap2.command <- paste(command.start, minimap2.parameters, command.end)
 
-echo.command <- paste("echo '", minimap2.command, "' > minimap2.log")
+echo.command <- paste("echo '", minimap2.command, "' >> minimap2.log")
 system(echo.command)
 system(minimap2.command)
 
@@ -125,6 +138,12 @@ system(paste(samtools.binary, "index alignment.sorted.bam"))
 system("mv alignment.sorted.bam minimap2.bam")
 system("mv alignment.sorted.bam.bai minimap2.bam.bai")
 system("ls -l >> minimap2.log")
+
+
+if ( save_log == "no") {
+	system ("rm -f minimap2.log")
+}
+
 
 # Substitute display names to log for clarity
 #displayNamesToFile("minimap2.log")
