20c77152d1b81fd3ab42d9e563bb7170ef96906c
klemela <klemela>
2018-06-28 12:08:01 +0300

    Make output similar to Define NGS experiment tool

--- tools/misc/python/test-phenodata-output.py
+++ tools/misc/python/test-phenodata-output.py
@@ -1,12 +1,14 @@
-# TOOL test-phenodata-output.py: "Test phenodata output in Python" () 
+# TOOL test-phenodata-output.py: "Test phenodata output in Python" ()
+# INPUT input{...}.tsv TYPE GENERIC
 # OUTPUT phenodata.tsv
 # OUTPUT output.tsv
 
 with open('output.tsv', 'w') as f:
-	f.write('test	output\n')
+	f.write('identifier	chip.sample1\n')
 	f.write('test	output\n')
 
 with open('phenodata.tsv', 'w') as f:
-	f.write('sample	original_name	chiptype	group\n')
-	f.write('microarray001.cel	cancerGSM11814.cel	hgu133ahsentrezg.db	1\n')
-	f.write('microarray002.cel	cancerGSM11830.cel	hgu133ahsentrezg.db	2\n')
+	f.write('dataset	column	sample	chiptype	experiment	group	library_size\n')
+	f.write('ngs-data-table.tsv	chip.sample000.tsv	sample000.tsv	not applicable	rna_seq		\n')
+	f.write('ngs-data-table.tsv	chip.sample001.tsv	sample001.tsv	not applicable	rna_seq		\n')
+	
