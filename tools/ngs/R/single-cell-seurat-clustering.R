8c9f0ce07062b605357ccf445c592d2107acf419
mlehtiva <maria.lehtivaara@csc.fi>
2018-09-27 10:36:13 +0300

    Update Seurat PCA and clustering tools for dataset with few cells
    Add parameters: number of PCs to compute and perplexity.

--- tools/ngs/R/single-cell-seurat-clustering.R
+++ tools/ngs/R/single-cell-seurat-clustering.R
@@ -6,6 +6,7 @@
 # OUTPUT OPTIONAL markers.tsv
 # PARAMETER OPTIONAL pcs_use: "Number of principal components to use" TYPE INTEGER DEFAULT 10 (How many principal components to use. User must define this based on the PCA-elbow and PCA plots from the setup tool.)
 # PARAMETER OPTIONAL res: "Resolution for granularity" TYPE DECIMAL DEFAULT 0.6 (Resolution parameter that sets the granularity of the clustering. Increased values lead to greater number of clusters. Values between 0.6-1.2 return good results for single cell datasets of around 3K cells. For larger data sets, try higher resolution.)
+# PARAMETER OPTIONAL perplex: "Perplexity, expected number of neighbors" TYPE INTEGER DEFAULT 30 (Perplexity, expected number of neighbors. Default 30. Set to lower number if you have very few cells.)
 # PARAMETER OPTIONAL minpct: "Min fraction of cells where a cluster marker gene is expressed" TYPE DECIMAL DEFAULT 0.25 (Test only genes which are detected in at least this fraction of cells in either of the two populations. Meant to speed up the function by not testing genes that are very infrequently expression)
 # PARAMETER OPTIONAL threshuse: "Differential expression threshold for a cluster marker gene" TYPE DECIMAL DEFAULT 0.25 (Limit testing to genes which show, on average, at least X-fold difference, in log-scale, between the two groups of cells. Increasing thresh.use speeds up the function, but can miss weaker signals.)
 # PARAMETER OPTIONAL test.type: "Which test to use for finding marker genes" TYPE [bimod, roc, t, tobit, poisson, negbinom] DEFAULT bimod (Denotes which test to use. Seurat currently implements \"bimod\" \(likelihood-ratio test for single cell gene expression, McDavid et al., Bioinformatics, 2011, default\), \"roc\" \(standard AUC classifier\), \"t\" \(Students t-test\), and \"tobit\" \(Tobit-test for differential gene expression, as in Trapnell et al., Nature Biotech, 2014\), \"poisson\", and \"negbinom\". The latter two options should only be used on UMI datasets, and assume an underlying poisson or negative-binomial distribution.)
@@ -20,6 +21,7 @@
 # 09.06.2017 ML
 # 2018-01-11 ML update Seurat version to 2.2.0
 # 2018-07-25 ML removed onlypos -parameter
+# 2018-09-26 ML add perplexity parameter for datasets with fewer cells
 
 library(Seurat)
 library(dplyr)
@@ -41,7 +43,7 @@ seurat_obj <- FindClusters(object = seurat_obj, reduction.type = "pca", dims.use
 
 
 # Non-linear dimensional reduction (tSNE)
-seurat_obj <- RunTSNE(seurat_obj, dims.use=1:pcs_use, do.fast=T)
+seurat_obj <- RunTSNE(seurat_obj, dims.use=1:pcs_use, do.fast=T, perplexity=perplex)
 pdf(file="tSNEplot.pdf") 
 TSNEPlot(seurat_obj)
 dev.off() # close the pdf
--- tools/ngs/R/single-cell-seurat-pca.R
+++ tools/ngs/R/single-cell-seurat-pca.R
@@ -2,6 +2,7 @@
 # INPUT OPTIONAL seurat_obj.Robj: "Seurat object" TYPE GENERIC
 # OUTPUT OPTIONAL PCAplots.pdf
 # OUTPUT OPTIONAL seurat_obj_2.Robj
+# PARAMETER OPTIONAL num.of.pcas: "Number of PCs to compute" TYPE INTEGER DEFAULT 20 (How many principal components to compute and store. If you get an error message, try lowering the number. This might happen especially if you have low number of cells in your data.)
 # PARAMETER OPTIONAL num.of.heatmaps: "Number of principal components to plot as heatmaps" TYPE INTEGER DEFAULT 12 (How many principal components to plot as heatmaps.)
 # RUNTIME R-3.4.3
 
@@ -12,6 +13,7 @@
 # 2017-06-06 ML
 # 2017-07-05 ML split into separate tool
 # 2018-01-11 ML update Seurat version to 2.2.0
+# 2018-09-26 ML add num.of.pcas parameter for datasets with fewer cells
 
 
 library(Seurat)
@@ -25,11 +27,12 @@ load("seurat_obj.Robj")
 
 # PCA
 # The variable genes = genes in seurat_object@var.genes are used as input
-seurat_obj <- RunPCA(object = seurat_obj, pc.genes = seurat_obj@var.genes, do.print = TRUE, pcs.print = 5, genes.print = 5)
+seurat_obj <- RunPCA(object = seurat_obj, pc.genes = seurat_obj@var.genes, pcs.compute = num.of.pcas, do.print = TRUE, pcs.print = 5, genes.print = 5)
 
 #PCA genes in txt file
 sink("PCAgenes.txt")
-PrintPCA(seurat_obj, pcs.print = 1:5, genes.print = 5, use.full = TRUE)
+#PrintPCA(seurat_obj, pcs.print = 1:5, genes.print = 5, use.full = TRUE)
+PrintPCA(seurat_obj, pcs.print = 1:5, genes.print = 5, use.full = FALSE)
 sink()
 # PDF
 pdf(file="PCAplots.pdf", , width=9, height=12) 
@@ -38,7 +41,8 @@ PCAPlot(seurat_obj, 1, 2)
 
 # Need to check the number of cells at this point.
 # cells_left <- seurat_obj@data@Dim[2] 
-cells_left <- dim(seurat_obj@raw.data)[2]
+#cells_left <- dim(seurat_obj@raw.data)[2]
+cells_left <- length(seurat_obj@cell.names)
 if (cells_left > 500) {
 	PCHeatmap(seurat_obj, pc.use = 1, cells.use = 100, do.balanced = TRUE)
 	PCHeatmap(seurat_obj, pc.use = 1:num.of.heatmaps, cells.use = 500, do.balanced = TRUE, label.columns = FALSE, use.full = FALSE)
