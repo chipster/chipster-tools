cbb16d9943f210f498282a44faee58f6607f5fed
Taavi Hupponen <taavi.hupponen@csc.fi>
2019-04-03 12:26:29 +0300

    Add sleep with python test tool

---
+++ tools/misc/python/sleep.py
@@ -0,0 +1,8 @@
+# TOOL sleep.py: "Sleep with Python" ()
+# PARAMETER delay: Delay TYPE INTEGER FROM 0 TO 1000 DEFAULT 10 (Delay in seconds)
+
+import time
+
+print("Start : %s" % time.ctime())
+time.sleep(delay)
+print("End : %s" % time.ctime())
--- tools/misc/python/test-phenodata-output.py
+++ tools/misc/python/test-phenodata-output.py
@@ -1,14 +1,12 @@
 # TOOL test-phenodata-output.py: "Test phenodata output in Python" ()
 # INPUT input{...}.tsv TYPE GENERIC
-# OUTPUT phenodata.tsv
 # OUTPUT output.tsv
+# OUTPUT META phenodata.tsv
 
 with open('output.tsv', 'w') as f:
 	f.write('identifier	chip.sample1\n')
 	f.write('test	output\n')
 
 with open('phenodata.tsv', 'w') as f:
-	f.write('dataset	column	sample	chiptype	experiment	group	library_size\n')
-	f.write('ngs-data-table.tsv	chip.sample000.tsv	sample000.tsv	not applicable	rna_seq		\n')
-	f.write('ngs-data-table.tsv	chip.sample001.tsv	sample001.tsv	not applicable	rna_seq		\n')
-	
+	f.write('sample	chiptype	group\n')
+	f.write('sample001.tsv	chiptype	1\n')
