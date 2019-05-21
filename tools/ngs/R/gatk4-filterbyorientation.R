5df4831f6b840cbc4a2faafc98ac6eed8b485d7e
amsaren <ari-matti.saren@csc.fi>
2019-05-21 17:59:42 +0300

    Added summary file to outputs. Changed output names.

--- tools/ngs/R/gatk4-filterbyorientation.R
+++ tools/ngs/R/gatk4-filterbyorientation.R
@@ -1,7 +1,8 @@
 # TOOL gatk4-filterbyorientation.R: "GATK4 -Filter Mutect2 calls by orientation bias" (Additionally filter Mutect2 somatic variant calls for sequence context-dependent artifacts, e.g. OxoG or FFPE deamination. Tool is based on GATK4 FilterByOrientationBias.)
 # INPUT input.vcf: "Mutect calls" TYPE GENERIC
 # INPUT metrics_file: "Detail metrics file" TYPE GENERIC
-# OUTPUT OPTIONAL Mutect2.filtered.unbiased.vcf
+# OUTPUT OPTIONAL filtered.vcf
+# OUTPUT OPTIONAL filtered.vcf.summary
 # OUTPUT OPTIONAL gatk_log.txt
 # PARAMETER OPTIONAL gatk.artifactmodes: "Artifacts on the forward strand" TYPE STRING (PreAdapter Detail artifacts of interest on the forward strand. Format CtoA for a single artifact. If you want to give multiple artifacts, use comma to separate them:  CtoA,TtoG. Artifacts must be one base to one base \(e.g. 'CCtoCA' is illegal\). G>T is OxoG.)
 
@@ -17,7 +18,7 @@ formatGatkVcf("input.vcf")
 gatk.binary <- c(file.path(chipster.tools.path, "GATK4", "gatk"))
 
 # Run GetPileupSummaries
-command <- paste(gatk.binary, "FilterByOrientationBias", "-O Mutect2.filtered.unbiased.vcf", "-V input.vcf.gz", "-P metrics_file")
+command <- paste(gatk.binary, "FilterByOrientationBias", "-O filtered.vcf", "-V input.vcf.gz", "-P metrics_file")
 
 if (nchar(gatk.artifactmodes) > 0 ){
 	#Split into individual entries
@@ -32,7 +33,7 @@ if (nchar(gatk.artifactmodes) > 0 ){
 runExternal(command)
 
 # Return error message if no result
-if (fileNotOk("Mutect2.filtered.unbiased.vcf")){
+if (fileNotOk("filtered.vcf")){
 	system("mv stderr.log gatk_log.txt")
 }
 
@@ -42,9 +43,9 @@ inputnames <- read_input_definitions()
 basename <- strip_name(inputnames$input.vcf)
 
 # Make a matrix of output names
-outputnames <- matrix(NA, nrow=1, ncol=2)
-outputnames[1,] <- c("Mutect2.filtered.unbiased.vcf", paste(basename, "_filtered_unbiased.vcf", sep=""))
-
+outputnames <- matrix(NA, nrow=2, ncol=2)
+outputnames[1,] <- c("filtered.vcf", paste(basename, "_filtered_unbiased.vcf", sep=""))
+outputnames[2,] <- c("filtered.vcf.summary", paste(basename, "_filtered_unbiased_summary.txt", sep=""))
 
 # Write output definitions file
 write_output_definitions(outputnames)
