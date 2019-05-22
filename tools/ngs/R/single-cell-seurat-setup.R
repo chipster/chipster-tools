0deb2e9445a5e5d308e30a1f9226786cecd5f7a2
mlehtiva <maria.lehtivaara@csc.fi>
2019-05-22 16:21:47 +0300

    Setup tool to v3 -comments left in the code to show the changes

--- tools/ngs/R/single-cell-seurat-setup.R
+++ tools/ngs/R/single-cell-seurat-setup.R
@@ -7,7 +7,6 @@
 # OUTPUT OPTIONAL seurat_obj.Robj
 # PARAMETER OPTIONAL project.name: "Project name for plotting" TYPE STRING DEFAULT Project_name (You can give your project a name. The name will appear on the plots. Do not use underscore _ in the names!)
 # PARAMETER OPTIONAL mincells: "Keep genes which are expressed in at least this many cells" TYPE INTEGER DEFAULT 3 (The genes need to be expressed in at least this many cells.)
-# PARAMETER OPTIONAL mingenes: "Keep cells which express at least this many genes" TYPE INTEGER DEFAULT 200 (The cells need to have expressed at least this many genes.)
 # PARAMETER OPTIONAL groupident: "Sample or group name" TYPE STRING DEFAULT empty (Type the group or sample name or identifier here. For example CTRL, STIM, TREAT. Do not use underscore _ in the names!)
 # RUNTIME R-3.4.3
 
@@ -16,6 +15,10 @@
 # 2017-07-05 ML split into separate tool
 # 2018-01-11 ML update Seurat version to 2.2.0
 # 2018-04-24 ML + AMS improve the input tar handling
+
+# Parameter removed from new R-version: "This functionality has been removed to simplify the initialization process/assumptions. If you would still like to impose this threshold for your particular dataset, simply filter the input expression matrix before calling this function."
+# PARAMETER OPTIONAL mingenes: "Keep cells which express at least this many genes" TYPE INTEGER DEFAULT 200 (The cells need to have expressed at least this many genes.)
+
 
 library(Seurat)
 library(dplyr)
@@ -50,9 +53,8 @@ if (file.exists("dropseq.tsv")){
 }
 
 # Initialize the Seurat object
-seurat_obj <- CreateSeuratObject(raw.data = dat, min.cells = mincells, min.genes = mingenes, 
-		project = project.name)
-
+seurat_obj <- CreateSeuratObject(counts = dat, min.cells = mincells, project = project.name)
+# v3: raw.data -> counts
 
 # For sample detection later on
 if (groupident != "empty") {
@@ -62,21 +64,33 @@ if (groupident != "empty") {
 
 # QC
 # % of mito genes (note: they are named either "MT-CO1" or "mt-Co1", have to check both)
-mito.genes <- grep(pattern ="^MT-", x = rownames(x =seurat_obj@data), value = T, ignore.case=T)
+# mito.genes <- grep(pattern ="^MT-", x = rownames(x =seurat_obj@data), value = T, ignore.case=T)
 #percent.mito <- colSums(expm1(seurat_obj@data[mito.genes, ]))/colSums(expm1(seurat_obj@data))
-percent.mito <- Matrix::colSums(seurat_obj@raw.data[mito.genes, ])/Matrix::colSums(seurat_obj@raw.data)
-seurat_obj <- AddMetaData(object = seurat_obj, metadata = percent.mito, col.name = "percent.mito")
+# percent.mito <- Matrix::colSums(seurat_obj@raw.data[mito.genes, ])/Matrix::colSums(seurat_obj@raw.data)
+# seurat_obj <- AddMetaData(object = seurat_obj, metadata = percent.mito, col.name = "percent.mito")
+# v3: new way to add metadata, [[ ]] operator
+seurat_obj[["percent.mt"]] <- PercentageFeatureSet(seurat_obj, pattern = "^MT-")
+
 # pdf plots
 pdf(file="QCplots.pdf", , width=13, height=7) 
-VlnPlot(seurat_obj, c("nGene", "nUMI", "percent.mito"), nCol = 3) 
-par(mfrow = c(1, 2))
-GenePlot(seurat_obj, "nUMI", "percent.mito")
-GenePlot(seurat_obj, "nUMI", "nGene")
+# VlnPlot(seurat_obj, c("nGene", "nUMI", "percent.mito"), nCol = 3) 
+# v3: new names for the features
+VlnPlot(seurat_obj, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+
+# par(mfrow = c(1, 2))
+# GenePlot(seurat_obj, "nUMI", "percent.mito")
+# GenePlot(seurat_obj, "nUMI", "nGene")
+# v3: new function FeatureScatter
+plot1 <- FeatureScatter(seurat_obj, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(seurat_obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+CombinePlots(plots = list(plot1, plot2))
 
 # Number of cells:
-textplot(paste("\v \v Number of \n \v \v cells: \n \v \v", length(seurat_obj@cell.names)), halign="center", valign="center", cex=2)
+# textplot(paste("\v \v Number of \n \v \v cells: \n \v \v", length(seurat_obj@cell.names)), halign="center", valign="center", cex=2)
+# v3: object@cell.names -> colnames(x = object) 
+textplot(paste("\v \v Number of \n \v \v cells: \n \v \v", length(colnames(x=seurat_obj))), halign="center", valign="center", cex=2)
 
-#dev.off() # close the pdf
+dev.off() # close the pdf
 
 
 # Save the Robj for the next tool
