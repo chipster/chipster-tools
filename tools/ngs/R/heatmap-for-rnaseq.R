11a72e089185c60a0c03cdedf4715af1f0da8ab1
mlehtiva <maria.lehtivaara@csc.fi>
2019-05-22 16:24:17 +0300

    Improve description, comment the better heatmap version for now

--- tools/ngs/R/heatmap-for-rnaseq.R
+++ tools/ngs/R/heatmap-for-rnaseq.R
@@ -1,4 +1,4 @@
-# TOOL heatmap-for-rnaseq.R: "Heatmap for RNA-seq results" (Annotated heatmap for RNA-seq results.)
+# TOOL heatmap-for-rnaseq.R: "Heatmap for RNA-seq results" (Draw a heatmap and dendrogram of the genes of interest. As input, give the list of genes of interest and the RAW COUNTS. The tool will first perform a transformation for the values.)
 # INPUT de-list.tsv: "List of differentially expressed genes" TYPE GENERIC
 # INPUT data.tsv: "Raw counts" TYPE GENERIC
 # INPUT META phenodata.tsv: "Phenodata file" TYPE GENERIC 
@@ -51,11 +51,13 @@ write.table(merged_part, "vst-transformed-counts.tsv", sep="\t", row.names=T, co
 
 col_from<-phenodata[,pmatch(column,colnames(phenodata))] # select the column from phenodata for plotting sample names
 pdf(file="heatmap.pdf", width=image.width/72, height=image.height/72)
+
+# Change this to pheatmap when the package is production
 heatmap(data.matrix(dat2), labCol = col_from, col = topo.colors(20)) 
 
 # nicer version, but needs install.packages("pheatmap")
- library("pheatmap")
- pheatmap(data.matrix(dat2), labels_col = col_from, cellwidth=image.width/30 , cellheight=image.height/30)
+# library("pheatmap")
+# pheatmap(data.matrix(dat2), labels_col = col_from, cellwidth=image.width/30 , cellheight=image.height/30)
  dev.off()
 
 # EOF
