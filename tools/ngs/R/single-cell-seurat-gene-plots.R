68b01b676fd334044eaf3520f8919d8e4218a51d
mlehtiva <maria.lehtivaara@csc.fi>
2019-06-13 10:21:59 +0300

    Update Visualise biomarkers tool to Seurat v3, add option for many genes

--- tools/ngs/R/single-cell-seurat-gene-plots.R
+++ tools/ngs/R/single-cell-seurat-gene-plots.R
@@ -3,12 +3,14 @@
 # OUTPUT OPTIONAL log.txt
 # OUTPUT OPTIONAL seurat_obj_2.Robj
 # OUTPUT OPTIONAL biomarker_plot.pdf
-# PARAMETER biomarker: "Gene name" TYPE STRING DEFAULT MS4A1 (Name of the biomarker gene to plot.)
+# PARAMETER biomarker: "Gene name\(s\)" TYPE STRING DEFAULT MS4A1 (Name\(s\) of the biomarker gene to plot. If you list multiple gene names, use comma \(,\) as separator.)
+# PARAMETER point.size: "Point size in tSNE plot" TYPE INTEGER DEFAULT 1 (Size of the points in tSNE plot.)
 # RUNTIME R-3.4.3
 
 
 # 13.06.2017 ML
 # 2018-01-11 ML update Seurat version to 2.2.0
+# 2019-06-13 ML Seurat v3
 
 library(Seurat)
 library(dplyr)
@@ -18,9 +20,19 @@ library(gplots)
 # Load the R-Seurat-object (called seurat_obj)
 load("seurat_obj.Robj")
 
+# If multiple genes are listed: (separate words from "," and remove whitespace)
+if(grep(",", biomarker) != 0) {
+   biomarker <- trimws(unlist(strsplit(biomarker, ",")))
+}
+
 # Violin plot:
 pdf(file="biomarker_plot.pdf") 
-VlnPlot(object = seurat_obj, features.plot = biomarker)
-FeaturePlot(object = seurat_obj, features.plot =biomarker, cols.use=c("grey", "blue"), pt.size=5, reduction.use = "tsne")
+# v2:
+# VlnPlot(object = seurat_obj, features.plot = biomarker)
+VlnPlot(seurat_obj, features = biomarker)
+# v2:
+# FeaturePlot(object = seurat_obj, features.plot =biomarker, cols.use=c("grey", "blue"), pt.size=5, reduction.use = "tsne")
+FeaturePlot(seurat_obj, features = biomarker, pt.size=point.size) 
 dev.off() # close the pdf
 
+# EOF
