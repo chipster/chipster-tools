a40b042c3a3bf0c594bffa4f2d696188eecf1292
ekorpela <ekorpela@EKORPELA-840G3.windows.csc.fi>
2019-06-15 08:18:18 +0300

    Change tool name

--- tools/ngs/R/single-cell-seurat-clustering.R
+++ tools/ngs/R/single-cell-seurat-clustering.R
@@ -1,4 +1,4 @@
-# TOOL single-cell-seurat-clustering.R: "Seurat -Clustering" (Clusters cells, performs non-linear dimensional reduction tSNE for visualization purposes, and finds marker genes for the clusters.)
+# TOOL single-cell-seurat-clustering.R: "Seurat -Clustering and detection of cluster marker genes" (Clusters cells, performs non-linear dimensional reduction tSNE for visualization purposes, and finds marker genes for the clusters.)
 # INPUT seurat_obj.Robj: "Seurat object from the Setup tool" TYPE GENERIC
 # OUTPUT OPTIONAL log.txt
 # OUTPUT OPTIONAL tSNEplot.pdf
@@ -9,7 +9,7 @@
 # PARAMETER OPTIONAL perplex: "Perplexity, expected number of neighbors for tSNE plot" TYPE INTEGER DEFAULT 30 (Perplexity, expected number of neighbors. Default 30. Set to lower number if you have very few cells. Used for the tSNE visualisation of the clusters.)
 # PARAMETER OPTIONAL point.size: "Point size in tSNE plot" TYPE DECIMAL DEFAULT 1 (Point size for tSNE plot.)
 # PARAMETER OPTIONAL minpct: "Min fraction of cells where a cluster marker gene is expressed" TYPE DECIMAL DEFAULT 0.25 (Test only genes which are detected in at least this fraction of cells in either of the two populations. Meant to speed up the function by not testing genes that are very infrequently expression)
-# PARAMETER OPTIONAL threshuse: "Differential expression threshold for a cluster marker gene" TYPE DECIMAL DEFAULT 0.25 (Limit testing to genes which show, on average, at least X-fold difference, in log-scale, between the two groups of cells. Increasing thresh.use speeds up the function, but can miss weaker signals.)
+# PARAMETER OPTIONAL threshuse: "Differential expression threshold for a cluster marker gene" TYPE DECIMAL DEFAULT 0.25 (Limit testing to genes which show, on average, at least X-fold difference, in log2 scale, between the two groups of cells. Increasing thresh.use speeds up the function, but can miss weaker signals.)
 # PARAMETER OPTIONAL test.type: "Which test to use for finding marker genes" TYPE [wilcox, bimod, roc, t, tobit, poisson, negbinom, MAST, DESeq2] DEFAULT wilcox (Denotes which test to use. Seurat currently implements \"wilcox\" \(Wilcoxon rank sum test, default\), \"bimod\" \(likelihood-ratio test for single cell gene expression\), \"roc\" \(standard AUC classifier\), \"t\" \(Students t-test\), \"tobit\" \(Tobit-test for differential gene expression\), \"MAST\" \(GLM-framework that treates cellular detection rate as a covariate\), \"DESeq2\" \(DE based on a model using the negative binomial distribution\), \"poisson\", and \"negbinom\". The latter two options should be used on UMI datasets only, and assume an underlying poisson or negative-binomial distribution.)
 # RUNTIME R-3.4.3
 
@@ -21,6 +21,7 @@
 # 2018-09-26 ML add perplexity parameter for datasets with fewer cells
 # 2019-02-18 ML add heatmap plotting & number of cells in each cluster
 # 2019-04-10 ML add more test.types (wilcox, MAST, DESeq2), wilcox changed as the default
+# 2019-06-15 EK changed name to include marker gene detection
 
 library(Seurat)
 library(dplyr)
