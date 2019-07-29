a2c4c6d799c9c38765f18e0ede60df05c99e567b
mlehtiva <maria.lehtivaara@csc.fi>
2019-07-29 09:28:39 +0300

    New tool: update Seurat object from v2 to v3

---
+++ tools/ngs/R/single-cell-seurat-update-object.R
@@ -0,0 +1,19 @@
+# TOOL single-cell-seurat-update-object.R: "Seurat -Update Seurat objects to version 3" (Updates Seurat objects to new v3 structure, so old objects can be analyzed with the updated versions of the tools.) 
+# INPUT OPTIONAL seurat_obj.Robj: "Seurat object" TYPE GENERIC
+# OUTPUT OPTIONAL seurat_obj_2.Robj
+# RUNTIME R-3.4.3
+
+# 2019-07-24 ML 
+
+library(Seurat)
+
+# Load the R-Seurat-object (called seurat_obj)
+load("seurat_obj.Robj")
+
+seurat_obj_2.Robj = UpdateSeuratObject(object = seurat_obj)
+
+
+# Save the Robj for the next tool
+save(seurat_obj, file="seurat_obj_2.Robj")
+
+## EOF
