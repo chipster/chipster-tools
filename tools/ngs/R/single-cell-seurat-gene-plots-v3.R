f0c0695a0a7f584a8b674a12191415057e72e598
mlehtiva <maria.lehtivaara@csc.fi>
2019-09-13 15:31:29 +0300

    Add UMAP (and tSNE and PCA) as visualisation options

--- tools/ngs/R/single-cell-seurat-gene-plots-v3.R
+++ tools/ngs/R/single-cell-seurat-gene-plots-v3.R
@@ -5,6 +5,7 @@
 # OUTPUT OPTIONAL biomarker_plot.pdf
 # PARAMETER biomarker: "Gene name\(s\)" TYPE STRING DEFAULT MS4A1 (Name\(s\) of the biomarker gene to plot. If you list multiple gene names, use comma \(,\) as separator.)
 # PARAMETER OPTIONAL point.size: "Point size in tSNE plot" TYPE DECIMAL DEFAULT 1 (Point size for tSNE plot.)
+# PARAMETER OPTIONAL reduction.method: "tSNE, UMAP or PCA" TYPE [umap:UMAP, tsne:tSNE, pca:PCA] DEFAULT umap (Which dimensionality reduction to use.)
 # RUNTIME R-3.6.1
 
 
@@ -12,6 +13,11 @@
 # 2018-01-11 ML update Seurat version to 2.2.0
 # 2019-06-28 EK Add point size parameter for tSNE plot
 # 2019-06-13 ML Seurat v3
+
+# for UMAP:
+library(reticulate)
+use_python("/opt/chipster/tools/miniconda3/envs/chipster_tools/bin/python")
+
 
 library(Seurat)
 library(dplyr)
@@ -33,7 +39,7 @@ if(length(grep(",", biomarker)) != 0) {
 # Violin plot:
 pdf(file="biomarker_plot.pdf") 
 VlnPlot(seurat_obj, features = biomarker)
-FeaturePlot(seurat_obj, features = biomarker, pt.size=point.size) 	
+FeaturePlot(seurat_obj, features = biomarker, pt.size=point.size, reduction=reduction.method) 	
 dev.off() # close the pdf
 
 # EOF
