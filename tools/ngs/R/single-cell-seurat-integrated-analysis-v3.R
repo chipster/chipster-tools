6dd738b485ef3ff7b39fb9cfdce3346e9ecf8f6d
mlehtiva <maria.lehtivaara@csc.fi>
2019-09-17 08:59:54 +0300

    UMAP or tSNE as parameter

--- tools/ngs/R/single-cell-seurat-integrated-analysis-v3.R
+++ tools/ngs/R/single-cell-seurat-integrated-analysis-v3.R
@@ -2,9 +2,10 @@
 # INPUT OPTIONAL combined_seurat_obj.Robj: "Combined Seurat object" TYPE GENERIC
 # OUTPUT OPTIONAL integrated_plot.pdf
 # OUTPUT OPTIONAL seurat_obj_combined.Robj
-# PARAMETER OPTIONAL num.dims: "Number of CCs to use " TYPE INTEGER DEFAULT 20 (Number of canonical correlates to use. Use the plots from CCA tool to estimate how many you wish to use.)
+# PARAMETER OPTIONAL num.dims: "Number of dimensions to use " TYPE INTEGER DEFAULT 20 (Number of canonical correlates to use. )
 # PARAMETER OPTIONAL res: "Resolution for granularity" TYPE DECIMAL DEFAULT 0.5 (Resolution parameter that sets the granularity of the clustering. Increased values lead to greater number of clusters. Values between 0.6-1.2 return good results for single cell datasets of around 3K cells. For larger data sets, try higher resolution.)
-# PARAMETER OPTIONAL point.size: "Point size in tSNE plot" TYPE DECIMAL DEFAULT 0.5 (Point size for tSNE plot. )
+# PARAMETER OPTIONAL point.size: "Point size in tSNE plot" TYPE DECIMAL DEFAULT 0.5 (Point size for dimensionality plot. )
+# PARAMETER OPTIONAL reduction.method: "Visualisation with tSNE, UMAP or PCA" TYPE [umap:UMAP, tsne:tSNE, pca:PCA] DEFAULT umap (Which dimensionality reduction to use.)
 # RUNTIME R-3.6.1
 
 
@@ -30,23 +31,22 @@ require(cowplot)
 load("combined_seurat_obj.Robj")
 # combined_seurat_obj <- data.combined
 
-# t-SNE and Clustering
- data.combined <- RunUMAP(data.combined, reduction = "pca", dims = 1:num.dims)  # dims = Which dimensions to use as input features
-# data.combined <- RunTSNE(data.combined, reduction = "pca", dims = 1:num.dims) # dims = Which dimensions to use as input features
+# t-SNE and UMAP 
+# NOTE: let's do both tSNE AND UMAP so that both can be later visualized.
+data.combined <- RunUMAP(data.combined, reduction = "pca", dims = 1:num.dims)  # dims = Which dimensions to use as input features
+data.combined <- RunTSNE(data.combined, reduction = "pca", dims = 1:num.dims) # dims = Which dimensions to use as input features
+
+# Clustering
 data.combined <- FindNeighbors(data.combined, reduction = "pca", dims = 1:num.dims) # dims = Dimensions of reduction to use as input
 data.combined <- FindClusters(data.combined, resolution = res)
 
 # Visualization
 pdf(file="integrated_plot.pdf", , width=13, height=7)  # open pdf
- p1 <- DimPlot(data.combined, reduction = "umap", group.by = "stim", pt.size = point.size)
- p2 <- DimPlot(data.combined, reduction = "umap", label = TRUE, pt.size = point.size)
-# p1 <- DimPlot(data.combined, reduction = "tsne", group.by = "stim", pt.size = point.size)
-# p2 <- DimPlot(data.combined, reduction = "tsne", label = TRUE, pt.size = point.size)
+ p1 <- DimPlot(data.combined, reduction = reduction.method, group.by = "stim", pt.size = point.size)
+ p2 <- DimPlot(data.combined, reduction = reduction.method, label = TRUE, pt.size = point.size)
 plot_grid(p1, p2)
 # Show both conditions in separate plots:
-# DimPlot(data.combined, reduction = "tsne", split.by = "stim", pt.size = point.size)
-DimPlot(data.combined, reduction = "umap", split.by = "stim", pt.size = point.size)
-
+DimPlot(data.combined, reduction = reduction.method, split.by = "stim", pt.size = point.size)
 
 cell_counts <- table(Idents(data.combined), data.combined$stim)
 
