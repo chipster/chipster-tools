1ef0adad33c2edb984bd8acd22a137bbc985e6de
Taavi Hupponen <taavi.hupponen@csc.fi>
2019-10-31 15:46:38 +0200

    Add slots 6 to single-cell-seurat-setup-v3.R

--- tools/ngs/R/single-cell-seurat-setup-v3.R
+++ tools/ngs/R/single-cell-seurat-setup-v3.R
@@ -9,7 +9,7 @@
 # PARAMETER OPTIONAL mincells: "Keep genes which are expressed in at least this many cells" TYPE INTEGER DEFAULT 3 (The genes need to be expressed in at least this many cells.)
 # PARAMETER OPTIONAL groupident: "Sample or group name" TYPE STRING DEFAULT empty (Type the group or sample name or identifier here. For example CTRL, STIM, TREAT. Do not use underscore _ in the names! Fill this field if you are combining samples later.)
 # RUNTIME R-3.6.1
-
+# SLOTS 6
 
 # 2017-06-06 ML
 # 2017-07-05 ML split into separate tool
