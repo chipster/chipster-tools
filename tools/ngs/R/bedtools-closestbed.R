c5a88bd6ba9fad47c80905a8d16a95eb9d9f279c
amsaren <ari-matti.saren@csc.fi>
2020-01-13 15:48:21 +0200

    Style edits

--- tools/ngs/R/bedtools-closestbed.R
+++ tools/ngs/R/bedtools-closestbed.R
@@ -1,7 +1,7 @@
 # TOOL bedtools-closestbed.R: "Closest BED" (For each feature in A, finds the closest feature (upstream or downstream\) in B. This tool is based on the BEDTools package.)
 # INPUT file.a: "BED file A" TYPE GENERIC
 # INPUT file.b: "BED file B" TYPE GENERIC
-# OUTPUT OPTIONAL closestbed.bed 
+# OUTPUT OPTIONAL closestbed.bed
 # OUTPUT OPTIONAL error.txt
 # PARAMETER OPTIONAL s: "Force strandedness" TYPE [yes, no] DEFAULT no (Force strandedness. That is, find the closest feature in B that overlaps A on the same strand. By default, overlaps are reported without respect to strand.)
 # PARAMETER OPTIONAL d: "Report distance" TYPE [yes, no] DEFAULT no (In addition to the closest feature in B, report its distance to A as an extra column. The reported distance for overlapping features will be 0.)
@@ -15,12 +15,16 @@ binary <- c(file.path(chipster.tools.path, "bedtools", "bin", "closestBed"))
 
 # optional options
 options <- paste("")
-if (s == "yes") {options <- paste(options, "-s")}
-if (d == "yes") {options <- paste(options, "-d")}
+if (s == "yes") {
+  options <- paste(options, "-s")
+}
+if (d == "yes") {
+  options <- paste(options, "-d")
+}
 options <- paste(options, "-t", t)
 
 # input files
-options <- paste(options,"-a file.a -b file.b")
+options <- paste(options, "-a file.a -b file.b")
 
 # command
 command <- paste(binary, options, " > closestbed.tmp 2> error.tmp")
@@ -30,10 +34,10 @@ system(command)
 
 # Generate output/error message
 if (file.info("closestbed.tmp")$size > 0) {
-	system("mv closestbed.tmp closestbed.bed")
+  system("mv closestbed.tmp closestbed.bed")
 } else if (file.info("error.tmp")$size > 0) {
-	system("mv error.tmp error.txt")
-} else{
-	system("echo \"# No results found\" > error.txt")
+  system("mv error.tmp error.txt")
+} else {
+  system("echo \"# No results found\" > error.txt")
 }
 
