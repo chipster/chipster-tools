ab4fc1f3b535b53376bd1fdb20670f45e863093f
mlehtiva <maria.lehtivaara@csc.fi>
2020-02-04 14:12:58 +0200

    Add option to print out aver. expr. for clusters
    Added option to print out average expression of each cell in the clusters.
    Added to tools: seurat-clustering and seurat-integrated-analysis.

--- tools/ngs/R/single-cell-seurat-clustering-v3.R
+++ tools/ngs/R/single-cell-seurat-clustering-v3.R
@@ -2,8 +2,9 @@
 # INPUT seurat_obj.Robj: "Seurat object from the Setup tool" TYPE GENERIC
 # OUTPUT OPTIONAL log.txt
 # OUTPUT OPTIONAL clusterPlot.pdf
-# OUTPUT OPTIONAL seurat_obj_2.Robj
+# OUTPUT seurat_obj_2.Robj
 # OUTPUT OPTIONAL markers.tsv
+# OUTPUT OPTIONAL aver_expr_in_clusters.tsv
 # PARAMETER OPTIONAL pcs_use: "Number of principal components to use" TYPE INTEGER DEFAULT 10 (How many principal components to use. User must define this based on the PCA-elbow and PCA plots from the setup tool. Seurat developers encourage to test with different parameters, and use preferably more than less PCs for downstream analysis.)
 # PARAMETER OPTIONAL res: "Resolution for granularity" TYPE DECIMAL DEFAULT 0.5 (Resolution parameter that sets the granularity of the clustering. Increased values lead to greater number of clusters. Values between 0.6-1.2 return good results for single cell datasets of around 3K cells. For larger data sets, try higher resolution.)
 # PARAMETER OPTIONAL perplex: "Perplexity, expected number of neighbors for tSNE plot" TYPE INTEGER DEFAULT 30 (Perplexity, expected number of neighbors. Default 30. Set to lower number if you have very few cells. Used for the tSNE visualisation of the clusters.)
@@ -11,6 +12,7 @@
 # PARAMETER OPTIONAL minpct: "Min fraction of cells where a cluster marker gene is expressed" TYPE DECIMAL DEFAULT 0.25 (Test only genes which are detected in at least this fraction of cells in either of the two populations. Meant to speed up the function by not testing genes that are very infrequently expression)
 # PARAMETER OPTIONAL threshuse: "Differential expression threshold for a cluster marker gene" TYPE DECIMAL DEFAULT 0.25 (Limit testing to genes which show, on average, at least X-fold difference, in log2 scale, between the two groups of cells. Increasing thresh.use speeds up the function, but can miss weaker signals.)
 # PARAMETER OPTIONAL test.type: "Which test to use for finding marker genes" TYPE [wilcox, bimod, roc, t, tobit, poisson, negbinom, MAST] DEFAULT wilcox (Denotes which test to use. Seurat currently implements \"wilcox\" \(Wilcoxon rank sum test, default\), \"bimod\" \(likelihood-ratio test for single cell gene expression\), \"roc\" \(standard AUC classifier\), \"t\" \(Students t-test\), \"tobit\" \(Tobit-test for differential gene expression\), \"MAST\" \(GLM-framework that treates cellular detection rate as a covariate\), \"poisson\", and \"negbinom\". The latter two options should be used on UMI datasets only, and assume an underlying poisson or negative-binomial distribution.)
+# PARAMETER OPTIONAL output_aver_expr: "Give a list of average expression in each cluster" TYPE [T: yes, F: no] DEFAULT F (Returns an expression table for an 'average' single cell in each cluster.)
 # RUNTIME R-3.6.1
 
 
@@ -26,6 +28,7 @@
 # 2019-09-09 ML UMAP
 # 2019-09-23 EK add only.pos=TRUE
 # 2019-11-20 EK remove DESeq2 option
+# 2020-01-31 ML Add option to output average expression table
 
 # for UMAP:
 library(reticulate)
@@ -39,8 +42,8 @@ library(gplots)
 # Load the R-Seurat-object (called seurat_obj)
 load("seurat_obj.Robj")
 
-if (exists("data.combined") ){
-	seurat_obj <- data.combined
+if (exists("data.combined")) {
+  seurat_obj <- data.combined
 }
 
 # Cluster the cells
@@ -51,38 +54,48 @@ seurat_obj <- FindClusters(seurat_obj, resolution = res)
 
 # Non-linear dimensional reduction (tSNE/UMAP) & number of cells in clusters
 # NOTE: let's do both tSNE AND UMAP so that both can be later visualized.
-seurat_obj <- RunTSNE(seurat_obj, dims = 1:pcs_use, do.fast=T, perplexity=perplex)
+seurat_obj <- RunTSNE(seurat_obj, dims = 1:pcs_use, do.fast = T, perplexity = perplex)
 seurat_obj <- RunUMAP(seurat_obj, dims = 1:pcs_use)
 
 # Plot tSNE and UMAP
-pdf(file="clusterPlot.pdf") 			
+pdf(file = "clusterPlot.pdf")
 DimPlot(seurat_obj, reduction = "umap", pt.size = point.size)
-DimPlot(seurat_obj, reduction = "tsne", pt.size = point.size)	
+DimPlot(seurat_obj, reduction = "tsne", pt.size = point.size)
 #TSNEPlot(object = seurat_obj, do.return = T, pt.size = point.size, plot.title = paste("Number of cells: ", length(colnames(x = seurat_obj))))
 
 # Number of cells in each cluster:
 cell_counts <- table(Idents(seurat_obj), seurat_obj$orig.ident)
-textplot(cell_counts, halign="center", valign="center", cex=1)
-title(paste("Total number of cells: ",length(colnames(x = seurat_obj)), "\n Number of cells in each cluster:" ) )
+textplot(cell_counts, halign = "center", valign = "center", cex = 1)
+title(paste("Total number of cells: ", length(colnames(x = seurat_obj)), "\n Number of cells in each cluster:"))
 
-# Find all markers 
-markers <- FindAllMarkers(seurat_obj, min.pct = minpct, logfc.threshold = threshuse, test.use = test.type, only.pos = TRUE) 
+# Find all markers
+markers <- FindAllMarkers(seurat_obj, min.pct = minpct, logfc.threshold = threshuse, test.use = test.type, only.pos = TRUE)
 
-if(length(warnings())>0){ # or !is.null(warnings())
-	stop("CHIPSTER-NOTE: There was issue with FindAllMarkers functions with the selected test type, try another test!")
+if (length(warnings()) > 0) {
+  # or !is.null(warnings())
+  stop("CHIPSTER-NOTE: There was issue with FindAllMarkers functions with the selected test type, try another test!")
 }
 
-write.table(as.matrix(markers), file = "markers.tsv", sep="\t", row.names=T, col.names=T, quote=F)
+write.table(as.matrix(markers), file = "markers.tsv", sep = "\t", row.names = T, col.names = T, quote = F)
 
-# Plot top10 genes of each cluster as a heatmap 
+# Plot top10 genes of each cluster as a heatmap
 top10 <- markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_logFC)
 
 # Heatmap
 DoHeatmap(object = seurat_obj, features = top10$gene) + NoLegend()
 dev.off() # close the pdf
 
+# Average expression table
+# If requested, return expression for an 'average' single cell in each cluster.
+if (output_aver_expr == "T") {
+  aver_expr <- AverageExpression(object = seurat_obj)
+  aver_expr_in_clusters <- aver_expr[[1]]
+  # Write to table
+  write.table(aver_expr_in_clusters, file = "aver_expr_in_clusters.tsv", sep = "\t", row.names = TRUE, col.names = TRUE, quote = FALSE)
+
+}
 
 # Save the Robj for the next tool
-save(seurat_obj, file="seurat_obj_2.Robj")
+save(seurat_obj, file = "seurat_obj_2.Robj")
 
 # EOF
--- tools/ngs/R/single-cell-seurat-integrated-analysis-v3.R
+++ tools/ngs/R/single-cell-seurat-integrated-analysis-v3.R
@@ -1,11 +1,13 @@
-# TOOL single-cell-seurat-integrated-analysis-v3.R: "Seurat v3 -Integrated analysis of two samples" (This tool performs integrated analysis on the data: clustering and visualisation of the clusters. This tool can be used for two sample combined Seurat objects.) 
-# INPUT OPTIONAL combined_seurat_obj.Robj: "Combined Seurat object" TYPE GENERIC
+# TOOL single-cell-seurat-integrated-analysis-v3.R: "Seurat v3 -Integrated analysis of two samples" (This tool performs integrated analysis on the data: clustering and visualisation of the clusters. This tool can be used for two sample combined Seurat objects.)
+# INPUT combined_seurat_obj.Robj: "Combined Seurat object" TYPE GENERIC
 # OUTPUT OPTIONAL integrated_plot.pdf
-# OUTPUT OPTIONAL seurat_obj_combined.Robj
+# OUTPUT seurat_obj_combined.Robj
+# OUTPUT OPTIONAL aver_expr_in_clusters.tsv
 # PARAMETER OPTIONAL num.dims: "Number of PCs to use " TYPE INTEGER DEFAULT 20 (Number of principal components to use. )
 # PARAMETER OPTIONAL res: "Resolution for granularity" TYPE DECIMAL DEFAULT 0.5 (Resolution parameter that sets the granularity of the clustering. Increased values lead to greater number of clusters. Values between 0.6-1.2 return good results for single cell datasets of around 3K cells. For larger data sets, try higher resolution.)
 # PARAMETER OPTIONAL reduction.method: "Visualisation of clusters with tSNE, UMAP or PCA" TYPE [umap:UMAP, tsne:tSNE, pca:PCA] DEFAULT umap (Which dimensionality reduction to use.)
 # PARAMETER OPTIONAL point.size: "Point size in cluster plot" TYPE DECIMAL DEFAULT 0.5 (Point size for the dimensionality reduction plot.)
+# PARAMETER OPTIONAL output_aver_expr: "Give a list of average expression in each cluster" TYPE [T: yes, F: no] DEFAULT F (Returns an expression table for an 'average' single cell in each cluster.)
 # RUNTIME R-3.6.1
 
 
@@ -15,6 +17,7 @@
 # 2019-04-08 ML number of cells in each cluster in each sample
 # 09.07.2019 ML Seurat v3
 # 2019-09-09 ML UMAP
+# 2020-01-31 ML Add option to output average expression table
 
 
 # for UMAP:
@@ -31,9 +34,9 @@ require(cowplot)
 load("combined_seurat_obj.Robj")
 # combined_seurat_obj <- data.combined
 
-# t-SNE and UMAP 
+# t-SNE and UMAP
 # NOTE: let's do both tSNE AND UMAP so that both can be later visualized.
-data.combined <- RunUMAP(data.combined, reduction = "pca", dims = 1:num.dims)  # dims = Which dimensions to use as input features
+data.combined <- RunUMAP(data.combined, reduction = "pca", dims = 1:num.dims) # dims = Which dimensions to use as input features
 data.combined <- RunTSNE(data.combined, reduction = "pca", dims = 1:num.dims) # dims = Which dimensions to use as input features
 
 # Clustering
@@ -43,23 +46,33 @@ data.combined <- FindNeighbors(data.combined, reduction = "pca", dims = 1:num.di
 data.combined <- FindClusters(data.combined, resolution = res)
 
 # Visualization
-pdf(file="integrated_plot.pdf", , width=13, height=7)  # open pdf
- p1 <- DimPlot(data.combined, reduction = reduction.method, group.by = "stim", pt.size = point.size)
- p2 <- DimPlot(data.combined, reduction = reduction.method, label = TRUE, pt.size = point.size)
+pdf(file = "integrated_plot.pdf", width = 13, height = 7) # open pdf
+p1 <- DimPlot(data.combined, reduction = reduction.method, group.by = "stim", pt.size = point.size)
+p2 <- DimPlot(data.combined, reduction = reduction.method, label = TRUE, pt.size = point.size)
 plot_grid(p1, p2)
 # Show both conditions in separate plots:
 DimPlot(data.combined, reduction = reduction.method, split.by = "stim", pt.size = point.size)
 
 cell_counts <- table(Idents(data.combined), data.combined$stim)
 
-textplot(cell_counts, halign="center", valign="center", cex=1)
-title(paste("Total number of cells: ",length(colnames(x = data.combined)), "\n Number of cells in each cluster:" ) )
+textplot(cell_counts, halign = "center", valign = "center", cex = 1)
+title(paste("Total number of cells: ", length(colnames(x = data.combined)), "\n Number of cells in each cluster:"))
 
 dev.off()
 
+# Average expression table
+# If requested, return expression for an 'average' single cell in each cluster.
+if (output_aver_expr == "T") {
+  aver_expr <- AverageExpression(object = data.combined)
+  aver_expr_in_clusters <- aver_expr[["integrated"]]
+  # Write to table
+  write.table(aver_expr_in_clusters, file = "aver_expr_in_clusters.tsv", sep = "\t", row.names = TRUE, col.names = TRUE, quote = FALSE)
+
+}
+
 
 # Save the Robj for the next tool
-save(data.combined, file="seurat_obj_combined.Robj")
+save(data.combined, file = "seurat_obj_combined.Robj")
 
 ## EOF
 
