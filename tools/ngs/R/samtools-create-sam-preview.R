a0a0a7dfac01888f2033e013303bc5d83e7c1899
amsaren <ari-matti.saren@csc.fi>
2020-02-25 14:23:28 +0200

    Tool Create a preview for BAM

---
+++ tools/ngs/R/samtools-create-sam-preview.R
@@ -0,0 +1,31 @@
+# TOOL samtools-create-sam-preview.R: "Create a preview for BAM" (Creates a SAM format preview from a BAM file. The preview contains the header and 200 first alignments. This tool is based on the SAMtools package.)
+# INPUT alignment.bam TYPE GENERIC
+# OUTPUT preview.sam
+
+# samtools binary
+samtools.binary <- c(file.path(chipster.tools.path,"samtools","samtools"))
+
+# Print header to preview
+system(paste(samtools.binary,"view -H alignment.bam > preview.sam"))
+# Print 200 first reads/alignments to preview
+system(paste(samtools.binary,"view alignment.bam | head -200 >> preview.sam"))
+
+# Handle output names
+source(file.path(chipster.common.path,"tool-utils.R"))
+
+# read input names
+inputnames <- read_input_definitions()
+base <- strip_name(inputnames$alignment.bam)
+
+# Make a matrix of output names
+outputnames <- matrix(NA,nrow = 1,ncol = 2)
+outputnames[1,] <- c("preview.sam",paste(base,".prev.sam",sep = ""))
+
+# Write output definitions file
+write_output_definitions(outputnames)
+
+
+
+
+
+
