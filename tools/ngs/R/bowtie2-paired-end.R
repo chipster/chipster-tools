33b2df60732c8342b588f9d264e7f8c208a89e82
Ari-Matti Saren <saren@csc.fi>
2020-10-19 15:28:00 +0300

    Clarified what gets written to fastq files

--- tools/ngs/R/bowtie2-paired-end.R
+++ tools/ngs/R/bowtie2-paired-end.R
@@ -6,15 +6,13 @@
 # OUTPUT bowtie2.bam 
 # OUTPUT bowtie2.bam.bai 
 # OUTPUT bowtie2.log 
-# OUTPUT OPTIONAL unaligned_1.fq
-# OUTPUT OPTIONAL unaligned_2.fq
-# OUTPUT OPTIONAL discordant_1.fq
-# OUTPUT OPTIONAL discordant_2.fq
+# OUTPUT OPTIONAL failed_1.fq
+# OUTPUT OPTIONAL failed_2.fq
 # PARAMETER organism: "Genome" TYPE ["FILES genomes/indexes/bowtie2 .fa"] DEFAULT "SYMLINK_TARGET genomes/indexes/bowtie2/default .fa" (Genome or transcriptome that you would like to align your reads against.)
 # PARAMETER strategy: "Alignment strategy to use" TYPE [--very-fast: "Very fast", --fast: "Fast", --sensitive: "Sensitive", --very-sensitive: "Very sensitive", --very-fast-local: "Very fast local", -fast-local: "Fast local", --sensitive-local: "Sensitive local", --very-sensitive-local: "Very sensitive local"] DEFAULT --sensitive (The alignment strategy to be used. Bowtie2 can map the reads using end-to-end or local alignments. When local alignment is used, Bowtie2 might "trim" or "clip" some read characters from one or both ends of the alignment if doing so maximizes the alignment score. Bowtie2 uses heuristics for mapping the reads to the reference genome. Several Bowtie2 parameters affect simultaneously both to the sensitivity and to computing time. In Chipster you can choose the sensitivity level from a set of pre-defined parameter combinations that allow you to tune the balance between the computing time and mapping sensitivity.)
 # PARAMETER quality.format: "Quality value format used" TYPE [--phred33: "Sanger - Phred+33", --phred64: "Illumina GA v1.3-1.5 - Phred+64", --ignore-quals: "Fixed 30 for all"] DEFAULT --phred33 (Quality scale used in the fastq-file.)
 # PARAMETER alignment.no: "How many valid alignments are reported per read" TYPE [0: "Best based on the mapping quality", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "All alignments"] DEFAULT 0 (By default Bowtie2 reports only the best alignment of the read (based on the mapping quality\). If there are several equally good alignments, you can choose how many of them should be reported.)
-# PARAMETER OPTIONAL discordant.file: "Put reads that did not align concordantly to a separate file" TYPE [yes, no] DEFAULT no (Write paired-end reads that fail to align concordantly to a fastq file.)
+# PARAMETER OPTIONAL discordant.file: "Put reads that did not align concordantly to a separate file" TYPE [yes, no] DEFAULT no (Write paired-end reads that fail to align concordantly to a fastq file. This includes reads that aligned discordantly, reads whose mate failed to align and unaligned reads.)
 # PARAMETER OPTIONAL ma: "Match bonus" TYPE INTEGER FROM 0 TO 10 DEFAULT 2 (Match bonus for a match in local alignment. Default value 2) 
 # PARAMETER OPTIONAL mp: "Maximum penalty for mismatch" TYPE INTEGER FROM 0 TO 20 DEFAULT 6 (Maximum penalty for mismatch; lower quality = lower penalty. Default value 6)
 # PARAMETER OPTIONAL np: "Penalty for non-ACGTs"  TYPE INTEGER FROM 0 TO 20 DEFAULT 1 ( Sets penalty for positions where the read, reference, or both, contain an ambiguous character such as N. Default: 1.) 
@@ -39,98 +37,94 @@
 
 # PARAMETER OPTIONAL unaligned.file: "Put unaligned reads to a separate file" TYPE [yes, no] DEFAULT no (Would you like to store unaligned reads to a new fastq file? Note that also multireads will be added to this file, unless you asked them to be put to a separate file.)
 
-source(file.path(chipster.common.path, "tool-utils.R"))
-source(file.path(chipster.common.path, "bam-utils.R"))
-source(file.path(chipster.common.path, "zip-utils.R"))
+source(file.path(chipster.common.path,"tool-utils.R"))
+source(file.path(chipster.common.path,"bam-utils.R"))
+source(file.path(chipster.common.path,"zip-utils.R"))
 
 # check out if the file is compressed and if so unzip it
-input.names <- read.table("chipster-inputs.tsv", header=F, sep="\t")
+input.names <- read.table("chipster-inputs.tsv",header = FALSE,sep = "\t")
 for (i in 1:nrow(input.names)) {
-	unzipIfGZipFile(input.names[i,1])	
+  unzipIfGZipFile(input.names[i,1])
 }
 
 # bowtie
-bowtie.binary <- c(file.path(chipster.tools.path, "bowtie2", "bowtie2"))
-bowtie.genome <- c(file.path(chipster.tools.path, "genomes", "indexes", "bowtie2", organism))
-command.start <- paste("bash -c '", bowtie.binary)
-rdg.value <- paste (rdg.open ,rdg.ext , sep=",")
-rfg.value <- paste (rfg.open ,rfg.ext , sep=",")
+bowtie.binary <- c(file.path(chipster.tools.path,"bowtie2","bowtie2"))
+bowtie.genome <- c(file.path(chipster.tools.path,"genomes","indexes","bowtie2",organism))
+command.start <- paste("bash -c '",bowtie.binary)
+rdg.value <- paste(rdg.open,rdg.ext,sep = ",")
+rfg.value <- paste(rfg.open,rfg.ext,sep = ",")
 # common parameters
 #common.parameters <- paste("-q", "-m", multiread, "-k", alignment.no, "-I", min.insert.size, "-X", max.insert.size)
 
-parameters <- paste(strategy, "--mp", mp,"--np", np, "--rdg", rdg.value, "--rfg", rfg.value, "--minins", minins, "--maxins", maxins, pair.order, quality.format, "--no-unal", "-p", chipster.threads.max)
+parameters <- paste(strategy,"--mp",mp,"--np",np,"--rdg",rdg.value,"--rfg",rfg.value,"--minins",minins,"--maxins",maxins,pair.order,quality.format,"--no-unal","-p",chipster.threads.max)
 
-if ( alignment.no>0){
-	if ( alignment.no==6){
-		parameters <- paste(parameters, "-a")
-	}
-	if ( alignment.no<6){
-		parameters <- paste(parameters, "-k", alignment.no )
-	}
+if (alignment.no > 0) {
+  if (alignment.no == 6) {
+    parameters <- paste(parameters,"-a")
+  }
+  if (alignment.no < 6) {
+    parameters <- paste(parameters,"-k",alignment.no)
+  }
 }
 
 # Local alignment specific parameters 
-if (strategy == "--very-fast-local" || strategy == "--fast-local" || strategy == "--sensitive-local" || strategy == "--very-sensitive-local" ) {
-	parameters <- paste(parameters ,"--local --ma", ma)
+if (strategy == "--very-fast-local" || strategy == "--fast-local" || strategy == "--sensitive-local" || strategy == "--very-sensitive-local") {
+  parameters <- paste(parameters,"--local --ma",ma)
 }
 
-if ( no.mixed== "yes"){
-	parameters <- paste(parameters, "--no-mixed")
+if (no.mixed == "yes") {
+  parameters <- paste(parameters,"--no-mixed")
 }
 
-if ( no.discordant== "yes"){
-	parameters <- paste(parameters, "--no-discordant")
+if (no.discordant == "yes") {
+  parameters <- paste(parameters,"--no-discordant")
 }
 
-if ( no.dovetail== "yes"){
-	parameters <- paste(parameters, "--no-dovetail")
+if (no.dovetail == "yes") {
+  parameters <- paste(parameters,"--no-dovetail")
 }
 
-if ( no.contain== "yes"){
-	parameters <- paste(parameters, "--no-contain")
+if (no.contain == "yes") {
+  parameters <- paste(parameters,"--no-contain")
 }
 
-if ( no.overlap== "yes"){
-	parameters <- paste(parameters, "--no-overlap")
+if (no.overlap == "yes") {
+  parameters <- paste(parameters,"--no-overlap")
 }
 
-if (discordant.file== "yes"){
-	parameters <- paste(parameters, "--un-conc discordant")
+if (discordant.file == "yes") {
+  parameters <- paste(parameters,"--un-conc failed")
 }
-
-#if (unaligned.file== "yes"){
-#	parameters <- paste(parameters, "--un unaligned")
-#}
 
 # Check if reads are in FASTA format 
-emboss.path <- file.path(chipster.tools.path, "emboss" ,"bin")
-sfcheck.binary <- file.path(chipster.module.path ,"../misc/shell/sfcheck.sh")
-sfcheck.command <- paste(sfcheck.binary, emboss.path, "reads001.fq" )
-str.filetype <- system(sfcheck.command, intern = TRUE )
-if ( str.filetype == "fasta"){
-	parameters <- paste(parameters, "-f")
+emboss.path <- file.path(chipster.tools.path,"emboss","bin")
+sfcheck.binary <- file.path(chipster.module.path,"../misc/shell/sfcheck.sh")
+sfcheck.command <- paste(sfcheck.binary,emboss.path,"reads001.fq")
+str.filetype <- system(sfcheck.command,intern = TRUE)
+if (str.filetype == "fasta") {
+  parameters <- paste(parameters,"-f")
 }
 
 # Input files
-if (file.exists("reads1.txt") && file.exists("reads2.txt")){
-	# Case: list files exist
-	reads1.list <- make_input_list("reads1.txt")
-	reads2.list <- make_input_list("reads2.txt")
-	if (identical(intersect(reads1.list, reads2.list), character(0))){
-		reads1 <- paste(reads1.list, sep="", collapse=",")
-		reads2 <- paste(reads2.list, sep="", collapse=",")
-	}else{
-		stop(paste('CHIPSTER-NOTE: ', "One or more files is listed in both lists."))
-	}
-}else if (file.exists("reads002.fq") && !file.exists("reads003.fq")){
-	# Case: no list file, but only two fastq inputs
-	in.sorted <- input.names[order(input.names[,2]),]
-	reads <- grep("reads", in.sorted[,1], value = TRUE)
-	reads1 <- reads[1]
-	reads2 <- reads[2]
-}else{
-	# Case: no list files, more than two fastq inputs
-	stop(paste('CHIPSTER-NOTE: ', "List file is missing. You need to provide a list of read files for both directions."))
+if (file.exists("reads1.txt") && file.exists("reads2.txt")) {
+  # Case: list files exist
+  reads1.list <- make_input_list("reads1.txt")
+  reads2.list <- make_input_list("reads2.txt")
+  if (identical(intersect(reads1.list,reads2.list),character(0))) {
+    reads1 <- paste(reads1.list,sep = "",collapse = ",")
+    reads2 <- paste(reads2.list,sep = "",collapse = ",")
+  } else {
+    stop(paste('CHIPSTER-NOTE: ',"One or more files is listed in both lists."))
+  }
+} else if (file.exists("reads002.fq") && !file.exists("reads003.fq")) {
+  # Case: no list file, but only two fastq inputs
+  in.sorted <- input.names[order(input.names[,2]),]
+  reads <- grep("reads",in.sorted[,1],value = TRUE)
+  reads1 <- reads[1]
+  reads2 <- reads[2]
+} else {
+  # Case: no list files, more than two fastq inputs
+  stop(paste('CHIPSTER-NOTE: ',"List file is missing. You need to provide a list of read files for both directions."))
 }
 
 
@@ -138,43 +132,38 @@ if (file.exists("reads1.txt") && file.exists("reads2.txt")){
 #output.parameters <- paste(unaligned.output, multiread.output)
 #stop(paste('CHIPSTER-NOTE: ', parameters))
 # command ending
-command.end <- paste("-x", bowtie.genome, "-1",  reads1, "-2", reads2, "1> alignment.sam 2>> bowtie2.log'")
+command.end <- paste("-x",bowtie.genome,"-1",reads1,"-2",reads2,"1> alignment.sam 2>> bowtie2.log'")
 
 # run bowtie
-bowtie.command <- paste(command.start, parameters, command.end)
+bowtie.command <- paste(command.start,parameters,command.end)
 #stop(paste('CHIPSTER-NOTE: ', bowtie.command))
 
-echo.command <- paste("echo '", bowtie.command , "' > bowtie2.log" )
+echo.command <- paste("echo '",bowtie.command,"' > bowtie2.log")
 system(echo.command)
 system(bowtie.command)
 
 # samtools binary
-samtools.binary <- c(file.path(chipster.tools.path, "samtools", "samtools"))
+samtools.binary <- c(file.path(chipster.tools.path,"samtools","samtools"))
 
 # convert sam to bam
-system(paste(samtools.binary, "view -bS alignment.sam -o alignment.bam"))
+system(paste(samtools.binary,"view -bS alignment.sam -o alignment.bam"))
 
 # Change file named in BAM header to display names
 displayNamesToBAM("alignment.bam")
 
 # sort bam
-system(paste(samtools.binary, "sort alignment.bam alignment.sorted"))
+system(paste(samtools.binary,"sort alignment.bam alignment.sorted"))
 
 # index bam
-system(paste(samtools.binary, "index alignment.sorted.bam"))
+system(paste(samtools.binary,"index alignment.sorted.bam"))
 
 # rename result files
 system("mv alignment.sorted.bam bowtie2.bam")
 system("mv alignment.sorted.bam.bai bowtie2.bam.bai")
 
-#if (unaligned.file== "yes"){
-#	system("mv unaligned.1 unaligned_1.fq")
-#	system("mv unaligned.2 unaligned_2.fq")
-#}
-
-if (discordant.file== "yes"){
-	system("mv discordant.1 discordant_1.fq")
-	system("mv discordant.2 discordant_2.fq")
+if (discordant.file == "yes") {
+  system("mv failed.1 failed_1.fq")
+  system("mv failed.2 failed_2.fq")
 }
 
 # Substitute display names to log for clarity
@@ -186,22 +175,20 @@ displayNamesToFile("bowtie2.log")
 inputnames <- read_input_definitions()
 
 # Determine base name
-name1 <- unlist(strsplit(reads1, ","))
+name1 <- unlist(strsplit(reads1,","))
 base1 <- strip_name(inputnames[[name1[1]]])
 
-name2 <- unlist(strsplit(reads2, ","))
+name2 <- unlist(strsplit(reads2,","))
 base2 <- strip_name(inputnames[[name2[1]]])
 
-basename <- paired_name(base1, base2)
+basename <- paired_name(base1,base2)
 
 # Make a matrix of output names
-outputnames <- matrix(NA, nrow=6, ncol=2)
-outputnames[1,] <- c("bowtie2.bam", paste(basename, ".bam", sep =""))
-outputnames[2,] <- c("bowtie2.bam.bai", paste(basename, ".bam.bai", sep =""))
-outputnames[3,] <- c("unaligned_1.fq", paste(base1, "_unaligned.fq", sep=""))
-outputnames[4,] <- c("unaligned_2.fq", paste(base2, "_unaligned.fq", sep=""))
-outputnames[5,] <- c("discordant_1.fq", paste(base1, "_discordant.fq", sep=""))
-outputnames[6,] <- c("discordant_2.fq", paste(base2, "_discordant.fq", sep=""))
+outputnames <- matrix(NA,nrow = 4,ncol = 2)
+outputnames[1,] <- c("bowtie2.bam",paste(basename,".bam",sep = ""))
+outputnames[2,] <- c("bowtie2.bam.bai",paste(basename,".bam.bai",sep = ""))
+outputnames[3,] <- c("failed_1.fq",paste(base1,"_failed.fq",sep = ""))
+outputnames[4,] <- c("failed_2.fq",paste(base2,"_failed.fq",sep = ""))
 
 # Write output definitions file
 write_output_definitions(outputnames)
