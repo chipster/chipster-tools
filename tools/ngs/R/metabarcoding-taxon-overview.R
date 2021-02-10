51db98982062e0622675e4961fc7270e7e13e8e6
Jesse Harrison <jesse.harrison@csc.fi>
2021-02-10 11:46:13 +0000

    Update phyloseq tools

--- tools/ngs/R/metabarcoding-taxon-overview.R
+++ tools/ngs/R/metabarcoding-taxon-overview.R
@@ -1,7 +1,7 @@
 # TOOL metabarcoding-taxon-overview.R: "Overview of taxon composition" (Tabulates the overall OTU count and a summary of taxon frequencies. Requires a phyloseq object in Rda format as the input.)
 # INPUT ps.Rda: "Phyloseq object in Rda format" TYPE GENERIC
 # OUTPUT ps_taxon.txt: ps_taxon.txt
-# PARAMETER OPTIONAL type: "Level of biological organization for tabulating taxon composition" TYPE [phylum: "Phylum", class: "Class", order: "Order", family: "Family", genus: "Genus"] DEFAULT phylum (Select the desired taxonomic level; default is phylum)
+# PARAMETER OPTIONAL type: "Level of biological organization for tabulating taxon composition" TYPE [phylum: "Phylum", class: "Class", order: "Order", family: "Family", genus: "Genus", species: "Species"] DEFAULT phylum (Select the desired taxonomic level; default is phylum)
 # RUNTIME R-3.6.1-phyloseq
 
 # JH 2020
@@ -15,6 +15,13 @@ load("ps.Rda")
 # Overall no. of taxa
 taxano <- ntaxa(ps)
 
+# if "Species" selected for taxon tabulation, check that using 7-level classification
+taxlength <- length(colnames(tax_table(ps)))
+if (type == "species" && 
+	taxlength == "6"){
+		stop("Data set lacks species-level assignments; please choose another level of organization.")
+}
+
 # Taxonomy table
 if (type == "phylum"){
 	taxonsummary <- table(tax_table(ps)[, "Phylum"], exclude = NULL) # column no. equivalents would be e.g. [, 2]
@@ -26,6 +33,8 @@ if (type == "phylum"){
 		taxonsummary <- table(tax_table(ps)[, "Family"], exclude = NULL)
 	} else if (type == "genus") {
 		taxonsummary <- table(tax_table(ps)[, "Genus"], exclude = NULL)
+	} else if (type == "species") {
+		taxonsummary <- table(tax_table(ps)[, "Species"], exclude = NULL)		
 }
 
 sink("ps_taxon.txt")
