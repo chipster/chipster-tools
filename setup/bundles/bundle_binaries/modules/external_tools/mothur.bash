2f8e3b25bbae2e5e79982f8e100ec13b41f47f3e
chipster <chipster@csc.fi>
2021-03-05 11:27:44 +0000

    Install Mothur 1.44.3

--- setup/bundles/bundle_binaries/modules/external_tools/mothur.bash
+++ setup/bundles/bundle_binaries/modules/external_tools/mothur.bash
@@ -5,21 +5,23 @@ source ../installation_files/functions.bash
 # mothur GPLv3
 cd ${TMPDIR_PATH}/
 
-# Mothur
-# Retain version 1.39.5 as backup
-wget_retry -nv  https://github.com/mothur/mothur/releases/download/v1.39.5/Mothur.linux_64.noReadLine.zip
-unzip -q Mothur.linux_64.noReadLine.zip
-mv mothur ${TOOLS_PATH}/mothur-1.39.5
-rm -rf  __MACOSX
-
-# Make version 1.41.3 the default
+# Retain version 1.41.3 as backup
 cd ${TMPDIR_PATH}/
 wget_retry -nv https://github.com/mothur/mothur/releases/download/v1.41.3/Mothur.linux_64.zip
 unzip -q Mothur.linux_64.zip
 mv mothur ${TOOLS_PATH}/mothur-1.41.3
 rm -rf  __MACOSX
+#cd ${TOOLS_PATH}
+#ln -s mothur-1.41.3 mothur
+
+# Make version 1.44.3 the default
+cd ${TMPDIR_PATH}/
+wget_retry -nv https://github.com/mothur/mothur/releases/download/v1.44.3/Mothur.linux.zip
+unzip -q Mothur.linux.zip
+mv mothur ${TOOLS_PATH}/mothur-1.44.3
+rm -rf  __MACOSX
 cd ${TOOLS_PATH}
-ln -s mothur-1.41.3 mothur
+ln -s mothur-1.44.3 mothur
 
 mkdir -p ${TOOLS_PATH}/mothur-silva-reference/
 curl -s http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/mothur/silva/v102.tar.lz4  | lz4 -d | tar -x -C ${TOOLS_PATH}/mothur-silva-reference/
