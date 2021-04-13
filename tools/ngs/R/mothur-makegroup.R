20e70a9fd3a7bee5e91ac3710b7db3afd5df416f
ekorpela <ekorpelainen@gmail.com>
2021-04-13 14:14:42 +0000

    Add summary.seqs

--- tools/ngs/R/mothur-makegroup.R
+++ tools/ngs/R/mothur-makegroup.R
@@ -1,7 +1,8 @@
-# TOOL mothur-makegroup.R: "Combine FASTQ or FASTA files and make a group file" (Combines FASTQ or FASTA files of all samples to one file and creates a Mothur group file for it. Input files can be gzipped. This tool is based on the Mothur tools make.group and merge.files.)
+# TOOL mothur-makegroup.R: "Combine FASTQ or FASTA files and make a group file" (Combines FASTQ or FASTA files of all samples to one file and creates a Mothur group file for it. Input files can be gzipped. This tool is based on the Mothur tools make.group, merge.files and summary.seqs.)
 # INPUT reads{...}: "read files" TYPE GENERIC
 # OUTPUT sequences.fasta.gz
 # OUTPUT sequences.groups
+# OUTPUT sequences-summary.tsv
 # PARAMETER input.type: "Input type" TYPE [FASTQ, FASTA] DEFAULT FASTQ (Input file type.)
 
 
@@ -21,7 +22,7 @@ for (i in 1:nrow(input.names)) {
 # Convert to FASTA if necessary
 # Binary
 binary <- c(file.path(chipster.tools.path, "fastx", "bin", "fastq_to_fasta"))
-# Go through the list and conver or rename depending on input type
+# Go through the list and convert or rename depending on input type
 for (i in 1:nrow(input.names)) {
   newname <- paste(input.names[i,1], ".fasta", sep = "")
   if (input.type == "FASTQ"){
@@ -68,6 +69,19 @@ write(mergefiles.options,"mergefiles.mth",append = FALSE)
 command2 <- paste(binary,"mergefiles.mth","> log2.txt")
 system(command2)
 
-# zip output fasta
+# Create summary file
+summaryseqs.options <- paste("summary.seqs(fasta=sequences.fasta")
+summaryseqs.options <- paste(summaryseqs.options,", processors=",chipster.threads.max,")",sep = "")
+documentCommand(summaryseqs.options)
+write(summaryseqs.options,"summary.mth",append = FALSE)
+command <- paste(binary,"summary.mth","> log_raw.txt")
+system(command)
+# Post process output
+system("grep -A 10 Start log_raw.txt > summary.tsv")
+# Remove one tab to get the column naming look nice:
+system("sed 's/^		/	/' summary.tsv > sequences-summary.tsv")
+
+# Gzip output fasta
 system("gzip sequences.fasta")
+
 #Sys.sleep(120)
