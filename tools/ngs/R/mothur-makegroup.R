3a576707216900a3369479e7131694c68bd89be2
Ari-Matti Saren <saren@csc.fi>
2021-05-03 07:27:32 +0000

    Added tar input

--- tools/ngs/R/mothur-makegroup.R
+++ tools/ngs/R/mothur-makegroup.R
@@ -1,10 +1,8 @@
-# TOOL mothur-makegroup.R: "Combine FASTQ or FASTA files and make a group file" (Combines FASTQ or FASTA files of all samples to one file and creates a Mothur group file for it. Input files can be gzipped. This tool is based on the Mothur tools make.group, merge.files and summary.seqs.)
+# TOOL mothur-makegroup.R: "Combine FASTQ or FASTA files and make a group file" (Combines FASTQ or FASTA files of all samples to one file and creates a Mothur group file for it. Input can be one or more FASTQ or FASTA files, or a tar file. Input files can be gzipped. This tool is based on the Mothur tools make.group, merge.files and summary.seqs.)
 # INPUT reads{...}: "read files" TYPE GENERIC
 # OUTPUT sequences.fasta.gz
 # OUTPUT sequences.groups
 # OUTPUT sequences-summary.tsv
-# PARAMETER input.type: "Input type" TYPE [FASTQ, FASTA] DEFAULT FASTQ (Input file type.)
-
 
 # EK 6.4.2021
 # OUTPUT log1.txt
@@ -15,42 +13,67 @@ source(file.path(chipster.common.path, "zip-utils.R"))
 
 # check out if the file is compressed and if so unzip it
 input.names <- read.table("chipster-inputs.tsv", header=F, sep="\t")
-for (i in 1:nrow(input.names)) {
-	unzipIfGZipFile(input.names[i,1])	
-}
 
-# Convert to FASTA if necessary
 # Binary
-binary <- c(file.path(chipster.tools.path, "fastx", "bin", "fastq_to_fasta"))
-# Go through the list and convert or rename depending on input type
+fastx.binary <- c(file.path(chipster.tools.path, "fastx", "bin", "fastq_to_fasta"))
+
+# Go through the input files. Uncompress if necessary. Untar if necessary. 
+fasta_names <- ""
+group_names <- ""
 for (i in 1:nrow(input.names)) {
-  newname <- paste(input.names[i,1], ".fasta", sep = "")
-  if (input.type == "FASTQ"){
-    command <- paste(binary, "-n -i", input.names[i,1],"-o", newname)
-    system(command)
-  }else{
-    file.rename(paste(input.names[i,1]), newname)
+	unzipIfGZipFile(input.names[i,1])
+  # Check if input is tar package
+  isTar <- grepl("POSIX tar", system(paste("file", input.names[i,1]), intern=TRUE))
+  # Input is a tar file
+  if (isTar){
+    tarlist <- untar(paste(input.names[i,1]), list = TRUE)
+    untar(paste(input.names[i,1]))
+    # go throug list
+    for (j in 1:length(tarlist)){
+      unzipIfGZipFile(tarlist[j])
+      basename <- paste(strip_name(paste(tarlist[j])))
+      fastaname <- paste(basename, ".fasta", sep = "")
+      # If input is FASTQ file, convert to FASTA
+      if (isFastq(paste(tarlist[j]))){
+        command <- paste(fastx.binary, "-n -i", tarlist[j],"-o", fastaname)
+        runExternal(command)        
+      # If input is FASTA file, just make sure file name ends with .fasta  
+      }else if(isFasta(paste(tarlist[j]))){
+        file.rename(paste(tarlist[j]), fastaname)
+      } else{
+        stop(paste('CHIPSTER-NOTE: Input not a FASTA or FASTQ sequence file'))
+      }
+      fasta_names <- paste(fasta_names, fastaname, sep="-")
+      group_names <- paste(group_names, basename, sep="-")
+    }
+  }
+  # Input file is a sequense file
+  else{
+    basename <- paste(strip_name(paste(input.names[i,1])))
+    fastaname <- paste(basename, ".fasta", sep = "")
+    groupname <- paste(strip_name(paste(input.names[i,2])))
+    # Input is a FASTQ file, convert to FASTA    
+    if (isFastq(paste(input.names[i,1]))){      
+      command <- paste(fastx.binary, "-n -i", input.names[i,1],"-o", fastaname)
+      runExternal(command)
+    # Input is FASTA file, just make sure file name ends with .fasta
+    }else if(isFasta(paste(input.names[i,1]))){
+      file.rename(paste(input.names[i,1]), fastaname)    
+    }else{
+      stop(paste('CHIPSTER-NOTE: Input not a FASTA or FASTQ sequence file'))
+    }
+    fasta_names <- paste(fasta_names, fastaname, sep="-")
+    group_names <- paste(group_names, groupname, sep="-")
   }
 }
+# List files now have extra "-" at beginning that has to be removed
+fasta_names <- sub(".","", fasta_names)
+group_names <- sub(".","",group_names)
+
 # Binary
 binary <- c(file.path(chipster.tools.path,"mothur","mothur"))
 version <- system(paste(binary,"--version"),intern = TRUE)
 documentVersion("Mothur",version)
-
-# Collect names of the input files
-input.names <- read.table("chipster-inputs.tsv", header=F, sep="\t")
-
-# Set names for first input
-fasta_names <- paste(strip_name(paste(input.names[1,1])), ".fasta", sep="")
-group_names <- strip_name(paste(input.names[1,2]))
-
-# Loop through the rest of the inputs
-for (i in 2:nrow(input.names)) {
-  fasta <- paste(strip_name(paste(input.names[i,1])), ".fasta", sep="")
-  fasta_names <- paste(fasta_names, fasta, sep="-") 
-  group <- strip_name(paste(input.names[i,2]))
-  group_names <- paste(group_names, group, sep="-")
-}
 
 makegroup.options <- paste("make.group(fasta=",fasta_names, ", groups=",group_names, ")", sep = "")
 documentCommand(makegroup.options)
