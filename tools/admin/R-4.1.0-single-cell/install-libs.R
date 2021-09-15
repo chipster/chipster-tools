ba8c50bb785e585483538844336a53c3e8649169
Petri Klemela <petri.klemela@csc.fi>
2021-09-15 13:31:38 +0300

    Let's try R-4.1.0 for single-cell

---
+++ tools/admin/R-4.1.0-single-cell/install-libs.R
@@ -0,0 +1,17 @@
+
+# CRAN packages and their dependencies
+install.packages("Seurat")
+install.packages("dplyr")
+install.packages("pheatmap")
+install.packages("Matrix")
+install.packages("gplots")
+install.packages("cowplot")
+install.packages("ggplot2")
+install.packages("umap")
+
+install.packages("BiocManager")
+BiocManager::install("DESeq2")
+BiocManager::install("MAST")
+BiocManager::install("GEOquery")
+BiocManager::install("scater")
+BiocManager::install("mvoutlier")
