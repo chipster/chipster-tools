4b90b25f46c4f692ecd15169103ba1ac5b1b9a8c
Petri Klemela <petri.klemela@csc.fi>
2021-11-16 10:53:39 +0000

    Allow shell to other images

--- comp-shell.bash
+++ comp-shell.bash
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-if ! kubectl get pod | grep install-r | grep Running > /dev/null 2>&1; then
+if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ $# -gt 1 ]; then
+  echo "Open shell in container image"
+  echo "Usage: $0 [IMAGE]"
+  exit 0
+fi
+
+image=${1:-comp-20.04-r-deps}
+name="comp-shell"
+
+if ! kubectl get pod | grep $name | grep Running > /dev/null 2>&1; then
   cat <<EOF | kubectl apply -f -
 apiVersion: apps/v1
 kind: Deployment
 metadata:
-  name: install-r
+  name: $name
 spec:
   replicas: 1
   selector:
     matchLabels:
-      app: install-r
+      app: $name
   template:
     metadata:
       labels:
-        app: install-r
+        app: $name
     spec:
       containers:
-      - name: install-r
-        image: docker-registry.rahti.csc.fi/chipster-images-beta/comp-20.04-r-base
+      - name: $name
+        image: docker-registry.rahti.csc.fi/chipster-images-beta/$image
         volumeMounts:
         - name: tools-bin
           mountPath: /mnt/tools
@@ -31,7 +40,7 @@ EOF
 
   echo "waiting pod to start"
   for i in $(seq 30); do
-    if kubectl get pod 2> /dev/null | grep install-r | grep Running > /dev/null; then     
+    if kubectl get pod 2> /dev/null | grep $name | grep Running > /dev/null; then     
       echo ""
       break
     fi
@@ -41,4 +50,6 @@ EOF
 fi
 
 echo "comp shell, press Ctrl+D to exit"
-kubectl exec -it $(kubectl get pod | grep install-r | grep Running | cut -d " " -f 1) -- bash
+kubectl exec -it $(kubectl get pod | grep $name | grep Running | cut -d " " -f 1) -- bash
+
+kubectl delete deployment $name
