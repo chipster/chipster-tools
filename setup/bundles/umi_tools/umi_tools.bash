b9b0b31a7def5dc35fc65e91cce350166b959735
Petri Klemela <petri.klemela@csc.fi>
2021-11-29 10:57:43 +0200

    Install umi-tools

---
+++ setup/bundles/umi_tools/umi_tools.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+source $OPENRC_PATH
+
+image="comp-20.04-python"
+BUNDLE_COLLECTION_VERSION="55"
+
+ssh $K3S_BUILD_HOST "bash $K3S_BUNDLE_DIR/start-pod.bash $JOB_NAME $BUILD_NUMBER $image $BUNDLE_COLLECTION_VERSION"
+
+ssh $K3S_BUILD_HOST "bash $K3S_BUNDLE_DIR/run-in-pod.bash $JOB_NAME $BUILD_NUMBER root -" <<EOF
+
+  apt-get update
+  apt-get install -y python3-venv
+  apt-get install -y build-essential
+  apt-get install -y python3-dev
+EOF
+  
+ssh $K3S_BUILD_HOST "bash $K3S_BUNDLE_DIR/run-in-pod.bash $JOB_NAME $BUILD_NUMBER ubuntu -" <<EOF
+
+  python3 -m venv /opt/chipster/tools/umi-tools/venv
+  source /opt/chipster/tools/umi-tools/venv/bin/activate
+  
+  pip install wheel
+  pip install umi_tools
+  
+  umi_tools --help
+EOF
+
+ssh $K3S_BUILD_HOST "bash $K3S_BUNDLE_DIR/move-to-artefacts.bash /opt/chipster/tools/umi-tools $JOB_NAME $BUILD_NUMBER"
+
+ssh $K3S_BUILD_HOST "bash $K3S_BUNDLE_DIR/clean-up.bash $JOB_NAME $BUILD_NUMBER"
