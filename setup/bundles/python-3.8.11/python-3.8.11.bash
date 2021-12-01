15688aab71a43b96fbf1e0a0bdad9d87095d45f1
Petri Klemela <petri.klemela@csc.fi>
2021-12-01 10:16:46 +0200

    Better defaults for install scripts

---
+++ setup/bundles/python-3.8.11/python-3.8.11.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+source $OPENRC_PATH
+
+# we don't need a python image to install python
+# most likely the tool wrappers are going to be written in R and hence this r-deps image will be used to run this python eventually
+image="comp-20.04-r-deps"
+
+# this installation doesn't need anythin from tools-bin
+BUNDLE_COLLECTION_VERSION=""
+
+ssh $K3S_BUILD_HOST "bash $K3S_BUNDLE_DIR/start-pod.bash $JOB_NAME $BUILD_NUMBER $image \"$BUNDLE_COLLECTION_VERSION\""
+
+ssh $K3S_BUILD_HOST "bash $K3S_BUNDLE_DIR/run-in-pod.bash $JOB_NAME $BUILD_NUMBER root -" <<EOF
+
+  apt-get update
+  apt-get install -y build-essential
+  apt-get install -y python3-dev
+  apt-get install -y libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev
+  apt-get install -y libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev libffi-dev uuid-dev
+EOF
+  
+ssh $K3S_BUILD_HOST "bash $K3S_BUNDLE_DIR/run-in-pod.bash $JOB_NAME $BUILD_NUMBER ubuntu -" <<EOF
+
+  mkdir /opt/chipster/tools/python-3.8.11
+  
+  # pwd is a temp dir
+  wget https://www.python.org/ftp/python/3.8.11/Python-3.8.11.tgz
+  tar -xzf Python-3.8.11.tgz 
+  cd Python-3.8.11
+  ./configure --prefix=/opt/chipster/tools/python-3.8.11
+  make
+  make install
+
+  /opt/chipster/tools/python-3.8.11/bin/python3 --version
+EOF
+
+ssh $K3S_BUILD_HOST "bash $K3S_BUNDLE_DIR/move-to-artefacts.bash /opt/chipster/tools/python-3.8.11 $JOB_NAME $BUILD_NUMBER"
+
+ssh $K3S_BUILD_HOST "bash $K3S_BUNDLE_DIR/clean-up.bash $JOB_NAME $BUILD_NUMBER"
--- setup/bundles/umi_tools/umi_tools.bash
+++ setup/bundles/umi_tools/umi_tools.bash
@@ -4,14 +4,18 @@ set -e
 
 source $OPENRC_PATH
 
-image="comp-20.04-python"
-BUNDLE_COLLECTION_VERSION="55"
+# wrapper script is going to be written in R
+image="comp-20.04-r-deps"
+
+# this needs python from tools-bin
+BUNDLE_COLLECTION_VERSION="58"
 
 ssh $K3S_BUILD_HOST "bash $K3S_BUNDLE_DIR/start-pod.bash $JOB_NAME $BUILD_NUMBER $image $BUNDLE_COLLECTION_VERSION"
 
 ssh $K3S_BUILD_HOST "bash $K3S_BUNDLE_DIR/run-in-pod.bash $JOB_NAME $BUILD_NUMBER root -" <<EOF
 
   apt-get update
+  apt-get install -y libssl-dev
   apt-get install -y python3-venv
   apt-get install -y build-essential
   apt-get install -y python3-dev
@@ -19,11 +23,11 @@ EOF
   
 ssh $K3S_BUILD_HOST "bash $K3S_BUNDLE_DIR/run-in-pod.bash $JOB_NAME $BUILD_NUMBER ubuntu -" <<EOF
 
-  python3 -m venv /opt/chipster/tools/umi-tools/venv
+  /opt/chipster/tools/python-3.8.11/bin/python3 -m venv /opt/chipster/tools/umi-tools/venv
   source /opt/chipster/tools/umi-tools/venv/bin/activate
   
-  pip install wheel
-  pip install umi_tools
+  pip3 install wheel
+  pip3 install umi_tools
   
   umi_tools --help
 EOF
