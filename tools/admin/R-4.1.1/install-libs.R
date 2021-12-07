c0fc6e4b1fc6337c121cb08771e632b37596002b
Petri Klemela <petri.klemela@csc.fi>
2021-12-07 12:20:02 +0200

    Add R-4.1.1-statistics

---
+++ tools/admin/R-4.1.1/install-libs.R
@@ -0,0 +1,13 @@
+
+# CRAN packages and their dependencies
+
+# this is only a copy of R-3.6.1-single-cell. The notes from the actual installation
+# seem to be lost
+
+cranPackages = c(
+#		"ggplot2",
+)
+
+for (package in cranPackages) {
+	install.packages(package=package)	
+}
