5d89f57f705efd8780fb22595aab7f9b187bce94
Petri Klemela <petri.klemela@csc.fi>
2021-12-22 11:54:12 +0200

    Unify path declarations

--- setup/bundles/builds/build-env.bash
+++ setup/bundles/builds/build-env.bash
@@ -2,5 +2,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-TOOLS_PATH="/opt/chipster/tools"
-TMPDIR_PATH="/opt/chipster/tools/tmp"
+# export these to environment variables so that these are available in ../scripts/run-in-pod.bash
+export TOOLS_PATH="/opt/chipster/tools"
+export TMPDIR_PATH="/opt/chipster/tools/.tmp"
--- setup/bundles/scripts/k3s-build-host/run-in-pod.bash
+++ setup/bundles/scripts/k3s-build-host/run-in-pod.bash
@@ -38,17 +38,20 @@ if [ "$command" == "-" ]; then
 fi
 
 # create temp dir for tool installations (start with "." to omit it for example when copying artefacts in binaries.bash with wildcard "*")
-TEMP_DIR="/opt/chipster/tools/.tmp"
-kubectl exec $kubectl_exec_opts $pod_name -- su - root -c "mkdir -p $TEMP_DIR; chown $user:$user $TEMP_DIR"
+kubectl exec $kubectl_exec_opts $pod_name -- su - root -c "mkdir -p $TMPDIR_PATH; chown $user:$user $TMPDIR_PATH"
 
 echo "** run as $user in $pod_name"
 
-# run strict mode to catch errors early
-kubectl exec $kubectl_exec_opts $pod_name -- su - $user -c "
+init_and_command="
   set -euo pipefail
   IFS=$'\n\t'
-  cd $TEMP_DIR
+  cd $TMPDIR_PATH
+  TOOLS_PATH="$TOOLS_PATH"
+  TMPDIR_PATH="$TMPDIR_PATH"
   $command"
 
+# run strict mode to catch errors early
+kubectl exec $kubectl_exec_opts $pod_name -- su - $user -c "$init_and_command"
+
 # delete TEMP_DIR after each run, otherwise we should think about its file owners more carefully
-kubectl exec $kubectl_exec_opts $pod_name -- su - root -c "rm -rf $TEMP_DIR"
+kubectl exec $kubectl_exec_opts $pod_name -- su - root -c "rm -rf $TMPDIR_PATH"
