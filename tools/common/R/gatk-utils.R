dd7595a444292baf0478a256db13d2a70b52d65d
Ari-Matti Saren <saren@csc.fi>
2022-03-08 23:52:34 +0000

    Fix the fix

--- tools/common/R/gatk-utils.R
+++ tools/common/R/gatk-utils.R
@@ -14,10 +14,7 @@ formatGatkVcf <- function(input.vcf,bam.names){
     
 	# Check if VCF chromosome names match BAM names
 	vcf.names <- getVCFNames(input.vcf)
-	if (vcf.names == bam.names){
-		# Naming schemes match
-		system(paste("mv output.vcf", input.vcf))
-	}else{
+	if (vcf.names != bam.names){
 		# Naming schemes don't match
 		if (bam.names == "1"){
 			# BAM names have "1"- Remove "chr" from VCF names.
@@ -26,6 +23,8 @@ formatGatkVcf <- function(input.vcf,bam.names){
 			# BAM names have "chr". Add "chr" to VCF names.
 			addChrToVCF(input.vcf, "output.vcf")
 		}
+		# Rename tmp file
+		system(paste("mv output.vcf", input.vcf))
 	}  
 
 	# Bgzip
