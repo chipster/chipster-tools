71c434533aee972f5edbe19d722b31d1884346b0
Petri Klemela <petri.klemela@csc.fi>
2022-04-05 09:35:38 +0300

    Add chipster-note test tool

---
+++ tools/misc/python/test-chipster-note.py
@@ -0,0 +1,6 @@
+# TOOL test-data-in-out3.py: "Test data input and output in Python3" (Data input output test.) 
+# INPUT input TYPE GENERIC
+# OUTPUT output
+# RUNTIME python3
+
+print("CHIPSTER-NOTE: This is a message from the wrapper script.")
