cef5a3d0e79aa96eac1fe9834594de87e5ba5d1f
Petri Klemela <petri.klemela@csc.fi>
2022-05-13 10:30:43 +0000

    Download mysql data

--- tools/misc/python/genome-download.py
+++ tools/misc/python/genome-download.py
@@ -1,6 +1,9 @@
 # TOOL genome-download.py: "Download genome from Ensembl" (Download genome fasta and gtf files from Ensembl.) 
-# OUTPUT output.fa.lz4
-# OUTPUT output.gtf.lz4
+# OUTPUT output.fa.gz
+# OUTPUT output.gtf
+# OUTPUT OPTIONAL coord_system.txt
+# OUTPUT OPTIONAL seq_region.txt
+# OUTPUT OPTIONAL karyotype.txt
 # PARAMETER species: Species TYPE STRING DEFAULT drosophila_melanogaster ()
 # PARAMETER version: Version TYPE STRING DEFAULT BDGP6.32 (Genome assembly version)
 # PARAMETER release: "Ensembl release" TYPE STRING DEFAULT current (Ensembl release number or "current")
@@ -20,13 +23,15 @@ import sys
 sys.path.append(os.getcwd() + "/../toolbox/tools")
 from common.python import tool_utils
 
+class DownloadError(Exception):
+    pass
 
 # Calls find_files and filters away some files that are typical for gtf
-def find_gtf_file(dir, file):
+def find_gtf_file(dir: str, file: str) -> str:
     return find_file(dir, file, ["abinitio", ".chr.gtf.gz", ".chr_patch"])
 
 # Finds files that are specified by its arguments first is directory and second is file (or part of a file name), rest of the arguments are expressions for grep to exclude e.g. '[\.]', ".gz", or "name". If a file is found from a directory its path is returned, otherwise raises an error
-def find_file(dir, file, ignore_names):
+def find_file(dir: str, file: str, ignore_names: [str]) -> str:
 
     try:
         url = urlparse(dir)
@@ -65,7 +70,7 @@ def find_file(dir, file, ignore_names):
     return dir + filtered_files[0]
 
 # Returns a path to a directory where a genome is found. First argument is TYPE.
-def get_dir(host, species, release, file_type):
+def get_dir(host: str, species: str, release: str, file_type: str) -> str:
 
     print("get_dir()", host, species, release, file_type)
 
@@ -80,7 +85,7 @@ def get_dir(host, species, release, file_type):
             dir = host + release + "/"+ file_type + "/"
     elif host.startswith("ftp://ftp.ensemblgenomes.org/"):
         # ftp://ftp.ensemblgenomes.org/pub/plants/current/fasta/populus_trichocarpa/dna/Populus_trichocarpa.JGI2.0.26.dna.toplevel.fa.gz
-        dir = host + release + "/" + file_type + "/"
+        dir = host + release + "/" + file_type + "/"		
 
     if host is "ftp://ftp.ensemblgenomes.org/pub/bacteria/":
         # iterate over collections to find the right one
@@ -102,6 +107,10 @@ def get_dir(host, species, release, file_type):
             
             i = i + 1
 
+    elif file_type == "mysql":
+        # ftp://ftp.ensembl.org/pub/current_mysql/drosophila_melanogaster_core_80_602/
+        dir = find_file(dir, species + "_core_", []) + "/"
+
     else:
         dir = dir + species + "/"
 
@@ -110,7 +119,7 @@ def get_dir(host, species, release, file_type):
 
     return dir
 
-def check_version(species, release, version, ftp_version):
+def check_version(species: str, release: str, version: str, ftp_version: str) -> str:
 
     print("checking " + species + " version " + version + " from release " + release)
 
@@ -126,26 +135,53 @@ def check_version(species, release, version, ftp_version):
             
             """)
 
-def download(host, species, release, ftp_release):
+def download(host: str, species: str, release: str, ftp_release: str) -> str:
     print("downloading " + species + " version " + version + " from release " + ftp_release)
 
+    # these must be the same than the output names in SADL
+    fasta_package = ".gz"
+    gtf_package = ""
+    mysql_package = ""
+
+    fasta_output = "output.fa" + fasta_package
+    gtf_output = "output.gtf" + gtf_package
+    coord_output = "coord_system.txt" + mysql_package
+    seq_output = "seq_region.txt" + mysql_package
+    karyotype_output = "karyotype.txt" + mysql_package
+
+    # download fasta
     fasta_dir = get_dir(host, species, release, "fasta")
     fasta_url = find_file(fasta_dir, ".dna.toplevel.fa.gz", [])
-    download_url(fasta_url, "output.fa.lz4")
+    download_url(fasta_url, fasta_output)
 
+    # download gtf
     gtf_dir = get_dir(host, species, release, "gtf")
     gtf_url = find_gtf_file(gtf_dir, ".gtf.gz")
-    download_url(gtf_url, "output.gtf.lz4")
+    download_url(gtf_url, gtf_output)
 
+    # download mysql data to find out primary chromosomes to filter fasta files
+    mysql_dir = get_dir(host, species, release, "mysql")
+
+    try:
+        download_url(mysql_dir + "coord_system.txt.gz", coord_output)
+        download_url(mysql_dir + "seq_region.txt.gz", seq_output)
+        download_url(mysql_dir + "karyotype.txt.gz", karyotype_output)
+    except DownloadError as e:
+        print("optional mysql data is not available (" + str(e) + ")")
+
+    # write proper filenames for the client
     ftp_fasta_filename = os.path.basename(fasta_url)
     ftp_gtf_filename = os.path.basename(gtf_url)
 
-    fasta_filename = tool_utils.remove_postfix(ftp_fasta_filename, '.gz') + ".lz4"
-    gtf_filename = tool_utils.remove_postfix(ftp_gtf_filename, '.gz') + ".lz4"
+    fasta_basename = tool_utils.remove_postfix(ftp_fasta_filename, '.gz')
+    gtf_basename = tool_utils.remove_postfix(ftp_gtf_filename, '.gz')
 
     output_names = {
-        'output.fa.lz4': fasta_filename,
-        'output.gtf.lz4': gtf_filename,
+        fasta_output: fasta_basename + fasta_package,
+        gtf_output: gtf_basename + gtf_package,
+        coord_output: gtf_basename + ".coord_system.txt" + mysql_package,
+        seq_output: gtf_basename + ".seq_region.txt" + mysql_package,
+        karyotype_output: gtf_basename + ".karyotype.txt" + mysql_package,
     }
 
     tool_utils.write_output_definitions(output_names)
@@ -153,18 +189,34 @@ def download(host, species, release, ftp_release):
     print("done " + species + " version " + ftp_release)
 
 # convert bytes to human readable string
-def to_hr(byte_size: int):
+def to_hr(byte_size: int) -> str:
     return str(round(byte_size / 1024 / 1024, 1)) + " MiB"
 
 def download_url(url: str, file_name: str):
 
     print("download url " + url  + " to " + file_name)
 
-    bash_cmd = "curl -s " + url + " | gunzip | lz4 > " + file_name
+    bash_cmd = "set -euxo pipefail; curl -s " + url
+
+    if file_name.endswith(".lz4"):
+        bash_cmd += " | gunzip | lz4"
+    elif file_name.endswith(".bgz"):
+        bash_cmd += " | gunzip | bgzip"
+    elif file_name.endswith(".gz"):
+        # original files are already .gz compressed
+        pass
+    else:
+        # extract other types
+        bash_cmd += " | gunzip "
+        
+    bash_cmd += " > " + file_name
     bash_process = subprocess.run(["bash", "-c", bash_cmd])
 
     if bash_process.returncode != 0:
-        raise RuntimeError("download failed with exit code: " + bash_process.returncode + ", command: " + bash_cmd)
+        # delete empty fail if the download failed (e.g. trying if there is mysql data for this species)
+        if os.path.exists(file_name):
+            os.remove(file_name)
+        raise DownloadError("download failed with exit code: " + str(bash_process.returncode) + ", command: " + bash_cmd)
 
     #print("download url " + url  + " to " + file_name)
     # parsed_url = urlparse(url)
@@ -210,7 +262,7 @@ def download_url(url: str, file_name: str):
     #     print("download completed")
 
 
-def get_host(ftp_site):
+def get_host(ftp_site: str) -> str:
     if ftp_site == "ensembl.org":
         return "ftp://ftp.ensembl.org/pub/"
     elif ftp_site == "fungi":
@@ -222,7 +274,7 @@ def get_host(ftp_site):
     else:
         raise RuntimeError("unknown ftp_site " + ftp_site)
 
-def parse_gtf_url(gtf_url):
+def parse_gtf_url(gtf_url: str) -> (str, str, str):
     # name 
     # (Drosophila_melanogaster.BDGP6.80)
     basename = os.path.basename(gtf_url).replace("\\.gtf\\.gz$", "")
