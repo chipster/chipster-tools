9cb39ec103a3f24da83cde2f3555f7d6b6ec4752
mlehtiva <maria.lehtivaara@csc.fi>
2022-07-21 15:14:59 +0300

    Modify to work on SCTransformed data

--- tools/ngs/R/single-cell-seurat-diffexp-samples.R
+++ tools/ngs/R/single-cell-seurat-diffexp-samples.R
@@ -1,8 +1,9 @@
-# TOOL single-cell-seurat-diffexp-samples.R: "Seurat v4 -Find conserved cluster markers and DE genes in two samples" (This tool lists the cell type markers that are conserved across the two conditions, and the differentially expressed genes between the two conditions for a user defined cluster. In case of more than 2 samples, each sample is compared to all the other samples. This tool can be used for Seurat objects with 2 or more samples in them.) 
+# TOOL single-cell-seurat-diffexp-samples.R: "Seurat v4 -Find conserved cluster markers and DE genes in multiple samples" (This tool lists the cell type markers that are conserved across the two conditions, and the differentially expressed genes between the two conditions for a user defined cluster. In case of more than 2 samples, each sample is compared to all the other samples. This tool can be used for Seurat objects with 2 or more samples in them.) 
 # INPUT OPTIONAL combined_seurat_obj.Robj: "Combined Seurat object" TYPE GENERIC
 # OUTPUT OPTIONAL de-list.tsv
 # OUTPUT OPTIONAL de-list_{...}.tsv
 # OUTPUT OPTIONAL conserved_markers.tsv
+# PARAMETER OPTIONAL normalisation.method: "Normalisation method used previously" TYPE [LogNormalize:"Global scaling normalization", SCT:"SCTransform"] DEFAULT LogNormalize (Which normalisation method was used in preprocessing, Global scaling normalization \(default, NormalizeData function used\) or SCTransform.)
 # PARAMETER cluster: "Name of the cluster" TYPE STRING DEFAULT 3 (Name of the cluster of which you want to identify the differentially expressed of. By default, the clusters are named with numbers starting from 0.)
 # PARAMETER OPTIONAL only.positive: "Return only positive marker genes" TYPE [FALSE, TRUE] DEFAULT TRUE (Tool only returns positive markers as default. Change the parameter here if you want to also include the negative markers.)
 # PARAMETER OPTIONAL logFC.conserved: "Fold change threshold for conserved markers in log scale" TYPE DECIMAL FROM 0 TO 5 DEFAULT 0.25 (Genes with an average fold change smaller than this are not included in the analysis. )
@@ -12,12 +13,16 @@
 # RUNTIME R-4.1.0-single-cell
 
 
+# TESTAA VM:llä!!
+
+
 # 2018-16-05 ML 
 # 11.07.2019 ML Seurat v3
 # 23.09.2019 EK Add only.pos = TRUE
 # 30.10.2019 ML Add filtering parameters
 # 02.12.2019 EK Change FC filter to use logfc.threshold prefiltering, add adjusted p-value filtering for DE genes
 # 2021-10-04 ML Update to Seurat v4
+# 2022-07-21 ML Tune for SCTransform data
 
 library(Seurat)
 
@@ -28,6 +33,10 @@ if (exists("seurat_obj")) {
   data.combined <- seurat_obj
 }
 
+# When SCTransform was used to normalise the data, do a prep step:
+if (normalisation.method == "SCT"){
+  data.combined <- PrepSCTFindMarkers(data.combined)
+}
 
 # Check that this cluster is available in data:
 
@@ -61,8 +70,11 @@ if (length(lvls) < 2) {
 } else if (length(lvls) == 2) { 
   ident1 <- paste(cluster, "_", lvls[1], sep = "")
   ident2 <- paste(cluster, "_", lvls[2], sep = "")
-  cluster_response <- FindMarkers(data.combined, ident.1 = ident1, ident.2 = ident2, verbose = FALSE, logfc.threshold = logFC.de)
-
+  if (normalisation.method == "SCT"){
+    cluster_response <- FindMarkers(data.combined, assay = "SCT", ident.1 = ident1, ident.2 = ident2, verbose = FALSE, logfc.threshold = logFC.de)
+  } else { 
+    cluster_response <- FindMarkers(data.combined, ident.1 = ident1, ident.2 = ident2, verbose = FALSE, logfc.threshold = logFC.de)
+  }
   # Filter DE genes based on adj p-val:
   de2 <- subset(cluster_response, (p_val_adj < pval.cutoff.de))
 
@@ -77,7 +89,11 @@ if (length(lvls) < 2) {
     ident1 <- paste(cluster, "_", lvls[i], sep = "")
     ident2 <- paste(cluster, "_", lvls[-i], sep = "")
     cluster_response <- FindMarkers(data.combined, ident.1 = ident1, ident.2 = ident2, verbose = FALSE, logfc.threshold = logFC.de)
-
+    if (normalisation.method == "SCT"){
+      cluster_response <- FindMarkers(data.combined, assay = "SCT", ident.1 = ident1, ident.2 = ident2, verbose = FALSE, logfc.threshold = logFC.de)
+    } else { 
+    cluster_response <- FindMarkers(data.combined, ident.1 = ident1, ident.2 = ident2, verbose = FALSE, logfc.threshold = logFC.de)
+    }
     # Filter DE genes based on adj p-val:
     de2 <- subset(cluster_response, (p_val_adj < pval.cutoff.de) )
 
