f5ccda1f097cbdfb7bc39d9dacd599d221846722
Eero Saarro <eero.saarro@csc.fi>
2022-12-22 13:26:08 +0000

    added tryrc parameter assign taxonomy

--- tools/ngs/R/dada2-assign-taxonomy.R
+++ tools/ngs/R/dada2-assign-taxonomy.R
@@ -6,6 +6,7 @@
 # OUTPUT OPTIONAL taxa_seqtab_combined.tsv
 # OUTPUT OPTIONAL taxonomy_assignment.tsv
 # PARAMETER boot: "The minimum bootstrap confidence for assigning a taxonomic level" TYPE INTEGER FROM 0 TO 100 DEFAULT 50 (The minimum bootstrap confidence for assigning a taxonomic level)
+# PARAMETER tryrc: "Try the reverse-complement of each sequence for classification if it is a better match to the reference sequences" TYPE [yes, no] DEFAULT no (If set to yes, use the reverse-complement of each sequences for classification if it is a better match to the reference sequences than the forward sequence.)
 # PARAMETER species: "Exact species level assignment?" TYPE [yes, no] DEFAULT yes (Do you want to assign the sequences to the species level if there is an exact match 100% identity between ASVs and sequenced reference strains?)
 # PARAMETER combine_tables: "Combine the taxonomy and the sequence table" TYPE [yes,no] DEFAULT yes (If set to yes, it combines the taxonomy and the sequence/ASV table into one .tsv file, otherwise the tsv file consist only of the taxonomy table.)
 # RUNTIME R-4.1.1-asv
@@ -31,9 +32,16 @@ if (file.exists("taxa_reference.fasta")){
     path1 <- c(file.path(chipster.tools.path,"dada2-silva-reference","silva_nr99_v138.1_train_set.fa"))
     #path1 <- c(file.path(chipster.tools.path,"dada2-silva-reference","silva_nr99_v138.1_wSpecies_train_set.fa"))
 }
+if (tryrc == "yes"){
+    tryrc = TRUE
+}else{
+    tryrc=FALSE
+}
+
+
 set.seed(100) # Initialize random number generator for reproducibility
 # run command assignTaxonomy verbose not important
-taxa <- assignTaxonomy(seqtab.nochim, path1, minBoot=boot, multithread=as.integer(chipster.threads.max), tryRC = TRUE)
+taxa <- assignTaxonomy(seqtab.nochim, path1, minBoot=boot, multithread=as.integer(chipster.threads.max), tryRC = tryrc)
 
 
 #check if parameter species yes, otherwise skip species level assignment. Use reference file if selected otherwise Silva v.138.1
@@ -50,7 +58,7 @@ if (species=="yes"){
     
     }
     # run addSpecies()
-    taxa <- addSpecies(taxa, path2, tryRC=TRUE)
+    taxa <- addSpecies(taxa, path2, tryRC=tryrc)
 }
 
 # save the taxonomy table as taxonomy-assignment-matrix.Rda
