8d643fdb6ab12ed121d8df13414f2e590c83594c
Lauren Gerber <lauren.gerber@csc.fi>
2023-02-10 14:35:51 +0000

    Add slots 2

--- tools/ngs/R/single-cell-seurat-diffexp-chosen-samples.R
+++ tools/ngs/R/single-cell-seurat-diffexp-chosen-samples.R
@@ -10,7 +10,7 @@
 # PARAMETER OPTIONAL logFC.de: "Fold change threshold for differentially expressed genes in log scale" TYPE DECIMAL FROM 0 TO 5 DEFAULT 0.25 (Genes with an average fold change smaller than this are not included in the analysis.)
 # PARAMETER OPTIONAL pval.cutoff.de: "Adjusted p-value cutoff for differentially expressed genes" TYPE DECIMAL FROM 0 TO 1 DEFAULT 0.05 (Cutoff for the adjusted p-value of the DE genes: by default, adjusted p-values bigger than 0.05 are filtered out.)
 # RUNTIME R-4.2.0-single-cell
-
+# SLOTS 2
 
 # 2018-16-05 ML 
 # 11.07.2019 ML Seurat v3
