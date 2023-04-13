b2a977d5e92976c568a59a83bbc82119537e9f04
Petri Klemela <petri.klemela@csc.fi>
2023-04-13 10:10:23 +0300

    Test container R

---
+++ tools/misc/R/test-r-in-container-without-tools-bin.R
@@ -0,0 +1,8 @@
+# TOOL test-r-in-container-without-tools-bin.R: "Test R in container image without tools-bin" (Data input output test.)
+# INPUT input: "input" TYPE GENERIC (Just some test file)
+# OUTPUT output
+# RUNTIME R-4.2.3
+# TOOLS-BIN ""
+
+system("cp input output")
+print("hello! hei!")
