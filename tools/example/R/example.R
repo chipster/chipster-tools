78cd52e3965fe32a649e11ef4aabf0f3c49a5336
Taavi Hupponen <taavi.hupponen@csc.fi>
2023-08-16 12:43:02 +0300

    Move example tools to example dir

--- tools/example/R/example.R
+++ tools/example/R/example.R
@@ -1,4 +1,4 @@
-# TOOL example.R: "Provide template for tools" (Provide template for tools)
+# TOOL example.R: "R template for tools" (R template for tools)
 # PARAMETER OPTIONAL int: Integer TYPE INTEGER FROM -100 TO 100 DEFAULT 0
 # PARAMETER OPTIONAL dec: Decimal TYPE DECIMAL FROM -0.1 TO 1.0 DEFAULT 0
 # PARAMETER OPTIONAL string: String TYPE STRING
