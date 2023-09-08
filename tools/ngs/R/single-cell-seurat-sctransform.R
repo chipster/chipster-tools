2059e811a2eb529b70e4120d5a8c8f34d1c46228
Iida Hakulinen <iida.hakulinen@csc.fi>
2023-09-08 11:23:31 +0000

    Add ribosomal filtering to sc and st tools

--- tools/ngs/R/single-cell-seurat-sctransform.R
+++ tools/ngs/R/single-cell-seurat-sctransform.R
@@ -1,11 +1,12 @@
-# TOOL single-cell-seurat-sctransform.R: "Seurat v4 -SCTransform: Filter cells, normalize, regress and detect variable genes" (This tool filters out dead cells, empties and doublets. It then normalizes gene expression values using the SCTransform method, detects highly variable genes, scales the data and regresses out unwanted variation based on the number of UMIs and mitochondrial transcript percentage. You can also choose to regress out variation due to cell cycle heterogeneity.)  
+# TOOL single-cell-seurat-sctransform.R: "Seurat v4 -SCTransform: Filter cells, normalize, regress and detect variable genes" (This tool filters out dead cells, empties and doublets. It then normalizes gene expression values using the SCTransform method, detects highly variable genes, scales the data and regresses out unwanted variation based on the number of UMIs and mitochondrial transcript percentage. You can also choose to regress out variation due to cell cycle heterogeneity.)
 # INPUT OPTIONAL seurat_obj.Robj: "Seurat object" TYPE GENERIC
 # OUTPUT OPTIONAL seurat_obj_sctransform.Robj
-# OUTPUT OPTIONAL Dispersion_plot.pdf 
+# OUTPUT OPTIONAL Dispersion_plot.pdf
 # OUTPUT OPTIONAL log.txt
 # PARAMETER OPTIONAL mingenes: "Filter out cells which have less than this many genes expressed" TYPE INTEGER DEFAULT 200 (Filter out empties. The cells to be kept must express at least this number of genes.)
 # PARAMETER OPTIONAL genecountcutoff: "Filter out cells which have more than this many genes expressed" TYPE INTEGER DEFAULT 2500 (Filter out multiplets. The cells to be kept must express less than this number of genes.)
 # PARAMETER OPTIONAL mitocutoff: "Filter out cells which have higher mitochondrial transcript percentage" TYPE DECIMAL FROM 0 TO 100 DEFAULT 5 (Filter out dead cells. The cells to be kept must have lower percentage of mitochondrial transcripts than this.)
+# PARAMETER OPTIONAL minribo: "Filter out cells which have lower ribosomal transcript percentage" TYPE DECIMAL FROM 0 TO 100 (Filter out cells that have lower ribosomal transcript percentage.)
 # PARAMETER OPTIONAL num.features: "Number of variable genes to return" TYPE INTEGER DEFAULT 3000 (Number of features to select as top variable features, i.e. how many features returned. For SCTransform, the recommended default is 3000.)
 # PARAMETER OPTIONAL filter.cell.cycle: "Regress out cell cycle differences" TYPE [no:no, all.diff:"all differences", diff.phases:"the difference between the G2M and S phase scores"] DEFAULT no (Would you like to regress out cell cycle scores during data scaling? If yes, should all signal associated with cell cycle be removed, or only the difference between the G2M and S phase scores.)
 # RUNTIME R-4.2.3-single-cell
@@ -14,12 +15,13 @@
 
 
 # 2020-06-17 ML
-# 2020-10-11 EK Unified parameter descriptions with the corresponding normalization tool 
+# 2020-10-11 EK Unified parameter descriptions with the corresponding normalization tool
 # 2020-12-18 ML Always compute the cell-cycle scoring and plot the PCA + Remove the plot titles, as they started giving errors.
 # 2021-10-04 ML Update to Seurat v4
 # 2023-02-01 ML Add 5 slots
 # 2023-04-06 LG Remove 2 slots - Discrepancy in number of slots added v. removed
 # 2023-02-01 ML Return to the original 2 slots
+# 2023-09-08 IH add ribosomal filtering
 
 # Source: https://github.com/satijalab/seurat/issues/1679
 
@@ -33,7 +35,7 @@ library(gplots)
 load("seurat_obj.Robj")
 
 # Open the pdf file for plotting
-pdf(file="Dispersion_plot.pdf", width=13, height=7) 
+pdf(file = "Dispersion_plot.pdf", width = 13, height = 7)
 
 # For cell cycle filtering, read in a list of cell cycle markers, from Tirosh et al, 2015
 cc.genes <- readLines(con = file.path(chipster.tools.path, "seurat/regev_lab_cell_cycle_genes.txt"))
@@ -42,12 +44,12 @@ s.genes <- cc.genes[1:43]
 g2m.genes <- cc.genes[44:97]
 
 # Subset: remove potential empties, multiplets and broken cells based on parameters.
-seurat_obj <- subset(seurat_obj, subset = nFeature_RNA > mingenes & nFeature_RNA < genecountcutoff & percent.mt < mitocutoff)	
+seurat_obj <- subset(seurat_obj, subset = nFeature_RNA > mingenes & nFeature_RNA < genecountcutoff & percent.mt < mitocutoff & percent.rb > minribo)
 
 # SCTransform:
 # Note that this single command replaces NormalizeData, ScaleData, and FindVariableFeatures.
 # seurat_obj <- SCTransform(seurat_obj, assay = 'RNA', new.assay.name = 'SCT', vars.to.regress = c('percent.mt', 'nFeature_RNA', 'nCount_RNA'), variable.features.n = num.features, verbose = FALSE)
-seurat_obj <- SCTransform(seurat_obj, assay = 'RNA', new.assay.name = 'SCT', vars.to.regress = c('percent.mt'), variable.features.n = num.features, verbose = FALSE)
+seurat_obj <- SCTransform(seurat_obj, assay = "RNA", new.assay.name = "SCT", vars.to.regress = c("percent.mt"), variable.features.n = num.features, verbose = FALSE)
 
 
 # Dispersion plot:
@@ -58,7 +60,7 @@ plot1 <- VariableFeaturePlot(seurat_obj)
 plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
 CombinePlots(plots = list(plot1, plot2))
 
-textplot(paste("\v \v Number of \n \v \v variable \n \v \v genes: \n \v \v", length(VariableFeatures(object = seurat_obj)), " \n  \n \v \v Number of \n \v \v cells: \n \v \v", length(colnames(x = seurat_obj))), halign="center", valign="center", cex=2)
+textplot(paste("\v \v Number of \n \v \v variable \n \v \v genes: \n \v \v", length(VariableFeatures(object = seurat_obj)), " \n  \n \v \v Number of \n \v \v cells: \n \v \v", length(colnames(x = seurat_obj))), halign = "center", valign = "center", cex = 2)
 
 
 # Cell cycle genes, get the scores & visualize:
@@ -66,57 +68,56 @@ textplot(paste("\v \v Number of \n \v \v variable \n \v \v genes: \n \v \v", len
 # http://satijalab.org/seurat/cell_cycle_vignette.html#regress-out-cell-cycle-scores-during-data-scaling
 
 # Check that there were some S or G2M genes in the list of variable genes:
-if (length(s.genes[!is.na(match(s.genes, VariableFeatures(object = seurat_obj)))]) <1 && length(g2m.genes[!is.na(match(g2m.genes, VariableFeatures(object = seurat_obj)))]) <1 ) {
-	# Write a log file (instead of ending with a Chipster-note, because we want the tool to finish and give the plot, when possible.)
-	fileConn<-file("log.txt")
-	writeLines(c("There are not enough cell cycle genes for correction in the list of variable genes."), fileConn)
-	close(fileConn)
-
-} else{
+if (length(s.genes[!is.na(match(s.genes, VariableFeatures(object = seurat_obj)))]) < 1 && length(g2m.genes[!is.na(match(g2m.genes, VariableFeatures(object = seurat_obj)))]) < 1) {
+    # Write a log file (instead of ending with a Chipster-note, because we want the tool to finish and give the plot, when possible.)
+    fileConn <- file("log.txt")
+    writeLines(c("There are not enough cell cycle genes for correction in the list of variable genes."), fileConn)
+    close(fileConn)
+} else {
     # Perform cell cycle analysis (make sure to specify the "assay" parameter)
-	seurat_obj <- CellCycleScoring(object = seurat_obj, s.features = s.genes, g2m.features = g2m.genes, assay = 'SCT', set.ident = TRUE)
-		
-	# Visualize in PCA:
-	# PCA plot 1: without/before filtering cell cycle effect
-	seurat_obj <- RunPCA(seurat_obj, features = c(s.genes, g2m.genes))
-	plot1 <- DimPlot(seurat_obj) #, plot.title = "PCA on cell cycle genes") 
-	# PCAPlot(seurat_obj, plot.title = "PCA on cell cycle genes") 
+    seurat_obj <- CellCycleScoring(object = seurat_obj, s.features = s.genes, g2m.features = g2m.genes, assay = "SCT", set.ident = TRUE)
 
-	# Cell cycle stage filtering:
-	if( filter.cell.cycle != "no" ) {	
-	# Remove the cell cycle scores:
+    # Visualize in PCA:
+    # PCA plot 1: without/before filtering cell cycle effect
+    seurat_obj <- RunPCA(seurat_obj, features = c(s.genes, g2m.genes))
+    plot1 <- DimPlot(seurat_obj) # , plot.title = "PCA on cell cycle genes")
+    # PCAPlot(seurat_obj, plot.title = "PCA on cell cycle genes")
 
-		# Option 1: remove all the difference:
-		if (filter.cell.cycle == "all.diff"){
+    # Cell cycle stage filtering:
+    if (filter.cell.cycle != "no") {
+        # Remove the cell cycle scores:
+
+        # Option 1: remove all the difference:
+        if (filter.cell.cycle == "all.diff") {
             # Normalise again with SCTransform, but this time including also the cell cycle scores
-            seurat_obj <- SCTransform(seurat_obj, assay = 'RNA', new.assay.name = 'SCT', vars.to.regress = c('percent.mt', 'nFeature_RNA', 'nCount_RNA', 'S.Score', 'G2M.Score'), variable.features.n = num.features, verbose = FALSE)
+            seurat_obj <- SCTransform(seurat_obj, assay = "RNA", new.assay.name = "SCT", vars.to.regress = c("percent.mt", "nFeature_RNA", "nCount_RNA", "S.Score", "G2M.Score"), variable.features.n = num.features, verbose = FALSE)
 
-            # PCA plot 2A: after filtering, all:	
-			seurat_obj <- RunPCA(object = seurat_obj, features = c(s.genes, g2m.genes), do.print = FALSE)
-			plot2 <- DimPlot(seurat_obj) #, plot.title = "After cell cycle correction (method: remove all)")
-			# PCAPlot(seurat_obj, plot.title = "After cell cycle correction (method: remove all)")
-			CombinePlots(plots = list(plot1, plot2))
+            # PCA plot 2A: after filtering, all:
+            seurat_obj <- RunPCA(object = seurat_obj, features = c(s.genes, g2m.genes), do.print = FALSE)
+            plot2 <- DimPlot(seurat_obj) # , plot.title = "After cell cycle correction (method: remove all)")
+            # PCAPlot(seurat_obj, plot.title = "After cell cycle correction (method: remove all)")
+            CombinePlots(plots = list(plot1, plot2))
 
-		# Option 2: regressing out the difference between the G2M and S phase scores:	
-		}else if (filter.cell.cycle == "diff.phases"){
-			seurat_obj$CC.Difference <- seurat_obj$S.Score - seurat_obj$G2M.Score
-            seurat_obj <- SCTransform(seurat_obj, assay = 'RNA', new.assay.name = 'SCT', vars.to.regress = c('percent.mt', 'nFeature_RNA', 'nCount_RNA', 'CC.Difference'), variable.features.n = num.features, verbose = FALSE)
+            # Option 2: regressing out the difference between the G2M and S phase scores:
+        } else if (filter.cell.cycle == "diff.phases") {
+            seurat_obj$CC.Difference <- seurat_obj$S.Score - seurat_obj$G2M.Score
+            seurat_obj <- SCTransform(seurat_obj, assay = "RNA", new.assay.name = "SCT", vars.to.regress = c("percent.mt", "nFeature_RNA", "nCount_RNA", "CC.Difference"), variable.features.n = num.features, verbose = FALSE)
 
-			# PCA plot 2B: after filtering, difference:	
-			seurat_obj <- RunPCA(object = seurat_obj, features  = c(s.genes, g2m.genes), do.print = FALSE)
-			plot2 <- DimPlot(seurat_obj) #, plot.title = "After cell cycle correction (method: difference between G2M and S phases)")
-			# PCAPlot(seurat_obj, plot.title = "After cell cycle correction (method: difference between G2M and S phases)")
-			CombinePlots(plots = list(plot1, plot2))
-		}
-	# just plot the 1 PCA plot, if no filtering: 
-	} else { 
-		DimPlot(seurat_obj) #, plot.title = "PCA on cell cycle genes") 
-	} 
-} 
+            # PCA plot 2B: after filtering, difference:
+            seurat_obj <- RunPCA(object = seurat_obj, features = c(s.genes, g2m.genes), do.print = FALSE)
+            plot2 <- DimPlot(seurat_obj) # , plot.title = "After cell cycle correction (method: difference between G2M and S phases)")
+            # PCAPlot(seurat_obj, plot.title = "After cell cycle correction (method: difference between G2M and S phases)")
+            CombinePlots(plots = list(plot1, plot2))
+        }
+        # just plot the 1 PCA plot, if no filtering:
+    } else {
+        DimPlot(seurat_obj) # , plot.title = "PCA on cell cycle genes")
+    }
+}
 
 dev.off() # close the pdf
 
 # Save the Robj for the next tool
-save(seurat_obj, file="seurat_obj_sctransform.Robj")
+save(seurat_obj, file = "seurat_obj_sctransform.Robj")
 
 ## EOF
--- tools/ngs/R/spatial-transcriptomics-seurat-setup.R
+++ tools/ngs/R/spatial-transcriptomics-seurat-setup.R
@@ -1,7 +1,7 @@
 # TOOL spatial-transcriptomics-seurat-setup.R: "Seurat v4 -Setup and QC" (Setup the Seurat object from spatial data.)
 # INPUT OPTIONAL files.tar: "tar package of 10X output files" TYPE GENERIC
 # OUTPUT OPTIONAL seurat_spatial_setup.Robj
-# OUTPUT OPTIONAL QC_plots.pdf 
+# OUTPUT OPTIONAL QC_plots.pdf
 # PARAMETER OPTIONAL sample_name: "Name for the sample" TYPE STRING DEFAULT "slice1" (Name for the sample in multisample analysis. Make sure the samples are named differently if you have multiple samples.)
 # RUNTIME R-4.2.3-single-cell
 # SLOTS 2
@@ -11,6 +11,7 @@
 # 2022-10-13 ML Coordinates to integers -check and input folder handling
 # 2022-10-18 ML Add samplename to a metadata field
 # 2022-11-01 ML Add top expressed genes boxplot
+# 2023-09-08 IH add percent.rb to QC
 
 library(Seurat)
 library(ggplot2)
@@ -26,9 +27,9 @@ system("tar tf files.tar > tar.contents 2>> log.txt")
 file.list <- scan("tar.contents", what = "", sep = "\n")
 
 # Check that the input is a valid tar file
-  if (length(file.list) == 0) {
-    stop(paste("CHIPSTER-NOTE: ", "It seems your input file is not a valid Tar package. Please check your input file."))
-  }
+if (length(file.list) == 0) {
+  stop(paste("CHIPSTER-NOTE: ", "It seems your input file is not a valid Tar package. Please check your input file."))
+}
 
 # make an output folder
 system("mkdir output_folder")
@@ -42,20 +43,20 @@ system("mkdir input_folder; cd input_folder; tar xf ../files.tar --xform='s#^.+/
 
 
 # code for manipulating aggregation files (not needer atm)
-#if (file.exists("aggregation.csv") & file.exists("aggr_tissue_positions_list.csv")) {
-    #aggregation splitting step based on the aggregation.csv
-    #aggregation.csv, aggr_tissue_position_list.csv -> tissue_positions_list.csv }
+# if (file.exists("aggregation.csv") & file.exists("aggr_tissue_positions_list.csv")) {
+# aggregation splitting step based on the aggregation.csv
+# aggregation.csv, aggr_tissue_position_list.csv -> tissue_positions_list.csv }
 
-#rename and move filtered_feature_bc_matrix.h5
+# rename and move filtered_feature_bc_matrix.h5
 file1 <- list.files("input_folder", pattern = "feature_bc_matrix.h5", full.names = TRUE)
-file1 <- paste("mv",file1,"output_folder/filtered_feature_bc_matrix.h5") 
+file1 <- paste("mv", file1, "output_folder/filtered_feature_bc_matrix.h5")
 system(file1)
 
-#rename and move tissue_positions_list.csv 
+# rename and move tissue_positions_list.csv
 tissue_positions <- list.files("input_folder", pattern = "tissue_positions", full.names = TRUE)
 if (file.exists(tissue_positions)) {
-        tissue_positions <- paste("mv",tissue_positions,"output_folder/spatial/tissue_positions_list.csv") 
-        system(tissue_positions)
+  tissue_positions <- paste("mv", tissue_positions, "output_folder/spatial/tissue_positions_list.csv")
+  system(tissue_positions)
 }
 
 # copy image files to spatial subdir
@@ -64,13 +65,13 @@ file.copy("input_folder/tissue_hires_image.png", "output_folder/spatial")
 file.copy("input_folder/tissue_lowres_image.png", "output_folder/spatial")
 file.copy("input_folder/tissue_positions.csv", "output_folder/spatial/tissue_positions_list.csv")
 
-#Load spatial data, returns a Seurat object
-#slice = name for the stored image of the tissue slice later used in the analysis
+# Load spatial data, returns a Seurat object
+# slice = name for the stored image of the tissue slice later used in the analysis
 seurat_obj <- Load10X_Spatial(data.dir = "output_folder", assay = "Spatial", slice = sample_name)
 
 # Sometimes the coordinates of the spatial image are characters instead of integers.
 # Check this and switch them to intergers if need be:
-if (class(seurat_obj@images[[sample_name]]@coordinates[["tissue"]]) == "character" ) {
+if (class(seurat_obj@images[[sample_name]]@coordinates[["tissue"]]) == "character") {
   seurat_obj@images[[sample_name]]@coordinates[["tissue"]] <- as.integer(seurat_obj@images[[sample_name]]@coordinates[["tissue"]])
   seurat_obj@images[[sample_name]]@coordinates[["row"]] <- as.integer(seurat_obj@images[[sample_name]]@coordinates[["row"]])
   seurat_obj@images[[sample_name]]@coordinates[["col"]] <- as.integer(seurat_obj@images[[sample_name]]@coordinates[["col"]])
@@ -80,31 +81,34 @@ if (class(seurat_obj@images[[sample_name]]@coordinates[["tissue"]]) == "characte
 
 # Add samplename to metadata field (for plotting)
 seurat_obj <- RenameIdents(seurat_obj, "SeuratProject" = sample_name)
-seurat_obj@meta.data$orig.ident <- sample_name 
+seurat_obj@meta.data$orig.ident <- sample_name
 
 
 # Open the pdf file for plotting
-pdf(file="QC_plots.pdf", width=13, height=7)
+pdf(file = "QC_plots.pdf", width = 13, height = 7)
 
-seurat_obj <- PercentageFeatureSet(seurat_obj, "^mt-", col.name = "percent_mito")
-seurat_obj <- PercentageFeatureSet(seurat_obj, "^Hb.*-", col.name = "percent_hb")
+seurat_obj[["percent.mt"]] <- PercentageFeatureSet(seurat_obj, pattern = "^MT-|^mt-|^Mt-")
+seurat_obj[["percent.hb"]] <- PercentageFeatureSet(seurat_obj, pattern = "^Hb.*-")
+seurat_obj[["percent.rb"]] <- PercentageFeatureSet(seurat_obj, pattern = "^RPS|^RPL|^rps|^rpl|^Rps|^Rpl")
 
-VlnPlot(seurat_obj, features = c("nCount_Spatial",  "nFeature_Spatial"), pt.size = 0.1, ncol = 2) + NoLegend()
-VlnPlot(seurat_obj, features = c("percent_mito","percent_hb"), pt.size = 0.1, ncol = 2) + NoLegend()
-SpatialFeaturePlot(seurat_obj, c("nCount_Spatial", "nFeature_Spatial", "percent_mito")) # + theme(legend.position = "right")
+VlnPlot(seurat_obj, features = c("nCount_Spatial", "nFeature_Spatial"), pt.size = 0.1, ncol = 2) + NoLegend()
+VlnPlot(seurat_obj, features = c("percent.mt", "percent.hb", "percent.rb"), pt.size = 0.1, ncol = 2) + NoLegend()
+SpatialFeaturePlot(seurat_obj, c("nCount_Spatial", "nFeature_Spatial", "percent.mt", "percent.rb")) # + theme(legend.position = "right")
 
 # Top expressing genes
 # Code from: https://nbisweden.github.io/workshop-scRNAseq/labs/compiled/seurat/seurat_07_spatial.html#Top_expressed_genes)
 C <- seurat_obj@assays$Spatial@counts
-C@x = C@x/rep.int(colSums(C), diff(C@p))
+C@x <- C@x / rep.int(colSums(C), diff(C@p))
 most_expressed <- order(Matrix::rowSums(C), decreasing = T)[20:1]
-boxplot(as.matrix(t(C[most_expressed, ])), cex = 0.1, las = 1, xlab = "% total count per spot",
-    col = (scales::hue_pal())(20)[20:1], horizontal = TRUE)
+boxplot(as.matrix(t(C[most_expressed, ])),
+  cex = 0.1, las = 1, xlab = "% total count per spot",
+  col = (scales::hue_pal())(20)[20:1], horizontal = TRUE
+)
 
 # close the pdf
- dev.off() 
+dev.off()
 
 # Save the Robj for the next tool
 save(seurat_obj, file = "seurat_spatial_setup.Robj")
 
-#EOF
+# EOF
