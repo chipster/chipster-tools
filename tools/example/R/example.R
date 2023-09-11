0af7d707c9f2f83217f2e4943d9a6bbecbf2ecd1
Taavi Hupponen <taavi.hupponen@csc.fi>
2023-09-11 11:19:34 +0300

    Improve example.R

--- tools/example/R/example.R
+++ tools/example/R/example.R
@@ -8,7 +8,10 @@
 # PARAMETER OPTIONAL metacolumn: "Phenodata column" TYPE METACOLUMN_SEL
 # PARAMETER OPTIONAL file: File TYPE INPUT_SEL
 
-# VARIABLES, examples values
+# Chipster variables
+# ------------------------------------------------------------------------------
+# Variables created by the R job. Use these to for example constract paths to
+# applications and other R scripts.
 chipster.tools.path # "/opt/chipster-web-server/tools"
 chipster.common.path # "../toolbox/tools/common/R"
 chipster.common.lib.path # "../toolbox/tools/common/R/lib"
@@ -19,15 +22,40 @@ chipster.threads.max # "2"
 chipster.memory.max # "8192"
 
 
-
-
-# include utils
+# Use a library script file
+# ------------------------------------------------------------------------------
+# Library R files should go to tools/common/R/lib.
+# Load the library with:
 source(file.path(chipster.common.lib.path, "tool-utils.R"))
 
+# Use a function from the library script (no need for prefix or anything):
+read_input_definitions()
 
-# source(file.path(chipster.common.lib.path, "version-utils.R"))
-documentVersion("R", R.version.string)
 
+# Document application version
+# ------------------------------------------------------------------------------
+# Store the version of the application used by a script. Application version
+# information is stored in with the job in the session db and it's visible for
+# the users in the job details.
+#
+# Version functions are implemented in the version-utils.R, which is always
+# sourced at the start of a R job, so no need to explicitly source
+# version-utils.R.
+#
+# First argument is the name of the application and the second is the string
+# containing version information. The string can have multiple lines.
+documentVersion("bowtie", version.string)
+
+
+# Notify user with Chipster note
+# ------------------------------------------------------------------------------
+# Use Chipster note to notify the user in a situation where running a script
+# can't continue, but the user can do something to fix the problem.
+#
+
+if (param.count < 2) {
+    stop(paste("CHIPSTER-NOTE:", "Count needs to greater than 2"))
+}
 
 
 
@@ -38,13 +66,8 @@ documentVersion("R", R.version.string)
 # - same display name?
 # - empty description?
 
-# how to call a function from own lib file
-
-
-# chipster-note
 
 # output rename
 
-# tool versions
 
 # unzip
