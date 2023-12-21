165b8cba5ce651b9d3e2126296f0453bb52ef779
Iida Hakulinen <iida.hakulinen@csc.fi>
2023-12-21 14:52:01 +0000

    add more manuals and fix merge

--- tools/ngs/R/single-cell-seurat-merge-samples.R
+++ tools/ngs/R/single-cell-seurat-merge-samples.R
@@ -7,6 +7,7 @@
 # PARAMETER OPTIONAL normalisation.method: "Normalization method to perform" TYPE [LogNormalize:"Global scaling normalization", SCT:"SCTransform"] DEFAULT LogNormalize (Normalize data with global scaling normalization or SCTransform.)
 # PARAMETER OPTIONAL totalexpr: "Scaling factor in the normalization" TYPE INTEGER DEFAULT 10000 (Scale each cell to this total number of transcripts.)
 # PARAMETER OPTIONAL num.features: "Number of variable genes to return" TYPE INTEGER DEFAULT 2000 (Number of features to select as top variable features, i.e. how many features returned.)
+# PARAMETER OPTIONAL num.of.pcas: "Number of PCs to compute" TYPE INTEGER DEFAULT 50 (How many principal components to compute and store. If you get an error message, try lowering the number. This might happen especially if you have low number of cells in your data.)
 # PARAMETER OPTIONAL num.of.heatmaps: "Number of principal components to plot as heatmaps" TYPE INTEGER DEFAULT 12 (How many principal components to plot as heatmaps.)
 # PARAMETER OPTIONAL loadings: "Print loadings in a file" TYPE [TRUE: yes, FALSE: no] DEFAULT FALSE (Print the PC loadings to a txt file.)
 # PARAMETER OPTIONAL num.of.genes.loadings: "Number of genes to list in the loadings file" TYPE INTEGER DEFAULT 5 (How many genes to list in the loadings txt file.)
@@ -45,13 +46,21 @@ cc.genes <- readLines(con = file.path("/opt/chipster/tools/seurat/regev_lab_cell
 # We can segregate this list into markers of G2/M phase and markers of S phase
 s.genes <- cc.genes[1:43]
 g2m.genes <- cc.genes[44:97]
+# Open the pdf file for plotting
+pdf(file = "Dispersion_plot.pdf", width = 13, height = 7)
 
 # Normalisation
 if (normalisation.method == "SCT") {
-    seurat_obj <- SCTransform(seurat_obj, assay = "RNA", new.assay.name = "SCT", vars.to.regress = c("percent.mt"), variable.features.n = num.features, verbose = FALSE)
-    #print(VariableFeatures(seurat_obj))
-    #seurat_obj <- FindVariableFeatures(seurat_obj, selection.method = "vst", nfeatures = num.features)
-    #VariableFeatures(seurat_obj) <- features 
+    seurat_obj <- SCTransform(seurat_obj, assay = "RNA", new.assay.name = "SCT", vars.to.regress = c("percent.mt"), variable.features.n = num.features, verbose = FALSE,  ncells = 5000)
+    seurat_obj <- SCTResults(seurat_obj, slot = "feature.attributes")
+    # to be added
+    # Dispersion plot:
+    # Identify the 10 most highly variable genes
+    #top10 <- head(VariableFeatures(seurat_obj), 10)
+    # Plot variable features with and without labels
+    #plot1 <- VariableFeaturePlot(seurat_obj)
+    #plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+    #CombinePlots(plots = list(plot1, plot2))
 } else if (normalisation.method == "LogNormalize") {
     seurat_obj <- NormalizeData(object = seurat_obj, normalization.method = "LogNormalize", scale.factor = totalexpr)
     # Detection of variable genes across the single cells
@@ -60,22 +69,16 @@ if (normalisation.method == "SCT") {
 
     ## Scaling:
     seurat_obj <- ScaleData(object = seurat_obj, vars.to.regress = c("nCount_RNA", "percent.mt"), verbose = FALSE)
-    print(seurat_obj)
+
+    # Dispersion plot:
+    # Identify the 10 most highly variable genes
+    top10 <- head(VariableFeatures(seurat_obj), 10)
+    # Plot variable features with and without labels
+    plot1 <- VariableFeaturePlot(seurat_obj)
+    plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+    CombinePlots(plots = list(plot1, plot2))
 }
 
-# Open the pdf file for plotting
-pdf(file = "Dispersion_plot.pdf", width = 13, height = 7)
-
-#print(DefaultAssay(seurat_obj))
-
-# Dispersion plot:
-# Identify the 10 most highly variable genes
-#top10 <- head(VariableFeatures(seurat_obj), 10)
-# Plot variable features with and without labels
-#dim(VariableFeatures(seurat_obj))
-#plot1 <- VariableFeaturePlot(seurat_obj)
-#plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
-#CombinePlots(plots = list(plot1, plot2))
 
 textplot(paste("\v \v Number of \n \v \v variable \n \v \v genes: \n \v \v", length(VariableFeatures(object = seurat_obj)), " \n  \n \v \v Number of \n \v \v cells: \n \v \v", length(colnames(x = seurat_obj))), halign = "center", valign = "center", cex = 2)
 
@@ -96,17 +99,14 @@ if (length(s.genes[!is.na(match(s.genes, VariableFeatures(seurat_obj)))]) < 1 &&
         seurat_obj[["joined"]] <- JoinLayers(seurat_obj[["RNA"]])
         DefaultAssay(seurat_obj) <- "joined"
     }
-    print(Layers(seurat_obj))
-    print(DefaultAssay(seurat_obj))
-    seurat_obj <- CellCycleScoring(seurat_obj,s.features = s.genes,g2m.features = g2m.genes, set.ident = TRUE)
-    head(seurat_obj[[]])
+
+    seurat_obj <- CellCycleScoring(object = seurat_obj, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
+
     # Visualize in PCA:
     # PCA plot 1: without/before filtering cell cycle effect
     seurat_obj <- RunPCA(seurat_obj, features = c(s.genes, g2m.genes))
-    print("eka")
-    #Dimplot(seurat_obj)
     plot1 <- DimPlot(seurat_obj) + ggtitle("PCA on cell cycle genes (no cell cycle regression)") # reduction = pca
-    print("toka")
+
     # Cell cycle stage filtering:
     if (filter.cell.cycle != "no") {
         # Remove the cell cycle scores:
@@ -130,21 +130,51 @@ if (length(s.genes[!is.na(match(s.genes, VariableFeatures(seurat_obj)))]) < 1 &&
             plot2 <- DimPlot(seurat_obj) + ggtitle("After cell cycle correction (method: G2M / S difference)")
             CombinePlots(plots = list(plot1, plot2))
         }
-    # just plot the 1 PCA plot, if no filtering:
+        # just plot the 1 PCA plot, if no filtering:
     } else {
-        print("kolmas")
         DimPlot(seurat_obj) # , plot.title = "PCA on cell cycle genes")
     }
-    print(DefaultAssay(seurat_obj))
-    # change back to original assay
     if (normalisation.method == "LogNormalize") {
         DefaultAssay(seurat_obj) <- "RNA"
     } else if (normalisation.method == "SCT") {
         DefaultAssay(seurat_obj) <- "SCT"
     }
 }
+
 dev.off() # close the pdf
 
+# run PCA
+# The variable genes are used as input
+seurat_obj <- RunPCA(seurat_obj, features = VariableFeatures(object = seurat_obj), npcs = num.of.pcas)
+
+# PCA genes in txt file
+if (loadings == TRUE) {
+    sink("PCAloadings.txt")
+    print(seurat_obj[["pca"]], dims = 1:num.of.pcas, nfeatures = num.of.genes.loadings)
+    sink()
+}
+
+# PDF plots
+pdf(file = "PCAplots.pdf", , width = 9, height = 12)
+VizDimLoadings(seurat_obj, dims = 1:2, reduction = "pca") + ggtitle("Top 30 genes associated with PCs 1 & 2")
+DimPlot(seurat_obj, reduction = "pca", group.by = "orig.ident") # orig.ident = otherwise colors based on cell cycle stages
+
+# Need to check the number of cells at this point.
+cells_left <- length(colnames(x = seurat_obj))
+if (cells_left > 500) {
+    DimHeatmap(seurat_obj, dims = 1, cells = 500, balanced = TRUE) #+ ggtitle("Heatmap for PC1")
+    DimHeatmap(seurat_obj, dims = 1:num.of.heatmaps, cells = 500, balanced = TRUE) #+ ggtitle("Heatmaps for N first PCs")
+} else {
+    DimHeatmap(seurat_obj, dims = 1, cells = cells_left, balanced = TRUE) #+ ggtitle("Heatmap for PC1")
+    DimHeatmap(seurat_obj, dims = 1:num.of.heatmaps, cells = cells_left, balanced = TRUE) #+ ggtitle("Heatmaps for N first PCs")
+}
+# fig.height=12,fig.width=9
+ElbowPlot(seurat_obj, ndims = num.of.pcas) + ggtitle("Amount of variation in the data explained by each PC")
+
+# Number of cells:
+textplot(paste("\v \v Number of \n \v \v cells: \n \v \v", length(colnames(x = seurat_obj))), halign = "center", valign = "center", cex = 2) # , cex=0.8
+
+dev.off() # close the pdf
 
 # Save the Robj for the next tool
 save(seurat_obj, file = "seurat_obj_merged.Robj")
