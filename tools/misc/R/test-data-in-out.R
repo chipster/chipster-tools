52590997b6b99c9b8554767f345564e2e19cacd0
Petri Klemela <petri.klemela@csc.fi>
2024-01-17 10:58:26 +0000

    Remove more unnecessary test tools

--- tools/misc/R/test-data-in-out.R
+++ tools/misc/R/test-data-in-out.R
@@ -1,6 +1,15 @@
 # TOOL test-data-in-out.R: "Test data input and output in R" (Data input output test.)
 # INPUT input: "input" TYPE GENERIC (Just some test file)
 # OUTPUT output
+# PARAMETER delay: Delay TYPE INTEGER FROM 0 TO 10000 DEFAULT 1 (Delay in seconds)
 
 system("cp input output")
-print("hello! ")
+
+print("Sleeping")
+
+for (i in 1:delay) {
+    print(paste("Sleeping ", i))
+    Sys.sleep(1)
+}
+
+print("Waking up")
--- tools/misc/python/sleep.py
+++
@@ -1,9 +0,0 @@
-# TOOL sleep.py: "Sleep in Python" ()
-# PARAMETER delay: Delay TYPE INTEGER FROM 0 TO 1000 DEFAULT 10 (Delay in seconds)
-
-import time
-# cannot use tool_utils in python2
-
-print("Start : %s" % time.ctime())
-time.sleep(delay)
-print("End : %s" % time.ctime())
