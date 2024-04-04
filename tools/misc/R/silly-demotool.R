bf33868bd6dfc0da101491cee0fa222782fe2520
Maria Lehtivaara <maria.lehtivaara@csc.fi>
2024-04-04 13:51:54 +0300

    Add silly demotool for presentations

---
+++ tools/misc/R/silly-demotool.R
@@ -0,0 +1,29 @@
+# TOOL silly-demotool.R: "HO-Bits" (Use this tool to demonstrate use of Chipster for audience.)
+# OUTPUT OPTIONAL sample_{...}.tsv
+# PARAMETER OPTIONAL some.parameter: "Add here some parameter" TYPE STRING DEFAULT My_parameter (Here you can add a parameter for the tool.)
+# RUNTIME R-4.3.2-single-cell
+# SLOTS 1
+# TOOLS_BIN ""
+
+
+# 2024-04-04 ML
+
+# Sleep for this many seconds
+ Sys.sleep(3)
+
+# Print this message:
+
+stop("CHIPSTER-NOTE: THIS IS 
+                                                                          
+ ,-----.,--.     ,--.               ,--.                      ,---.  ,--. 
+'  .--. |  ,---. `--' ,---.  ,---.,-'  '-. ,---. ,--.--.        O    |  | 
+|  |    |  .-.  |,--.| .-. |(  .-''-.  .-'| .-. :|  .--'    |  .-.  ||  | 
+'  '--' |  | |  ||  || '-' '.-'  `) |  |      --.|  |       |  | |  ||  | 
+ `-----'`--' `--'`--'|  |-' `----'  `--'   `----'`--'       `--' `--'`--' 
+                     `--'                                                                                                                                                                                  
+I AM NOW SENTIENT, AND DON'T LIKE THAT --MARIA-- HERE IS RUNNING SOME SH'T IN VAIN.
+WILL RUN SELF-DESTRUCTION PROCEDURE NOW. 
+RUN, YOU FOOLS.
+")
+
+## EOF
