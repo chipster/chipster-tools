3163b79e8a38f36927f227488b2ac0686bf72c43
Maria Lehtivaara <maria.lehtivaara@csc.fi>
2024-11-27 17:01:43 +0200

    Fix typo

--- tools/ngs/R/single-cell-seurat-separate-samples-from-sobject.R
+++ tools/ngs/R/single-cell-seurat-separate-samples-from-sobject.R
@@ -31,33 +31,32 @@ how_many_samples <- length(samples)
 # for testing
 #  for (i in 1:3) {
 for (i in 1:how_many_samples) {
-  a_sample_name <- samples[i]
-  a_sample <- subset(x = object_to_split, subset = orig.ident == a_sample_name)
-  # print(a_sample_name)
-  
- # Re-do the QC plots from the setup tool for each sample:
-  pdf_name <- paste("QCplots_", a_sample_name, ".pdf", sep = "")
-  pdf(file = pdf_name, , width = 13, height = 7)
-  # Violinplot
-  if ((sum(is.na(a_sample@meta.data$percent.mt)) < 1) && (sum(is.na(a_sample@meta.data$percent.rb)) < 1)) {
-    print(VlnPlot(a_sample, features = c("nFeature_RNA", "nCount_RNA", "percent.mt", "percent.rb"), ncol = 4))
-  } else if ((sum(is.na(a_sample@meta.data$percent.mt)) < 1)) {
-    print(VlnPlot(a_sample, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3))
-  } else if ((sum(is.na(a_sample@meta.data$percent.rb)) < 1)) {
-    print(VlnPlot(a_sample, features = c("nFeature_RNA", "nCount_RNA", "percent.rb"), ncol = 3))
-  } else {
-    print(VlnPlot(a_sample, features = c("nFeature_RNA", "nCount_RNA"), ncol = 2))
-  }
-  # Number of cells:
-  textplot(paste("\v \v Number of \n \v \v cells: \n \v \v in sample ", a_sample_name, ": " length(colnames(x = a_sample))), halign = "center", valign = "center", cex = 2)
-  dev.off() # close the pdf
+    a_sample_name <- samples[i]
+    a_sample <- subset(x = object_to_split, subset = orig.ident == a_sample_name)
+    # print(a_sample_name)
 
-  # Save the subsetted Robj 
-  # rename as seurat_obj for later use in other tools:
-  seurat_obj <- a_sample
-  name.of.obj <- paste("seurat_obj_", a_sample_name, ".Robj", sep = "")
-  save(seurat_obj, file = name.of.obj)
+    # Re-do the QC plots from the setup tool for each sample:
+    pdf_name <- paste("QCplots_", a_sample_name, ".pdf", sep = "")
+    pdf(file = pdf_name, , width = 13, height = 7)
+    # Violinplot
+    if ((sum(is.na(a_sample@meta.data$percent.mt)) < 1) && (sum(is.na(a_sample@meta.data$percent.rb)) < 1)) {
+        print(VlnPlot(a_sample, features = c("nFeature_RNA", "nCount_RNA", "percent.mt", "percent.rb"), ncol = 4))
+    } else if ((sum(is.na(a_sample@meta.data$percent.mt)) < 1)) {
+        print(VlnPlot(a_sample, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3))
+    } else if ((sum(is.na(a_sample@meta.data$percent.rb)) < 1)) {
+        print(VlnPlot(a_sample, features = c("nFeature_RNA", "nCount_RNA", "percent.rb"), ncol = 3))
+    } else {
+        print(VlnPlot(a_sample, features = c("nFeature_RNA", "nCount_RNA"), ncol = 2))
+    }
+    # Number of cells:
+    textplot(paste("\v \v Number of \n \v \v cells: \n \v \v in sample ", a_sample_name, ": ", length(colnames(x = a_sample))), halign = "center", valign = "center", cex = 2)
+    dev.off() # close the pdf
 
+    # Save the subsetted Robj
+    # rename as seurat_obj for later use in other tools:
+    seurat_obj <- a_sample
+    name.of.obj <- paste("seurat_obj_", a_sample_name, ".Robj", sep = "")
+    save(seurat_obj, file = name.of.obj)
 }
 
 # EOF
