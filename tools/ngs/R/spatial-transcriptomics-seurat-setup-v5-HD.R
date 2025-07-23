9e88f04f0ce536f33b94b2b7cd42b1c1838aba8a
mlehtiva <maria.lehtivaara@csc.fi>
2025-07-23 14:28:00 +0000

    testing

--- tools/ngs/R/spatial-transcriptomics-seurat-setup-v5-HD.R
+++ tools/ngs/R/spatial-transcriptomics-seurat-setup-v5-HD.R
@@ -88,10 +88,10 @@ bin_sizes <- as.numeric(unlist(strsplit(bin_sizes, ",")))
 # open tar:
 # system("mkdir input_folder; cd input_folder; tar -xvzf ../files.tar")
 system("mkdir input_folder; cd input_folder; tar -xzf ../files.tar")
-system("cd input_folder; cd binned_outputs; rm -rf square_002um; rm -rf square_008um/analysis; rm -rf square_008um/raw*; rm -rf square_012um/analysis; rm -rf square_012um/raw*; cd square_008um; ls; cd spatial; ls; cd ..; cd ..; ls")
+system("cd input_folder; cd binned_outputs; rm -rf square_002um; rm -rf square_008um/analysis; rm -rf square_008um/raw*; rm -rf square_012um/analysis; rm -rf square_012um/raw*; cd square_008um; pwd; ls; cd spatial; pwd; ls; cd ..; cd ..; pwd; ls")
 
 
-seurat_obj <- Load10X_Spatial(data.dir = "input_folder", assay = "Spatial", slice = sample_name, bin.size = bin_sizes) # bin.size = c(8, 16)
+seurat_obj <- Load10X_Spatial(data.dir = "input_folder/binned_outputs", assay = "Spatial", slice = sample_name, bin.size = bin_sizes) # bin.size = c(8, 16)
 
 # Sometimes the coordinates of the spatial image are characters instead of integers.
 # Check this and switch them to intergers if need be:
