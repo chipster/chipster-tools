be8467cb50a762c785e73ed83a7374e6db4cc19a
mlehtiva <maria.lehtivaara@csc.fi>
2025-07-25 08:08:57 +0000

    test

--- tools/ngs/R/spatial-transcriptomics-seurat-setup-v5-HD.R
+++ tools/ngs/R/spatial-transcriptomics-seurat-setup-v5-HD.R
@@ -87,17 +87,18 @@ bin_sizes <- as.numeric(unlist(strsplit(bin_sizes, ",")))
 
 # open tar:
 # system("mkdir input_folder; cd input_folder; tar -xvzf ../files.tar")
-# EI TEHÄ input folderia. Sillä tulee ikävästi extra folder kerros ja se aiheuttaa file not found -ongelman.
-system("mkdir input_folder; cd input_folder; tar -xzf ../files.tar")
-system("pwd; ls -lth; cd input_folder; ls -lth")
-system("cd input_folder; pwd; chmod -R o=r binned_outputs/; ls -lth")
 
-system("cd input_folder; pwd; ls -lth; cd binned_outputs; ls -lth; cd square_008um; pwd; ls -lth")
+# system("mkdir input_folder; cd input_folder; tar -xzf ../files.tar")
+# system("pwd; ls -lth; cd input_folder; ls -lth")
+# system("cd input_folder; pwd; chmod -R o=r binned_outputs/; ls -lth")
+# system("cd input_folder; pwd; ls -lth; cd binned_outputs; ls -lth; cd square_008um; pwd; ls -lth")
 
 # system("cd input_folder; cd binned_outputs; rm -rf square_002um; rm -rf square_008um/analysis; rm -rf square_008um/raw*; rm -rf square_012um/analysis; rm -rf square_012um/raw*; cd square_008um; pwd; ls; cd spatial; pwd; ls; cd ..; cd ..; pwd; ls")
 
+# EI TEHÄ input folderia. Sillä tulee ikävästi extra folder kerros ja se aiheuttaa file not found -ongelman.
+system("tar -xzf ../files.tar")
 
-seurat_obj <- Load10X_Spatial(data.dir = "input_folder/binned_outputs/", assay = "Spatial", slice = sample_name, bin.size = bin_sizes) # bin.size = c(8, 16)
+seurat_obj <- Load10X_Spatial(data.dir = "binned_outputs/", assay = "Spatial", slice = sample_name, bin.size = bin_sizes) # bin.size = c(8, 16)
 
 # Sometimes the coordinates of the spatial image are characters instead of integers.
 # Check this and switch them to intergers if need be:
