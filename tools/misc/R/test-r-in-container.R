a9c14ce02a04b1c33a527c55d4264b4acd40c5c9
Petri Klemela <petri.klemela@csc.fi>
2025-08-26 09:59:39 +0000

    Remove remaining usage of old R version

--- tools/misc/R/test-r-in-container.R
+++ tools/misc/R/test-r-in-container.R
@@ -1,7 +1,7 @@
 # TOOL test-r-in-container.R: "Test R in container image" (Data input output test.)
 # INPUT input: "input" TYPE GENERIC (Just some test file)
 # OUTPUT output
-# RUNTIME R-4.2.3
+# RUNTIME R-4.4.3
 
 system("cp input output")
 print("hello! hei!")
