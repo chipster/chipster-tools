ef6df109b0eaaa149749c96c7cfeaa5a5b613a0e
Petri Klemela <petri.klemela@csc.fi>
2025-12-05 17:21:19 +0200

    Remove some unused files from R-3.2.3

--- setup/bundles/builds/R-3.2.3.bash
+++ setup/bundles/builds/R-3.2.3.bash
@@ -18,8 +18,18 @@ bash $BUNDLE_SCRIPTS_DIR/start-pod.bash $JOB_NAME $BUILD_NUMBER $image \"$BUNDLE
 
 bash $BUNDLE_SCRIPTS_DIR/run-in-pod.bash $JOB_NAME $BUILD_NUMBER ubuntu - <<EOF
 
-  echo download and extract
+  pwd
+  whoami
+  ls -lah
+
+  echo download and extract  
   lz4 -d /mnt/artefacts/downloads/R-3.2.3_ubuntu-16.04_2019-07-30.tar.lz4 -c | tar x -C $TOOLS_PATH
+
+  # only used in MEDIPS.R which is commented away
+  rm -rf $TOOLS_PATH/R-3.2.3/lib/R/library/BSgenome.*
+
+  # one of the largest files, but most likely unused, because the name is not familiar for tool developers
+  rm -rf $TOOLS_PATH/R-3.2.3/lib/R/library/ChemmineDrugs
 
   ls -lah $TOOLS_PATH/
 
