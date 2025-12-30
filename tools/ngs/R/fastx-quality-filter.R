4d627c9bf62188d565f0aa120de3865ca6cfaad2
ekorpela <ekorpelainen@gmail.com>
2025-12-30 14:31:00 +0000

    Update to runtime R-4.5.1-fastx

--- tools/ngs/R/fastx-quality-filter.R
+++ tools/ngs/R/fastx-quality-filter.R
@@ -4,8 +4,8 @@
 # OUTPUT quality-filtered.log
 # PARAMETER quality: "Quality cut-off value" TYPE INTEGER FROM 1 TO 100 DEFAULT 20 (What is the minimum quality score to keep.)
 # PARAMETER percentage: "Minimum percent of bases that must have that quality" TYPE INTEGER FROM 1 TO 100 DEFAULT 90 (Percent of bases in sequence that must have quality equal to or higher than the cut-off value.)
-# PARAMETER quality.format: "Quality value format used" TYPE [sanger: Sanger, illuminaold: "Illumina GA v1.3-1.5"] DEFAULT sanger (What quality encoding is used in your FASTQ file. Select Sanger if your data comes from Illumina 1.8 or later, SOLiD or 454.)
-
+# RUNTIME R-4.5.1-fastx
+# TOOLS_BIN ""
 
 # EK 28.6.2011
 # AMS 11.3.2014, gzip fastq outputs
@@ -15,11 +15,10 @@ source(file.path(chipster.common.lib.path, "zip-utils.R"))
 unzipIfGZipFile("reads.fastq")
 
 # binary
-binary <- c(file.path(chipster.tools.path, "fastx", "bin", "fastq_quality_filter"))
+binary <- c(file.path("/opt/chipster/tools", "fastx", "bin", "fastq_quality_filter"))
 
 # command
-quality.scale <- ifelse(quality.format == "sanger", "-Q 33", "")
-command <- paste(binary, "-v", "-q", quality, "-p", percentage, quality.scale, "-i reads.fastq -o quality-filtered.fastq > quality-filtered.log")
+command <- paste(binary, "-v", "-q", quality, "-p", percentage, "-Q 33 -i reads.fastq -o quality-filtered.fastq > quality-filtered.log")
 
 # run
 system(command)
