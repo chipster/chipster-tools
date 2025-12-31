ec0ab6bf5de7ad066a3de52565051b1d0bde6b5e
ekorpela <ekorpelainen@gmail.com>
2025-12-31 08:32:19 +0000

    Remove quality parameter

--- tools/ngs/R/fastx-collapser.R
+++ tools/ngs/R/fastx-collapser.R
@@ -1,7 +1,6 @@
 # TOOL fastx-collapser.R: "Remove duplicate reads from FASTQ" (Collapses identical sequences in a FASTQ file into a single sequence. The sequences are renamed with sequence number and the multiplicity value. This tool is based on the FASTA/Q Collapser tool of the FASTX package.)
 # INPUT reads.fastq TYPE GENERIC
 # OUTPUT duplicates-removed.fastq
-# PARAMETER quality.format: "Quality value format used" TYPE [sanger: Sanger, illuminaold: "Illumina GA v1.3-1.5"] DEFAULT sanger (What quality encoding is used in your FASTQ file. Select Sanger if your data comes from Illumina 1.8 or later, SOLiD or 454.)
 # RUNTIME R-4.5.1-fastx
 # TOOLS_BIN ""
 
@@ -15,8 +14,7 @@ unzipIfGZipFile("reads.fastq")
 binary <- c(file.path("/opt/chipster/tools", "fastx", "bin", "fastx_collapser"))
 
 # command
-quality.scale <- ifelse(quality.format == "sanger", "-Q 33", "")
-command <- paste(binary, quality.scale, "-i reads.fastq -o duplicates-removed.fastq")
+command <- paste(binary, quality.scale, "-Q 33 -i reads.fastq -o duplicates-removed.fastq")
 
 # run
 system(command)
