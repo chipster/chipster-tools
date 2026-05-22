7045141777403d06485796aa67f1553663719fbc
Joonas Vornanen <joonas.vornanen@csc.fi>
2026-05-22 10:14:46 +0000

    Update single-cell-seurat-featureplot-v5.R

--- tools/ngs/R/single-cell-seurat-featureplot-v5.R
+++ tools/ngs/R/single-cell-seurat-featureplot-v5.R
@@ -1,7 +1,7 @@
 # TOOL single-cell-seurat-featureplot-v5.R: "Seurat v5 -Visualize features in UMAP plot" (Color single cells on a UMAP dimensional reduction plot according to a feature, i.e. gene expression, PC scores, number of genes detected, etc.)
 # INPUT seurat_obj.Robj: "Seurat object" TYPE GENERIC
 # OUTPUT OPTIONAL UMAPplot.pdf
-# PARAMETER OPTIONAL feature_to_plot: "Feature" TYPE [percent.mt, nCount_RNA, nFeature_RNA] DEFAULT percent.mt (Denotes which feature to use for coloring the cells.)
+# PARAMETER OPTIONAL feature_to_plot: "Feature" TYPE [percent.mt, nCount_RNA, nFeature_RNA, percent.rb] DEFAULT percent.mt (Denotes which feature to use for coloring the cells.)
 # PARAMETER OPTIONAL point.size: "Point size in UMAP plot" TYPE DECIMAL DEFAULT 1 (Point size for UMAP plot. )
 # PARAMETER OPTIONAL add.labels: "Add labels on top of clusters in plot" TYPE [TRUE:yes, FALSE:no] DEFAULT FALSE (Add cluster number on top of the cluster in UMAP plot.)
 # PARAMETER OPTIONAL plotting.order.used: "Plotting order of cells based on expression" TYPE [TRUE:yes, FALSE:no] DEFAULT FALSE (Plot cells in the the order of expression. Can be useful to turn this on if cells expressing given feature are getting buried.)
@@ -14,6 +14,7 @@
 # 09.09.2019 ML remove optins for orig.ident, PC1, PC2
 # 2021-10-04 ML Update to Seurat v4
 # 2023-10-25 IH Update to Seurat v5
+# 2026-05-22 JV Added "percent.rb" as a feature_to_plot
 
 library(Seurat)
 library(gplots)
