bf116f67dd9db97ea093a244b684a93edc493c22
Joonas Vornanen <joonas.vornanen@csc.fi>
2026-05-25 07:09:27 +0000

    Task 2 in progress...

--- tools/ngs/R/single-cell-seurat-featureplot-v5.R
+++ tools/ngs/R/single-cell-seurat-featureplot-v5.R
@@ -1,9 +1,11 @@
 # TOOL single-cell-seurat-featureplot-v5.R: "Seurat v5 -Visualize features in UMAP plot" (Color single cells on a UMAP dimensional reduction plot according to a feature, i.e. gene expression, PC scores, number of genes detected, etc.)
 # INPUT seurat_obj.Robj: "Seurat object" TYPE GENERIC
 # OUTPUT OPTIONAL UMAPplot.pdf
-# PARAMETER OPTIONAL feature_to_plot: "Feature" TYPE [percent.mt, nCount_RNA, nFeature_RNA] DEFAULT percent.mt (Denotes which feature to use for coloring the cells.)
+# PARAMETER OPTIONAL feature_to_plot: "Feature" TYPE [percent.mt, nCount_RNA, nFeature_RNA, percent.rb] DEFAULT percent.mt (Denotes which feature to use for coloring the cells.)
 # PARAMETER OPTIONAL point.size: "Point size in UMAP plot" TYPE DECIMAL DEFAULT 1 (Point size for UMAP plot. )
 # PARAMETER OPTIONAL add.labels: "Add labels on top of clusters in plot" TYPE [TRUE:yes, FALSE:no] DEFAULT FALSE (Add cluster number on top of the cluster in UMAP plot.)
+# PARAMETER OPTIONAL add.colors: "Add custom colors for clusters" TYPE [red, blue, green, pink] DEFAULT pink (tesktiä)
+# PARAMETER OPTIONAL label.size: "sajflksaj" TYPE INTEGER DEFAULT 5 (Valitse numero)
 # PARAMETER OPTIONAL plotting.order.used: "Plotting order of cells based on expression" TYPE [TRUE:yes, FALSE:no] DEFAULT FALSE (Plot cells in the the order of expression. Can be useful to turn this on if cells expressing given feature are getting buried.)
 # RUNTIME R-4.3.2-single-cell
 # TOOLS_BIN ""
@@ -28,9 +30,9 @@ if (exists("data.combined")) {
 
 add.labels <- as.logical(add.labels)
 
-# Plot tSNE
+# Plot UMAP
 pdf(file = "UMAPplot.pdf")
-FeaturePlot(object = seurat_obj, features = feature_to_plot, pt.size = point.size, label = add.labels, order = as.logical(plotting.order.used))
+FeaturePlot(object = seurat_obj, features = feature_to_plot, pt.size = point.size, label.size = label.size, label.color = add.colors, label = add.labels, order = as.logical(plotting.order.used))
 dev.off() # close the pdf
 
 # EOF
