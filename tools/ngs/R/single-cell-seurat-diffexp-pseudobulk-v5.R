02ff0cd5fb5ce8e51127f3f9d537f623b43a8012
Maria Lehtivaara <maria.lehtivaara@csc.fi>
2026-09-16 10:52:03 +0300

    Update the tests according to which are actually suitable for pseudobulk

--- tools/ngs/R/single-cell-seurat-diffexp-pseudobulk-v5.R
+++ tools/ngs/R/single-cell-seurat-diffexp-pseudobulk-v5.R
@@ -1,6 +1,5 @@
-# TOOL single-cell-seurat-diffexp-pseudobulk-v5.R: "Seurat v5 -Find DE genes between sample groups, pseudobulk" (This tool lists the differentially expressed genes between user defined sample groups for a user defined cluster. This tool can be used for Seurat objects with more than 2 samples in them.)
+# TOOL single-cell-seurat-diffexp-pseudobulk-v5.R: "Seurat v5 -Find DE genes between sample groups, pseudobulk" (This tool lists the differentially expressed genes between user defined sample groups for a user defined cluster. This tool can be used for Seurat objects with more than 2 samples in them. Note that )
 # INPUT OPTIONAL combined_seurat_obj.Robj: "Combined Seurat object" TYPE GENERIC
-# OUTPUT OPTIONAL de-list.tsv
 # OUTPUT OPTIONAL pseudobulk_de-list_{...}.tsv
 # OUTPUT OPTIONAL expressionPlots.pdf
 # OUTPUT OPTIONAL pseudobulk_markers_NAs.tsv
@@ -10,7 +9,7 @@
 # PARAMETER samples1: "Name of the sample group to compare with" TYPE STRING DEFAULT "STIM" (Name of the sample group of which you want to identify the differentially expressed genes of.)
 # PARAMETER samples2: "Name of the sample group to compare to" TYPE STRING DEFAULT "CTRL" (Name of the sample group of which you want to identify the differentially expressed genes of.)
 # PARAMETER cluster: "Name of the cluster" TYPE STRING DEFAULT 3 (Name of the cluster of which you want to identify the differentially expressed genes of. By default, the clusters are named with numbers starting from 0.)
-# PARAMETER OPTIONAL test.type: "Which test to use for detecting marker genes" TYPE [wilcox, DESeq2, bimod, roc, t, tobit, poisson, negbinom] DEFAULT wilcox (Seurat currently implements Wilcoxon rank sum test, bimod \(likelihood-ratio test for single cell gene expression\), roc \(standard AUC classifier\), Students t-test, Tobit-test, poisson, negbinom and DESeq2. The latter three should be used on UMI datasets only, and assume an underlying poisson or negative-binomial distribution. Note that DESeq2 can sometimes give as results NAs for some genes.)
+# PARAMETER OPTIONAL test.type: "Which test to use for detecting marker genes" TYPE [negbinom, DESeq2, wilcox, t, poisson] DEFAULT negbinom (Seurat currently implements Wilcoxon rank sum test, bimod \(likelihood-ratio test for single cell gene expression\), roc \(standard AUC classifier\), Students t-test, Tobit-test, poisson, negbinom and DESeq2. The latter three should be used on UMI datasets only, and assume an underlying poisson or negative-binomial distribution.  DESeq2 and negbinom are recommended for pseudobulk analysis. Wilcoxon and t-test require a larger number of samples per group \(≥5\) to have adequate power. Bimod, tobit, and roc are designed for single-cell data and are not appropriate here.)
 # PARAMETER OPTIONAL only.positive: "Return only positive marker genes" TYPE [FALSE, TRUE] DEFAULT TRUE (Tool only returns positive markers as default. Change the parameter here if you want to also include the negative markers.)
 # PARAMETER OPTIONAL logFC.de: "Fold change threshold for differentially expressed genes in log2 scale" TYPE DECIMAL FROM 0 TO 5 DEFAULT 0.25 (Genes with an average fold change smaller than this are not included in the analysis.)
 # PARAMETER OPTIONAL pval.cutoff.de: "Adjusted p-value cutoff for differentially expressed genes" TYPE DECIMAL FROM 0 TO 1 DEFAULT 0.05 (Cutoff for the adjusted p-value of the DE genes: by default, adjusted p-values bigger than 0.05 are filtered out.)
@@ -51,7 +50,7 @@ if (normalisation.method == "SCT") {
 # Rename clusters, because if they are just numbers, Seurat adds a "g" in front of the name,
 # which can be confusing and looks stupid in the plots.
 # data.combined$seurat_clusters[1:5]
-data.combined$seurat_clusters <- paste("cluster", data.combined$seurat_clusters, sep="")
+data.combined$seurat_clusters <- paste("cluster", data.combined$seurat_clusters, sep = "")
 
 # Organise the levels alphabetically (as for some reason, they are not by default)
 data.combined$stim <- as.ordered(data.combined$stim)
@@ -64,13 +63,13 @@ sel.clust <- "seurat_clusters"
 data.combined <- SetIdent(data.combined, value = sel.clust)
 # table(data.combined@active.ident)
 
-# Aggregate 
-# bulk <- AggregateExpression(data.combined,  return.seurat = T, assays = "RNA",  group.by = c("seurat_clusters", "stim", "type"), verbose = F) 
-# ei assays = "RNA", so that it works also for sct?
-bulk <- AggregateExpression(data.combined,  return.seurat = T, group.by = c("seurat_clusters", "stim", "type"), verbose = F) 
+# Aggregate
+# bulk <- AggregateExpression(data.combined,  return.seurat = T, assays = "RNA",  group.by = c("seurat_clusters", "stim", "type"), verbose = F)
+# Note: not assays = "RNA", so that it works also for sct
+bulk <- AggregateExpression(data.combined, return.seurat = T, group.by = c("seurat_clusters", "stim", "type"), verbose = F)
 
 # Subset based on cluster number
-cluster.bulk <- subset(bulk, seurat_clusters == paste("cluster",cluster, sep="")) 
+cluster.bulk <- subset(bulk, seurat_clusters == paste("cluster", cluster, sep = ""))
 
 # data.combined$seurat_clusters[1:5]
 # cluster.bulk$seurat_clusters[1:5]
@@ -87,33 +86,35 @@ Idents(cluster.bulk) <- "type"
 
 
 # Compute differential expression
+# For SCTransformed data:
 if (normalisation.method == "SCT") {
   #  save(cluster.bulk, file = "data_combined_pseudobulk.Robj")
   pseudobulk_markers <- FindMarkers(cluster.bulk, assay = "SCT", ident.1 = samples1, ident.2 = samples2, slot = "counts", , test.use = test.type, verbose = F, logfc.threshold = logFC.de, min.pct = minpct, recorrect_umi = FALSE, only.pos = only.positive)
   # Currently, it is unsure how to (best) use SCTransformed data with DESeq2:
   # https://github.com/satijalab/seurat/issues/7659
-  if (test.type == "DESeq2"){
+  if (test.type == "DESeq2") {
     stop("CHIPSTER-NOTE: Apologies, but currently in Seurat, DESeq2 won't work with SCTransformed data. Try with another method.")
   }
+  # For normalised data:
 } else {
   pseudobulk_markers <- FindMarkers(cluster.bulk, ident.1 = samples1, ident.2 = samples2, slot = "counts", test.use = test.type, verbose = F, min.pct = minpct, only.pos = only.positive, logfc.threshold = logFC.de)
 }
 
 # FindMarkers function gives NAs to some "outlier" genes when using DESeq.
 # https://bioconductor.org/packages/release/bioc/vignettes/DESeq2/inst/doc/DESeq2.html#pvaluesNA
-# "If a row contains a sample with an extreme count outlier then the p value and adjusted p value will be set to NA."" 
+# "If a row contains a sample with an extreme count outlier then the p value and adjusted p value will be set to NA.""
 
 # Sepate NAs to another table for user:
 pseudobulk_markers_NAs <- pseudobulk_markers[is.na(pseudobulk_markers$p_val_adj), ]
-if(length(pseudobulk_markers_NAs) != 0){
- write.table(pseudobulk_markers_NAs, file = "pseudobulk_markers_NAs.tsv", sep = "\t", row.names = TRUE, col.names = TRUE, quote = FALSE)
+if (nrow(pseudobulk_markers_NAs) != 0) {
+  write.table(pseudobulk_markers_NAs, file = "pseudobulk_markers_NAs.tsv", sep = "\t", row.names = TRUE, col.names = TRUE, quote = FALSE)
 }
 
 # Remove or eplace NAs with 0.0, so that it doesn't cause problems later on.
-if(replace.NAs == "replace"){
+if (replace.NAs == "replace") {
   pseudobulk_markers$p_val <- replace_na(pseudobulk_markers$p_val, 0.0)
   pseudobulk_markers$p_val_adj <- replace_na(pseudobulk_markers$p_val_adj, 0.0)
-}else if (replace.NAs == "remove"){
+} else if (replace.NAs == "remove") {
   pseudobulk_markers <- pseudobulk_markers[!is.na(pseudobulk_markers$p_val_adj), ]
 }
 
@@ -121,43 +122,43 @@ if(replace.NAs == "replace"){
 pseudobulk_markers_filtered <- pseudobulk_markers[pseudobulk_markers$p_val_adj < pval.cutoff.de, ]
 
 
-## Comparison name for the output file:
-comparison.name <- paste(samples1, "vs", samples2, "in_cluster", cluster, sep = "_")
-name.for.output.file <- paste("pseudobulk_de-list_", comparison.name, ".tsv", sep = "")
-
-
-# Plot
+# Plots
 pdf(file = "expressionPlots.pdf")
 
 pseudobulk_markers$gene <- rownames(pseudobulk_markers)
-ggplot(pseudobulk_markers, aes(avg_log2FC, -log10(p_val))) + geom_point(size = 0.5, alpha = 0.5) + theme_bw() +
-  ylab("-log10(unadjusted p-value)") + geom_text_repel(aes(label = ifelse(p_val_adj < 0.01, gene, "")), 
-  colour = "red", size = 3)  + ggtitle("Volcano plot of pseudobulk results (p_val_adj < 0.01 in red)")
+ggplot(pseudobulk_markers, aes(avg_log2FC, -log10(p_val))) +
+  geom_point(size = 0.5, alpha = 0.5) +
+  theme_bw() +
+  ylab("-log10(unadjusted p-value)") +
+  geom_text_repel(aes(label = ifelse(p_val_adj < 0.01, gene, "")),
+    colour = "red", size = 3
+  ) +
+  ggtitle("Volcano plot of pseudobulk results (p_val_adj < 0.01 in red)")
 
 # Choose top 6, use head in case of ties
 pseudobulk_markers %>%
   # group_by(cluster) %>%
   top_n(-6, p_val) %>%
-  head(6)-> top5_cell_selection
+  head(6) -> top5_cell_selection
 
 
 # VlnPlot(cell_selection, features = as.character(unique(top5_cell_selection$gene)), ncol = 3, group.by = "type", assay = "RNA", pt.size = 0.1)
 plotlist <- VlnPlot(cluster.bulk, features = as.character(rownames(top5_cell_selection)), ncol = 3, group.by = "type", assay = "RNA", pt.size = 0.1, combine = FALSE)
 p1 <- cowplot::plot_grid(plotlist = plotlist, ncol = 2, label_size = 10)
-title <- ggdraw() + draw_label("top genes, pseudobulk", fontface = 'bold')
+title <- ggdraw() + draw_label("top genes, pseudobulk", fontface = "bold")
 cowplot::plot_grid(title, p1, ncol = 1, rel_heights = c(0.1, 1))
 
 
 # Plot these genes across all clusters, but split by “type”
-plotlist2 <- VlnPlot(data.combined, features = as.character(rownames(top5_cell_selection)), ncol = 3, split.by = "type", assay = "RNA", pt.size = 0.1, combine = FALSE) 
+plotlist2 <- VlnPlot(data.combined, features = as.character(rownames(top5_cell_selection)), ncol = 3, split.by = "type", assay = "RNA", pt.size = 0.1, combine = FALSE)
 p2 <- cowplot::plot_grid(plotlist = plotlist2, ncol = 2, label_size = 10)
-title <- ggdraw() + draw_label("all cells top genes, clusters", fontface = 'bold')
+title <- ggdraw() + draw_label("all cells top genes, clusters", fontface = "bold")
 cowplot::plot_grid(title, p2, ncol = 1, rel_heights = c(0.1, 1))
 
 Idents(data.combined) <- "stim"
-plotlist3 <- VlnPlot(data.combined, features = as.character(rownames(top5_cell_selection)), ncol = 3, split.by = "type", assay = "RNA", pt.size = 0.1, combine = FALSE)  
+plotlist3 <- VlnPlot(data.combined, features = as.character(rownames(top5_cell_selection)), ncol = 3, split.by = "type", assay = "RNA", pt.size = 0.1, combine = FALSE)
 p3 <- cowplot::plot_grid(plotlist = plotlist3, ncol = 2, label_size = 10)
-title <- ggdraw() + draw_label("all cells top genes, samples", fontface = 'bold')
+title <- ggdraw() + draw_label("all cells top genes, samples", fontface = "bold")
 cowplot::plot_grid(title, p3, ncol = 1, rel_heights = c(0.1, 1))
 
 # According: https://nbisweden.github.io/workshop-scRNAseq/labs/seurat/seurat_05_dge.html#pseudobulk
@@ -168,10 +169,10 @@ pseudobulk_markers %>%
   head(10) -> top10_cell_selection
 
 
-#DotPlot(cluster.bulk,
- #       features = rownames(top10_cell_selection), group.by = "orig.ident",
+# DotPlot(cluster.bulk,
+#       features = rownames(top10_cell_selection), group.by = "orig.ident",
 #        assay = "RNA"
-#) + coord_flip() + ggtitle("Expression, pseudobulk") + RotatedAxis()
+# ) + coord_flip() + ggtitle("Expression, pseudobulk") + RotatedAxis()
 
 # To get the samples in reasonable order:
 dot_plot <- DotPlot(cluster.bulk, features = rownames(top10_cell_selection), group.by = "orig.ident", assay = "RNA")
@@ -179,52 +180,40 @@ levels_in_data <- unique(dot_plot$data$id)
 dot_plot$data$id <- factor(x = dot_plot$data$id, levels = levels_in_data) # change the order of the factor levels
 dot_plot + coord_flip() + ggtitle("Expression, pseudobulk") + RotatedAxis() # print again the plot
 
-# For some reason, any of the following doesn't work:
-# levels(as.ordered(as.character(dot_plot$data$id)))
-# levels(gtools::mixedsort(dot_plot$data$id)))
-# as.ordered(levels(dot_plot$data$id))
-# order(as.character(levels(dot_plot$data$id)))
-# sort(levels(dot_plot$data$id))
-# order(levels(dot_plot$data$id))
-# order(as.character(levels(dot_plot$data$id)))
-# This works in R 4.2.3 (Seurat 5.0.1): levels(as.ordered(as.character(dot_plot$data$id)))
-
-# save(dot_plot, file = "dot_plot.Robj")
 
 DotPlot(data.combined,
-        features = rownames(top10_cell_selection), group.by = "stim",
-        assay = "RNA"
+  features = rownames(top10_cell_selection), group.by = "stim",
+  assay = "RNA"
 ) + coord_flip() + ggtitle("Expression, all cells") + RotatedAxis()
 
 dev.off() # close the pdf
 
 
 
- # Add average expression to the table:
-   if (normalisation.method == "SCT") {
-     # testaa viel tää: 
-     aver_expr <- AverageExpression(object = cluster.bulk, slot = "data", assay = "SCT")
-   } else {
-     aver_expr <- AverageExpression(object = cluster.bulk)
-   }
+# Add average expression to the table:
+if (normalisation.method == "SCT") {
+  # to be tested:
+  aver_expr <- AverageExpression(object = cluster.bulk, slot = "data", assay = "SCT")
+} else {
+  aver_expr <- AverageExpression(object = cluster.bulk)
+}
 
-   aver_expr_in_clusters <- aver_expr[[1]]
-   
-   # select the wanted columns (based on samples1.cluster and samples2.cluster ) and rows (DEGs):
-   aver_expr_ident1 <- round(aver_expr_in_clusters[row.names(pseudobulk_markers_filtered),samples1 ], digits = 4)
-   aver_expr_ident2 <- round(aver_expr_in_clusters[row.names(pseudobulk_markers_filtered),samples2 ], digits = 4)
+aver_expr_in_clusters <- aver_expr[[1]]
 
-   full_table <- cbind(pseudobulk_markers_filtered, aver_expr_ident1 , aver_expr_ident2)
-   colnames(full_table) <- c(colnames(full_table)[1:5], paste("aver_expr_",samples1), paste("aver_expr_",samples2))
+# select the wanted columns (based on samples1.cluster and samples2.cluster ) and rows (DEGs):
+aver_expr_ident1 <- round(aver_expr_in_clusters[row.names(pseudobulk_markers_filtered), samples1], digits = 4)
+aver_expr_ident2 <- round(aver_expr_in_clusters[row.names(pseudobulk_markers_filtered), samples2], digits = 4)
 
+full_table <- cbind(pseudobulk_markers_filtered, aver_expr_ident1, aver_expr_ident2)
+colnames(full_table) <- c(colnames(full_table)[1:5], paste("aver_expr_", samples1), paste("aver_expr_", samples2))
 
 
 # Comparison name for the output file:
- comparison.name <- paste(samples1, "vs", samples2, "in_cluster", cluster, sep = "_")
- name.for.output.file <- paste("pseudobulk_de-list_", comparison.name, ".tsv", sep = "")
+comparison.name <- paste(samples1, "vs", samples2, "in_cluster", cluster, sep = "_")
+name.for.output.file <- paste("pseudobulk_de-list_", comparison.name, ".tsv", sep = "")
 
 # Write to table
- write.table(full_table, file = name.for.output.file, sep = "\t", row.names = TRUE, col.names = TRUE, quote = FALSE)
+write.table(full_table, file = name.for.output.file, sep = "\t", row.names = TRUE, col.names = TRUE, quote = FALSE)
 
 
 
